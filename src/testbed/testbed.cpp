8d9a52e749f42b8dba563a612dc04c67b9dedd1e
Andreas Schaefer <gentryx@gmx.de>
2011-06-29 01:22:02 +0200

    updated testbed example successfully builds OpenCL code

--- src/testbed/testbed.cpp
+++ src/testbed/testbed.cpp
@@ -1,6 +1,17 @@
-#include "cl.hpp"
+#include <CL/cl.hpp>
+#include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[])
 {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    for (int i = 0; i < platforms.size(); ++i) {
+        std::string str;
+        platforms[i].getInfo(CL_PLATFORM_NAME, &str);
+        std::cout << "Platform[" << i << "] = " << str << std::endl;
+    }
+
     return 0;
 }
