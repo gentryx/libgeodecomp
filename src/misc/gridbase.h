58241e50746a8b5ea46e28a5e9b561bf56bed27e
Andreas Schaefer <gentryx@gmx.de>
2012-03-06 23:52:58 +0100

    cosmetics, coding style

--- src/misc/gridbase.h
+++ src/misc/gridbase.h
@@ -11,15 +11,17 @@ namespace LibGeoDecomp {
  * because all methods are virtual, but not very efficient -- for the
  * same reason.
  */
-template<typename CELL, int DIM>
+template<typename CELL, int DIMENSIONS>
 class GridBase
 {
 public:
-    virtual CELL& at(const Coord<DIM>&) =0;
-    virtual const CELL& at(const Coord<DIM>&) const =0;
-    virtual CELL& atEdge() =0;
-    virtual const CELL& atEdge() const =0;
-    virtual CoordBox<DIM> boundingBox() const =0;
+    const static int DIM = DIMENSIONS;
+
+    virtual CELL& at(const Coord<DIM>&) = 0;
+    virtual const CELL& at(const Coord<DIM>&) const = 0;
+    virtual CELL& atEdge() = 0;
+    virtual const CELL& atEdge() const = 0;
+    virtual CoordBox<DIM> boundingBox() const = 0;
 };
 
 }
