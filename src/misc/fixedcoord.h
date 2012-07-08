c57c0a44d095f3b729de64366d91aa4b2701edde
Andreas Schaefer <gentryx@gmx.de>
2012-07-08 08:35:42 +0200

    cosmetics

--- src/misc/fixedcoord.h
+++ src/misc/fixedcoord.h
@@ -4,14 +4,13 @@
 namespace LibGeoDecomp {
 
 /**
- * is not meant to be used with actual instances, but rather to bind
- * template parameters in a convenient way. See the testbed and unit
- * tests for examples of how to use FixedCoord.
+ * is not meant to be used with actual instances, but rather to
+ * bind template parameters in a convenient way. See the testbed and
+ * unit tests for examples of how to use FixedCoord.
  */
 template<int DIM_X=0, int DIM_Y=0, int DIM_Z=0>
 class FixedCoord
 {
-public:
 };
 
 }
