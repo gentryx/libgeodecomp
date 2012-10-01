346e01f21f7f1b387575d3d69547e66c93082ec2
Andreas Schaefer <gentryx@gmx.de>
2012-10-01 23:50:46 +0200

    added first test for 2D Moore stencil

--- src/misc/test/unit/linepointerupdatefunctortest.h
+++ src/misc/test/unit/linepointerupdatefunctortest.h
@@ -1,5 +1,8 @@
 #include <cxxtest/TestSuite.h>
 #include <libgeodecomp/io/testinitializer.h>
+#include <libgeodecomp/misc/grid.h>
+#include <libgeodecomp/misc/testhelper.h>
+#include <libgeodecomp/misc/linepointerassembly.h>
 #include <libgeodecomp/misc/linepointerupdatefunctor.h>
 
 using namespace LibGeoDecomp; 
@@ -10,8 +13,30 @@ class LinePointerUpdateFunctorTest : public CxxTest::TestSuite
 {
 public:
 
-    void testBasic()
+    void testMoore2D()
     {
+        typedef Stencils::Moore<2, 1> Stencil;
+        typedef Grid<TestCell<2> > GridType;
+
+        Coord<2> dim(10, 5);
+        CoordBox<2> gridBox(Coord<2>(), dim);
+        TestInitializer<2> init(dim);
+        GridType gridOld(dim);
+        init.grid(&gridOld);
+        GridType gridNew = gridOld;
+        
+        for (int y = 0; y < dim.y(); ++y) {
+            Streak<2> streak(Coord<2>(0, y), dim.x());
+            TestCell<2> *pointers[Stencil::VOLUME];
+            LinePointerAssembly<Stencil>()(pointers, streak, gridOld);
+            LinePointerUpdateFunctor<TestCell<2> >()(
+                        streak, gridBox, pointers, &gridNew[streak.origin]);
+        }
+                                     
+        int cycle = init.startStep() * TestCell<2>::nanoSteps();
+        TS_ASSERT_TEST_GRID(GridType, gridOld, cycle);
+        cycle += 1;
+        TS_ASSERT_TEST_GRID(GridType, gridNew, cycle);
     }
 };
 
