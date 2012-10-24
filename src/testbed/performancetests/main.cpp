86b98d09e0cc66c511f5ef99842b536262d7524e
Andreas Schaefer <gentryx@gmx.de>
2012-10-24 18:13:18 +0200

    fixed coordinate access

--- src/testbed/performancetests/main.cpp
+++ src/testbed/performancetests/main.cpp
@@ -672,7 +672,7 @@ public:
                 hood[FixedCoord<0,  0,  0>()].temp +
                 hood[FixedCoord<1,  0,  0>()].temp +
                 hood[FixedCoord<0,  1,  0>()].temp +
-                hood[FixedCoord<1,  0,  1>()].temp) * (1.0 / 7.0);
+                hood[FixedCoord<0,  0,  1>()].temp) * (1.0 / 7.0);
     }
 
     double temp;
