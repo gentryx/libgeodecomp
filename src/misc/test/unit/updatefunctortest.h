ff30e376aab85da67d13bebab429e1af76456335
Andreas Schaefer <gentryx@gmx.de>
2012-10-25 22:32:08 +0200

    UpdateFunctor now delegates to the most suitable functor

---
+++ src/misc/test/unit/updatefunctortest.h
@@ -0,0 +1,156 @@
+#include <sstream>
+#include <vector>
+#include <cxxtest/TestSuite.h>
+#include <libgeodecomp/misc/grid.h>
+#include <libgeodecomp/misc/testhelper.h>
+#include <libgeodecomp/misc/updatefunctor.h>
+
+using namespace LibGeoDecomp; 
+
+namespace LibGeoDecomp {
+
+std::stringstream log;
+
+class BasicCell
+{
+public:
+    typedef Stencils::Moore<2, 1> Stencil;
+    typedef Topologies::Torus<2>::Topology Topology;
+
+    class API : public APIs::Base
+    {};
+
+    static int nanoSteps()
+    {
+        return 1;
+    }
+
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        log << "BasicCell::update(nanoStep = " << nanoStep << ")\n";
+    }
+
+};
+
+class LineUpdateCell
+{
+public:
+    typedef Stencils::Moore<2, 1> Stencil;
+    typedef Topologies::Torus<2>::Topology Topology;
+
+    class API : public APIs::Line
+    {};
+
+    static int nanoSteps()
+    {
+        return 1;
+    }
+
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        log << "LineUpdateCell::update(nanoStep = " << nanoStep << ")\n";
+    }
+
+    // won't ever be called as all current update functors support
+    // updateLine only with fixed neighborhoods
+    template<typename NEIGHBORHOOD>
+    static void updateLine(LineUpdateCell *target, long *x, long endX, const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        log << "LineUpdateCell::updateLine(x = " << *x << ", endX = " << endX << ", nanoStep = " << nanoStep << ")\n";
+    
+    }
+};
+
+class FixedCell
+{
+public:
+    typedef Stencils::Moore<2, 1> Stencil;
+    typedef Topologies::Torus<2>::Topology Topology;
+
+    class API : public APIs::Fixed
+    {};
+
+    static int nanoSteps()
+    {
+        return 1;
+    }
+
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        log << "FixedCell::update(nanoStep = " << nanoStep << ")\n";
+    }
+
+};
+
+class FixedLineUpdateCell
+{
+public:
+    typedef Stencils::Moore<2, 1> Stencil;
+    typedef Topologies::Torus<2>::Topology Topology;
+
+    class API : public APIs::Fixed, public APIs::Line
+    {};
+
+    static int nanoSteps()
+    {
+        return 1;
+    }
+
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        log << "FixedLineUpdateCell::update(nanoStep = " << nanoStep << ")\n";
+    }
+
+    template<typename NEIGHBORHOOD>
+    static void updateLine(FixedLineUpdateCell *target, long *x, long endX, const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        log << "FixedLineUpdateCell::updateLine(x = " << *x << ", endX = " << endX << ", nanoStep = " << nanoStep << ")\n";
+    
+    }
+};
+
+class UpdateFunctorTest : public CxxTest::TestSuite 
+{
+public:
+    void testSelector()
+    {
+        checkSelector<BasicCell>(
+            "BasicCell::update(nanoStep = 0)\n", 8);
+        checkSelector<LineUpdateCell>(
+            "LineUpdateCell::update(nanoStep = 0)\n", 8);
+        checkSelector<FixedCell>(
+            "FixedCell::update(nanoStep = 0)\n", 8);
+        checkSelector<FixedLineUpdateCell>(
+            "FixedLineUpdateCell::update(nanoStep = 0)\nFixedLineUpdateCell::updateLine(x = 2, endX = 9, nanoStep = 0)\nFixedLineUpdateCell::update(nanoStep = 0)\n", 1);
+    }
+
+private:
+    template<typename CELL>
+    void checkSelector(const std::string& line, int repeats)
+    {
+        Streak<2> streak(Coord<2>(2, 1), 10);
+        Coord<2> dim(20, 10);
+        int nanoStep = 0;
+
+        Grid<CELL> gridOld(dim);
+        Grid<CELL> gridNew(dim);
+
+        UpdateFunctor<CELL>()(streak, gridOld, &gridNew, nanoStep);
+
+        std::vector<char> message(1024 * 16, 0);
+        log.read(&message[0], 1024 * 16);
+        std::string expected = "";
+        for (int i = 0; i < repeats; ++i) {
+            expected += line;
+        }
+        TS_ASSERT_EQUALS(expected, std::string(&message[0]));
+        log.clear();
+    }
+    
+};
+
+}
