0d4154594cb7a79fce985d5f25050b6b9c5d931b
Andreas Schaefer <gentryx@gmx.de>
2013-01-09 20:02:40 +0100

    added convenience header libgeodecomp.h to prevent user code from having to include a pleothora of LibGeoDecomp-internal headers

---
+++ src/libgeodecomp.h
@@ -0,0 +1,13 @@
+#ifndef _libgeodecomp_libgeodecomp_h_
+#define _libgeodecomp_libgeodecomp_h_
+
+#include <libgeodecomp/config.h>
+
+#include <libgeodecomp/io/simpleinitializer.h>
+#include <libgeodecomp/io/parallelwriter.h>
+#include <libgeodecomp/loadbalancer/noopbalancer.h>
+#include <libgeodecomp/misc/fixedarray.h>
+#include <libgeodecomp/parallelization/serialsimulator.h>
+#include <libgeodecomp/parallelization/stripingsimulator.h>
+
+#endif
