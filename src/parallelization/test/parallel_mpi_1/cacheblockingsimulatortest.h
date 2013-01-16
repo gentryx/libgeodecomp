6a9b7dad83c8774c599ef2dcdd2b451adc6f077c
Andreas Schaefer <gentryx@gmx.de>
2013-01-16 03:54:02 +0100

    coordinate generations looks good, even for pipeline flushing

--- src/parallelization/test/parallel_mpi_1/cacheblockingsimulatortest.h
+++ src/parallelization/test/parallel_mpi_1/cacheblockingsimulatortest.h
@@ -9,6 +9,42 @@ using namespace LibGeoDecomp;
 
 namespace LibGeoDecomp {
 
+class Line
+{
+public:
+
+    Line() :
+        offset(-2)
+    {
+        std::stringstream buf;
+        for (int i = 0; i < 30; ++i) {
+            buf << ".";
+        }
+        string = buf.str();
+    }
+
+    template<typename ELEM>
+    Line(int offset, ELEM elem) :
+        offset(offset)
+    {
+        std::stringstream buf;
+        for (int i = 0; i < 30; ++i) {
+            buf << elem;
+        }
+        string = buf.str();
+    }
+
+    std::string toString()
+    {
+        std::stringstream buf;
+        buf << std::setw(2) << offset << " " << string;
+        return buf.str();
+    }
+
+    int offset;
+    std::string string;
+};
+
 class CacheBlockingSimulatorTest : public CxxTest::TestSuite 
 {
 public:
@@ -17,21 +53,15 @@ public:
         std::cout << "\n";
         pipelineLength = 5;
 
-        SuperVector<std::string> gridSource(10);
-        SuperVector<std::string> gridTarget(10);
-        SuperVector<std::string> gridBuffer(bufferSize());
+        SuperVector<Line> gridSource(10);
+        SuperVector<Line> gridTarget(10);
+        SuperVector<Line> gridBuffer(bufferSize());
 
         for (int i = 0; i < 10; ++i) {
-            gridSource[i] = line(0);
-        }
-        for (int i = 0; i < 10; ++i) {
-            gridTarget[i] = line('.');
-        }
-        for (int i = 0; i < bufferSize(); i += 2) {
-            gridBuffer[i] = line('.');
+            gridSource[i] = Line(i, 0);
         }
         for (int i = 1; i < bufferSize(); i += 2) {
-            gridBuffer[i] = line('X');
+            gridBuffer[i] = Line(-1, 'X');
         }
 
         printGrid("source", gridSource);
@@ -41,27 +71,28 @@ public:
 
         std::string command;
 
-        pipelinedUpdate(0, 0, 1, gridSource, gridBuffer, gridTarget);
-        std::cin >> command;
-        pipelinedUpdate(1, 1, 2, gridSource, gridBuffer, gridTarget);
-        std::cin >> command;
-        pipelinedUpdate(2, 2, 3, gridSource, gridBuffer, gridTarget);
-        std::cin >> command;
-        pipelinedUpdate(3, 3, 4, gridSource, gridBuffer, gridTarget);
-        std::cin >> command;
-        pipelinedUpdate(4, 4, 5, gridSource, gridBuffer, gridTarget);
-        std::cin >> command;
-
-        // pipelinedUpdate(0, 14, 5);
-
+        pipelinedUpdate( 0,  0, 0, 1, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 1,  1, 0, 2, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 2,  2, 0, 3, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 3,  3, 0, 4, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 4,  4, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 5,  5, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 6,  6, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 7,  7, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 8,  8, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate( 9,  9, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate(10, 10, 0, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate(11, 11, 1, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate(12, 12, 2, 5, gridSource, gridBuffer, gridTarget);
+        pipelinedUpdate(13, 13, 3, 5, gridSource, gridBuffer, gridTarget);
     }
 
-    void pipelinedUpdate(int sourceZ, int localZ, int currentLength,
-                         SuperVector<std::string>& gridSource,
-                         SuperVector<std::string>& gridBuffer,
-                         SuperVector<std::string>& gridTarget)
+    void pipelinedUpdate(int sourceZ, int localZ, int firstStage, int lastStage,
+                         SuperVector<Line>& gridSource,
+                         SuperVector<Line>& gridBuffer,
+                         SuperVector<Line>& gridTarget)
     {
-        for (int i = 0; i < currentLength; ++i) {
+        for (int i = firstStage; i < lastStage; ++i) {
             std::string source = (i == 0)                    ? "source" : "buffer";
             std::string target = (i == (pipelineLength - 1)) ? "target" : "buffer";
 
@@ -74,11 +105,22 @@ public:
                       << source << "[" << std::setw(2) << sourceIndex << "] -> " 
                       << target << "[" << std::setw(2) << targetIndex << "], time: " 
                       << i << " -> " << (i + 1) << ")\n";
-            SuperVector<std::string> *grid = &gridBuffer;
-            if (i == (pipelineLength - 1)) {
-                grid = &gridTarget;
+
+            SuperVector<Line> *gridOld = &gridBuffer;
+            if (i == 0) {
+                gridOld = &gridSource;
             }
-            (*grid)[targetIndex] = line(i + 1);
+
+            SuperVector<Line> *gridNew = &gridBuffer;
+            if (i == (pipelineLength - 1)) {
+                gridNew = &gridTarget;
+            }
+            
+            if ((sourceZ >= gridSource.size()) && (i == firstStage)) {
+                (*gridNew)[targetIndex] = Line(-1, "X");
+            } else {
+                (*gridNew)[targetIndex] = Line((*gridOld)[sourceIndex].offset, i + 1);
+            }
 
             printGrid("source", gridSource);
             printGrid("buffer", gridBuffer);
@@ -99,21 +141,11 @@ public:
         return pipelineLength * 3 - 3;
     }
 
-    template<typename T>
-    std::string line(T item)
-    {
-        std::stringstream buf;
-        for (int i = 0; i < 20; ++i) {
-            buf << item;
-        }
-        return buf.str();
-    }
-
-    void printGrid(std::string name, SuperVector<std::string> grid)
+    void printGrid(std::string name, SuperVector<Line> grid)
     {
         std::cout << name << ":\n";
         for (int i = 0; i < grid.size(); ++i) {
-            std::cout << std::setw(2) << i << " " << grid[i] << "\n";
+            std::cout << std::setw(2) << i << " " << grid[i].toString() << "\n";
         }
     }
 
