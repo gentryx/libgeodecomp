2c2484a348b1d22988eb264f236ab93088c7d116
Andreas Schaefer <gentryx@gmx.de>
2013-01-30 01:00:49 +0100

    CacheBlockingSimulator's test heavily reworked, apparently functional

--- src/parallelization/cacheblockingsimulator.h
+++ src/parallelization/cacheblockingsimulator.h
@@ -1,33 +1,52 @@
 #ifndef _libgeodecomp_parallelization_serialsimulator_h_
 #define _libgeodecomp_parallelization_serialsimulator_h_
 
-#include <libgeodecomp/misc/grid.h>
+#include <libgeodecomp/misc/displacedgrid.h>
 #include <libgeodecomp/misc/updatefunctor.h>
 #include <libgeodecomp/parallelization/monolithicsimulator.h>
 
 namespace LibGeoDecomp {
+
 /**
  * CacheBlockingSimulator is an experimental simulator to explore the
  * infrastructure required to implement a pipelined wavefront update
  * algorith and which benefits is may provide.
  */
-template<typename CELL_TYPE>
-class CacheBlockingSimulator : public MonolithicSimulator<CELL_TYPE>
+template<typename CELL>
+class CacheBlockingSimulator : public MonolithicSimulator<CELL>
 {
 public:
-    friend class SerialSimulatorTest;
-    typedef typename CELL_TYPE::Topology Topology;
-    typedef Grid<CELL_TYPE, Topology> GridType;
+    friend class CacheBlockingSimulatorTest;
+    typedef typename CELL::Topology Topology;
+    typedef typename Topologies::NDimensional<Topologies::NDimensional<Topologies::NDimensional<Topologies::ZeroDimensional, Topology::ParentTopology::ParentTopology::WRAP_EDGES>, Topology::ParentTopology::WRAP_EDGES>, true> BufferTopology;
+    typedef Grid<CELL, Topology> GridType;
+    typedef DisplacedGrid<CELL, BufferTopology> BufferType;
+    typedef SuperVector<SuperVector<Region<3> > > WavefrontFrames;
     static const int DIM = Topology::DIMENSIONS;
 
-    CacheBlockingSimulator(Initializer<CELL_TYPE> *_initializer) : 
-        MonolithicSimulator<CELL_TYPE>(_initializer)
+    CacheBlockingSimulator(
+        Initializer<CELL> *initializer,
+        int pipelineLength,
+        const Coord<DIM - 1>& wavefrontDim) : 
+        MonolithicSimulator<CELL>(initializer),
+        pipelineLength(pipelineLength),
+        wavefrontDim(wavefrontDim)
     {
         Coord<DIM> dim = initializer->gridBox().dimensions;
         curGrid = new GridType(dim);
         newGrid = new GridType(dim);
         initializer->grid(curGrid);
         initializer->grid(newGrid);
+
+        Coord<DIM> bufferDim;
+        for (int i = 0; i < DIM - 1; ++i) {
+            bufferDim[i] = wavefrontDim[i] + 2 * pipelineLength - 2;
+        }
+        bufferDim[DIM - 1] = pipelineLength * 4 - 4;
+        buffer = BufferType(CoordBox<DIM>(Coord<DIM>(), bufferDim), curGrid->getEdgeCell());
+
+        generateFrames();
+        nanoStep = 0;
     }
     
     ~CacheBlockingSimulator()
@@ -46,233 +65,203 @@ public:
         // fixme
     }
 
-private:
-    using MonolithicSimulator<CELL_TYPE>::initializer;
-    using MonolithicSimulator<CELL_TYPE>::steerers;
-    using MonolithicSimulator<CELL_TYPE>::stepNum;
-    using MonolithicSimulator<CELL_TYPE>::writers;
-    using MonolithicSimulator<CELL_TYPE>::getStep;
+    virtual const GridType *getGrid()
+    {
+        // fixme
+        return 0;
+    }
 
+private:
+    using MonolithicSimulator<CELL>::initializer;
+    using MonolithicSimulator<CELL>::steerers;
+    using MonolithicSimulator<CELL>::stepNum;
+    using MonolithicSimulator<CELL>::writers;
+    using MonolithicSimulator<CELL>::getStep;
+    
     GridType *curGrid;
     GridType *newGrid;
+    BufferType buffer;
+    int pipelineLength;
+    Coord<DIM - 1> wavefrontDim;
+    Grid<WavefrontFrames> frames;
+    unsigned nanoStep;
 
-    void updateWavefront(const Coord<DIM>& origin, const Coord<DIM>& dim, unsigned nanoStep, const Coord<DIM>& orginIncrement, const Coord<DIM>& dimDecrement)
+    void generateFrames()
     {
-        // wavefronts[y][x][z][t]
+        Coord<DIM> gridDim = initializer->gridBox().dimensions;
 
+        Coord<DIM - 1> framesDim;
+        for (int i = 0; i < (DIM - 1); ++i) {
+            framesDim[i] = gridDim[i] / wavefrontDim[i];
+            if ((gridDim[i] % wavefrontDim[i]) != 0) {
+                framesDim[i] += 1;
+            }
+        }
+        frames.resize(framesDim);
+
+        for (int y = 0; y < framesDim.y(); ++y) {
+            for (int x = 0; x < framesDim.x(); ++x) {
+                frames[Coord<2>(x, y)] = generateWavefrontFrames(
+                    Coord<3>(x * wavefrontDim.x(),
+                             y * wavefrontDim.y(),
+                             0));
+
+            }
+        }                
+    }
+
+    WavefrontFrames generateWavefrontFrames(const Coord<DIM> offset)
+    {
+        Coord<DIM> gridDim = initializer->gridBox().dimensions;
+        Coord<DIM> wavefrontRegionDim;
+        for (int i = 0; i < (DIM - 1); ++i) {
+            wavefrontRegionDim[i] = wavefrontDim[i];
+        }
+        wavefrontRegionDim[DIM - 1] = gridDim[DIM - 1] - offset[DIM - 1];
+        Region<DIM> region;
+        region << CoordBox<DIM>(offset, wavefrontRegionDim);
+        SuperVector<Region<3> > regions(pipelineLength);
+        regions[pipelineLength - 1] = region.expandWithTopology(
+            0, gridDim, Topologies::Cube<3>::Topology());
+        
+        for (int i = pipelineLength - 2; i >= 0; --i) {
+            regions[i] = regions[i + 1].expandWithTopology(
+                1, gridDim, Topologies::Cube<3>::Topology());
+        }
+
+        WavefrontFrames ret(gridDim[DIM - 1], SuperVector<Region<DIM> >(pipelineLength));
+
+        for (int index = index; index < gridDim[DIM - 1]; ++index) {
+            Coord<DIM> maskOrigin;
+            maskOrigin[DIM - 1] = index;
+            Coord<DIM> maskDim = gridDim;
+            maskDim[DIM - 1] = 1;
+            
+            Region<DIM> mask;
+            mask << CoordBox<DIM>(maskOrigin, maskDim);
+            for (int i = 0; i < pipelineLength; ++i) {
+                ret[index][i] = regions[i] & mask;
+            }
+        }
+            
+        return ret;
+    }
+
+    void pipelinedUpdate(
+        const Coord<DIM - 1>& frameCoord, 
+        int globalIndex, 
+        int localIndex, 
+        int firstStage, 
+        int lastStage)
+    {
+        // fixme: do this before z-loop
+        Coord<DIM> bufferOrigin;
+        // fixme: wrong on boundary with Torus topology
+        for (int d = 0; d < (DIM - 1); ++d) {
+            bufferOrigin[d] = std::max(0, frameCoord[d] * wavefrontDim[d] - pipelineLength + 1);
+        }
+        bufferOrigin[DIM - 1] = 0;
+        buffer.setOrigin(bufferOrigin);
+
+        for (int i = firstStage; i < lastStage; ++i) {
+            bool firstIteration = (i == 0);
+            bool lastIteration =  (i == (pipelineLength - 1));
+            int currentGlobalIndex = globalIndex - 2 * i;
+            bool needsFlushing = (i == firstStage) &&
+                (currentGlobalIndex >= newGrid->getDimensions()[DIM - 1]);
+            int sourceIndex = firstIteration ? currentGlobalIndex : normalizeIndex(localIndex + 2 - 4 * i);
+            int targetIndex = lastIteration  ? currentGlobalIndex : normalizeIndex(localIndex + 0 - 4 * i);
+            const Region<DIM>& updateFrame = frames[frameCoord][globalIndex - 2 * i][i];
+            unsigned curNanoStep = (nanoStep + i) % CELL::nanoSteps();
+
+            std::string source = firstIteration ? "source" : "buffer";
+            std::string target = lastIteration  ? "target" : "buffer";
+            // std::cout << "update(grid: " 
+            //           << source << "[" << std::setw(2) << sourceIndex << "] -> " 
+            //           << target << "[" << std::setw(2) << targetIndex << "], time: " 
+            //           << i << " -> " << (i + 1) << ", currentGlobalIndex: " 
+            //           << currentGlobalIndex << ")\n";
+            
+            if ( firstIteration &&  lastIteration) {
+                frameUpdate(needsFlushing, updateFrame, sourceIndex, targetIndex, *curGrid, newGrid, curNanoStep);
+            }
+            if ( firstIteration && !lastIteration) { 
+                frameUpdate(needsFlushing, updateFrame, sourceIndex, targetIndex, *curGrid, &buffer, curNanoStep);
+            }
+            
+            if (!firstIteration &&  lastIteration) {
+                frameUpdate(needsFlushing, updateFrame, sourceIndex, targetIndex, buffer, newGrid, curNanoStep);
+            }
+            if (!firstIteration && !lastIteration) {
+                frameUpdate(needsFlushing, updateFrame, sourceIndex, targetIndex, buffer, &buffer, curNanoStep);
+            }
+        }
+    }
+
+    template<class GRID1, class GRID2>
+    void frameUpdate(
+        bool needsFlushing,
+        const Region<DIM>& updateFrame,
+        int sourceIndex,
+        int targetIndex,
+        const GRID1& sourceGrid,
+        GRID2 *targetGrid,
+        unsigned curNanoStep)
+    {
+        if (needsFlushing) {
+            Coord<DIM> fillOrigin = buffer.getOrigin();
+            fillOrigin[DIM - 1] = targetIndex;
+            Coord<DIM> fillDim = buffer.getDimensions();
+            fillDim[DIM - 1] = 1;
+
+            // std::cout << "flushing " << CoordBox<DIM>(fillOrigin, fillDim);
+            buffer.fill(CoordBox<DIM>(fillOrigin, fillDim), buffer.getEdgeCell());
+        } else {
+            for (typename Region<DIM>::StreakIterator iter = updateFrame.beginStreak(); 
+                 iter != updateFrame.endStreak(); ++iter) {
+                Streak<DIM> sourceStreak = *iter;
+                Coord<DIM> targetCoord  = iter->origin;
+                sourceStreak.origin[DIM - 1] = sourceIndex;
+                targetCoord[DIM - 1] = targetIndex;
+                // fixme: fix nanostep
+                UpdateFunctor<CELL>()(sourceStreak, targetCoord, sourceGrid, targetGrid, curNanoStep);
+            }
+            // std::cout << "  frame: " << updateFrame.boundingBox();
+            // std::cout << "  sourceStreak: " << sourceStreak << "\n";
+            // std::cout << "  targetStreak: " << targetStreak << "\n";
+        }
+        // std::cout << "  bufferOrigin: " << buffer.getOrigin() << "\n";
+            
+        // SuperVector<Line> *gridOld = &gridBuffer;
+        // if (i == 0) {
+        //     gridOld = &gridSource;
+        // }
+
+        // SuperVector<Line> *gridNew = &gridBuffer;
+        // if (i == (pipelineLength - 1)) {
+        //     gridNew = &gridTarget;
+        // }
+            
+        // if ((globalIndex >= gridSource.size()) && (i == firstStage)) {
+        //     (*gridNew)[targetIndex] = Line(-1, "X");
+        // } else {
+        //     (*gridNew)[targetIndex] = Line((*gridOld)[sourceIndex].offset, i + 1);
+        // }
+
+        // printGrid("source", gridSource);
+        // printGrid("buffer", gridBuffer);
+        // printGrid("target", gridTarget);
+        // std::cout << "\n";
+    }
+
+    // normalize via topology, if at all?
+    int normalizeIndex(int localIndex)
+    {
+        int bufferSize = buffer.getDimensions()[DIM - 1];
+        return (localIndex + bufferSize) % bufferSize;
     }
 };
 
 }
 
 #endif
-
-// -1 XXXXXXXXXXXXXXXX
-//  0 X00000000000000X
-//  1 X00000000000000X
-//  2 X00000000000000X
-// -----------------------
-
-// 0.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d    ................ 
-// e -1 XXXXXXXXXXXXXXXX
-// f    ................
-// g -1 XXXXXXXXXXXXXXXX
-// h  0 X11111111111111X +
-// i    ................
-// j    ................
-// k    ................
-
-// 1.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d    ................ 
-// e -1 XXXXXXXXXXXXXXXX
-// f    ................
-// g -1 XXXXXXXXXXXXXXXX
-// h  0 X11111111111111X
-// i  1 X11111111111111X +
-// j    ................
-// k    ................
-
-// 2.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d    ................ 
-// e -1 XXXXXXXXXXXXXXXX
-// f  0 X22222222222222X +
-// g -1 XXXXXXXXXXXXXXXX
-// h  0 X11111111111111X
-// i  1 X11111111111111X
-// j    ................
-// k    ................
-
-// 3.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d    ................ 
-// e -1 XXXXXXXXXXXXXXXX
-// f  0 X22222222222222X 
-// g -1 XXXXXXXXXXXXXXXX
-// h  0 X11111111111111X
-// i  1 X11111111111111X
-// j  2 X11111111111111X +
-// k    ................
-
-// 4.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d    ................ 
-// e -1 XXXXXXXXXXXXXXXX
-// f  0 X22222222222222X
-// g  1 X22222222222222X +
-// h  0 X11111111111111X
-// i  1 X11111111111111X
-// j  2 X11111111111111X 
-// k    ................
-
-// 5.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X +
-// e -1 XXXXXXXXXXXXXXXX
-// f  0 X22222222222222X
-// g  1 X22222222222222X 
-// h  0 X11111111111111X
-// i  1 X11111111111111X
-// j  2 X11111111111111X
-// k    ................
-
-// 6.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e -1 XXXXXXXXXXXXXXXX
-// f  0 X22222222222222X
-// g  1 X22222222222222X
-// h  0 X11111111111111X
-// i  1 X11111111111111X
-// j  2 X11111111111111X
-// k  3 X11111111111111X +
-
-// 7.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e -1 XXXXXXXXXXXXXXXX
-// f  0 X22222222222222X
-// g  1 X22222222222222X
-// h  2 X22222222222222X +
-// i  1 X11111111111111X
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-
-// 8.
-// a -1 XXXXXXXXXXXXXXXX
-// b    ................ 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e  1 X33333333333333X +
-// f  0 X22222222222222X
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  1 X11111111111111X
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-
-// 9.
-// a -1 XXXXXXXXXXXXXXXX
-// b  0 X44444444444444X +
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e  1 X33333333333333X 
-// f  0 X22222222222222X
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  1 X11111111111111X
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-
-// 10. pipeline go
-// a -1 XXXXXXXXXXXXXXXX
-// b  0 X44444444444444X 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e  1 X33333333333333X 
-// f  0 X22222222222222X
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  1 X11111111111111X
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-// l  4 X11111111111111X +
-
-// 11.
-// a -1 XXXXXXXXXXXXXXXX
-// b  0 X44444444444444X 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e  1 X33333333333333X 
-// f  0 X22222222222222X
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  3 X22222222222222X +
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-// l  4 X11111111111111X 
-
-// 12.
-// a -1 XXXXXXXXXXXXXXXX
-// b  0 X44444444444444X 
-// c -1 XXXXXXXXXXXXXXXX
-// d  0 X33333333333333X 
-// e  1 X33333333333333X 
-// f  2 X33333333333333X +
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  3 X22222222222222X 
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-// l  4 X11111111111111X 
-
-// 13.
-// a -1 XXXXXXXXXXXXXXXX
-// b  0 X44444444444444X 
-// c  1 X44444444444444X +
-// d  0 X33333333333333X 
-// e  1 X33333333333333X 
-// f  2 X33333333333333X 
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  3 X22222222222222X 
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-// l  4 X11111111111111X 
-
-// 14.
-// -> to external grid
-// -------------------------
-
-// a -1 XXXXXXXXXXXXXXXX
-// b  0 X44444444444444X 
-// c  1 X44444444444444X +
-// d  0 X33333333333333X 
-// e  1 X33333333333333X 
-// f  2 X33333333333333X 
-// g  1 X22222222222222X
-// h  2 X22222222222222X 
-// i  3 X22222222222222X 
-// j  2 X11111111111111X
-// k  3 X11111111111111X 
-// l  4 X11111111111111X 
-
--- src/parallelization/test/parallel_mpi_1/cacheblockingsimulatortest.h
+++ src/parallelization/test/parallel_mpi_1/cacheblockingsimulatortest.h
@@ -2,155 +2,118 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <libgeodecomp/io/testinitializer.h>
 #include <libgeodecomp/misc/supervector.h>
+#include <libgeodecomp/misc/testcell.h>
 #include <libgeodecomp/parallelization/cacheblockingsimulator.h>
 
 using namespace LibGeoDecomp; 
 
 namespace LibGeoDecomp {
 
-class Line
-{
-public:
-
-    Line() :
-        offset(-2)
-    {
-        std::stringstream buf;
-        for (int i = 0; i < 30; ++i) {
-            buf << ".";
-        }
-        string = buf.str();
-    }
-
-    template<typename ELEM>
-    Line(int offset, ELEM elem) :
-        offset(offset)
-    {
-        std::stringstream buf;
-        for (int i = 0; i < 30; ++i) {
-            buf << elem;
-        }
-        string = buf.str();
-    }
-
-    std::string toString()
-    {
-        std::stringstream buf;
-        buf << std::setw(2) << offset << " " << string;
-        return buf.str();
-    }
-
-    int offset;
-    std::string string;
-};
-
 class CacheBlockingSimulatorTest : public CxxTest::TestSuite 
 {
 public:
+    typedef TestCell<3, Stencils::Moore<3, 1>, Topologies::Cube<3>::Topology> MyTestCell;
+
     void testBasic()
     {
-        std::cout << "\n";
-        pipelineLength = 5;
+        
+        CacheBlockingSimulator<MyTestCell> sim(
+            new TestInitializer<MyTestCell>(
+                Coord<3>(40, 1, 10), 
+                10000,
+                0),
+            5, 
+            Coord<2>(16, 16));
+        std::string buf;
 
-        SuperVector<Line> gridSource(10);
-        SuperVector<Line> gridTarget(10);
-        SuperVector<Line> gridBuffer(bufferSize());
 
-        for (int i = 0; i < 10; ++i) {
-            gridSource[i] = Line(i, 0);
-        }
-        for (int i = 1; i < bufferSize(); i += 2) {
-            gridBuffer[i] = Line(-1, 'X');
-        }
+        sim.buffer.fill(sim.buffer.boundingBox(), sim.curGrid->getEdgeCell());
 
-        printGrid("source", gridSource);
-        printGrid("buffer", gridBuffer);
-        printGrid("target", gridTarget);
-        std::cout << "\n";
+        printState(sim);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 0, 0, 0, 1);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 1, 1, 0, 1);
 
-        std::string command;
+        sim.pipelinedUpdate(Coord<2>(0, 0), 2, 2, 0, 2);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 3, 3, 0, 2);
+        printState(sim);
 
-        pipelinedUpdate( 0,  0, 0, 1, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 1,  1, 0, 2, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 2,  2, 0, 3, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 3,  3, 0, 4, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 4,  4, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 5,  5, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 6,  6, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 7,  7, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 8,  8, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate( 9,  9, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate(10, 10, 0, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate(11, 11, 1, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate(12, 12, 2, 5, gridSource, gridBuffer, gridTarget);
-        pipelinedUpdate(13, 13, 3, 5, gridSource, gridBuffer, gridTarget);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 4, 4, 0, 3);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 5, 5, 0, 3);
+        printState(sim);
+
+        sim.pipelinedUpdate(Coord<2>(0, 0), 6, 6, 0, 4);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 7, 7, 0, 4);
+        printState(sim);
+
+        sim.pipelinedUpdate(Coord<2>(0, 0), 8, 8, 0, 5);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 9, 9, 0, 5);
+        printState(sim);
+
+        sim.pipelinedUpdate(Coord<2>(0, 0), 10, 10, 0, 5);
+        printState(sim);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 11, 11, 1, 5);
+        printState(sim);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 12, 12, 1, 5);
+        printState(sim);
+
+        sim.pipelinedUpdate(Coord<2>(0, 0), 13, 13, 2, 5);
+        printState(sim);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 14, 14, 2, 5);
+        printState(sim);
+
+
+        sim.pipelinedUpdate(Coord<2>(0, 0), 15, 15, 3, 5);
+        printState(sim);
+        sim.pipelinedUpdate(Coord<2>(0, 0), 16, 16, 3, 5);
+        printState(sim);
+
+        sim.pipelinedUpdate(Coord<2>(0, 0), 17, 17, 4, 5);
+        printState(sim);
+
+        // sim.pipelinedUpdate(Coord<2>(0, 0), 12, 12, 2, 5);
+        // printState(sim);
+
+        // for (int z = 0; z < 20; ++z) {
+        //     int lastStage = std::min(5, z);
+        //     sim.pipelinedUpdate(Coord<2>(1, 1), z, z, 0, lastStage);
+        // }
+        // // std::cin >> buf;
     }
 
-    void pipelinedUpdate(int globalIndex, int localIndex, int firstStage, int lastStage,
-                         SuperVector<Line>& gridSource,
-                         SuperVector<Line>& gridBuffer,
-                         SuperVector<Line>& gridTarget)
+
+    void printState(const CacheBlockingSimulator<MyTestCell>& sim)
     {
-        for (int i = firstStage; i < lastStage; ++i) {
-            std::string source = (i == 0)                    ? "source" : "buffer";
-            std::string target = (i == (pipelineLength - 1)) ? "target" : "buffer";
+        return;
 
-            int sourceIndex = (i == 0)                    ? globalIndex                : 
-                                                            normalizeIndex(localIndex + 2 - 3 * i);
-            int targetIndex = (i == (pipelineLength - 1)) ? globalIndex - pipelineLength + 1 : 
-                                                            normalizeIndex(localIndex + 0 - 3 * i);
+        std::cout << "curGrid:\n";
+        printGrid(*sim.curGrid);
+        std::cout << "buffer:\n";
+        printGrid(sim.buffer);
+        std::cout << "newGrid:\n";
+        printGrid(*sim.newGrid);
+        std::cout << "\n";
+    }
 
-            std::cout << "update(grid: " 
-                      << source << "[" << std::setw(2) << sourceIndex << "] -> " 
-                      << target << "[" << std::setw(2) << targetIndex << "], time: " 
-                      << i << " -> " << (i + 1) << ")\n";
-
-            SuperVector<Line> *gridOld = &gridBuffer;
-            if (i == 0) {
-                gridOld = &gridSource;
+    template<typename GRID>
+    void printGrid(GRID grid)
+    {
+        Coord<3> dim = grid.getDimensions();
+        for (int z = 0; z < dim.z(); ++z) {
+            std::cout << std::setw(3) << z << " " 
+                      << std::setw(3) << grid[Coord<3>(5, 0, z)].pos.z() << "  ";
+            for (int x = 0; x < dim.x(); ++x) {        
+                MyTestCell c = grid[Coord<3>(x, 0, z)];
+                if (c.isEdgeCell) {
+                    std::cout << "X"; 
+                } else {
+                    std::cout << c.cycleCounter;
+                }
             }
-
-            SuperVector<Line> *gridNew = &gridBuffer;
-            if (i == (pipelineLength - 1)) {
-                gridNew = &gridTarget;
-            }
-            
-            if ((globalIndex >= gridSource.size()) && (i == firstStage)) {
-                (*gridNew)[targetIndex] = Line(-1, "X");
-            } else {
-                (*gridNew)[targetIndex] = Line((*gridOld)[sourceIndex].offset, i + 1);
-            }
-
-            printGrid("source", gridSource);
-            printGrid("buffer", gridBuffer);
-            printGrid("target", gridTarget);
             std::cout << "\n";
         }
-
-        std::cout << "\n";
     }
-
-    int normalizeIndex(int localIndex)
-    {
-        return (localIndex + bufferSize()) % bufferSize();
-    }
-
-    int bufferSize()
-    {
-        return pipelineLength * 3 - 3;
-    }
-
-    void printGrid(std::string name, SuperVector<Line> grid)
-    {
-        std::cout << name << ":\n";
-        for (int i = 0; i < grid.size(); ++i) {
-            std::cout << std::setw(2) << i << " " << grid[i].toString() << "\n";
-        }
-    }
-
-private:
-    int pipelineLength;
 };
 
 }
