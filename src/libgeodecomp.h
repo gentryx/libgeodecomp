9cf681d697d009b1665f477a5f03df22b3b0f180
Andreas Schaefer <gentryx@gmx.de>
2013-04-10 07:50:29 +0200

    fixing build errors with Intel MPI on RRZE's Woody

--- src/libgeodecomp.h
+++ src/libgeodecomp.h
@@ -3,6 +3,10 @@
 
 #include <libgeodecomp/config.h>
 
+#ifdef LIBGEODECOMP_FEATURE_MPI
+#include <mpi.h>
+#endif
+
 #include <libgeodecomp/io/simpleinitializer.h>
 #include <libgeodecomp/io/parallelwriter.h>
 #include <libgeodecomp/loadbalancer/noopbalancer.h>
