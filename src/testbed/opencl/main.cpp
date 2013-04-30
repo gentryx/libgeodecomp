0e45ad1351956af709b0fb1535287d10d807f837
Jochen Keil <jochen.keil@gmail.com>
2013-04-30 09:21:18 +0200

    Indendation was a bit off; fix it
    Cosmetic change only
    
    --HG--
    branch : for_andi

--- src/testbed/opencl/main.cpp
+++ src/testbed/opencl/main.cpp
@@ -1,4 +1,4 @@
-/* vim:set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/* vim:set expandtab tabstop=2 shiftwidth=2 softtabstop=2: */
 
 #include <iostream>
 #include <libgeodecomp/libgeodecomp.h>
@@ -8,68 +8,67 @@
 using namespace LibGeoDecomp;
 
 std::ostream& operator<< ( std::ostream& o, cl::Platform p ) {
-    o << "CL_PLATFORM_VERSION\t= "     << p.getInfo<CL_PLATFORM_VERSION>()
-      << std::endl
-      << "CL_PLATFORM_NAME\t= "        << p.getInfo<CL_PLATFORM_NAME>()
-      << std::endl
-      << "CL_PLATFORM_VENDOR\t= "      << p.getInfo<CL_PLATFORM_VENDOR>()
-      << std::endl
-      << "CL_PLATFORM_EXTENSIONS\t= "  << p.getInfo<CL_PLATFORM_EXTENSIONS>();
-    return o;
+  o << "CL_PLATFORM_VERSION\t= "     << p.getInfo<CL_PLATFORM_VERSION>()
+    << std::endl
+    << "CL_PLATFORM_NAME\t= "        << p.getInfo<CL_PLATFORM_NAME>()
+    << std::endl
+    << "CL_PLATFORM_VENDOR\t= "      << p.getInfo<CL_PLATFORM_VENDOR>()
+    << std::endl
+    << "CL_PLATFORM_EXTENSIONS\t= "  << p.getInfo<CL_PLATFORM_EXTENSIONS>();
+  return o;
 }
 
 std::ostream& operator<< ( std::ostream& o, cl::Device d ) {
-    o << "CL_DEVICE_EXTENSIONS\t\t\t= "
-      << d.getInfo<CL_DEVICE_EXTENSIONS>()                << std::endl
-      << "CL_DEVICE_GLOBAL_MEM_SIZE\t\t= "
-      << d.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()           << std::endl
-      << "CL_DEVICE_LOCAL_MEM_SIZE\t\t= "
-      << d.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>()            << std::endl
-      << "CL_DEVICE_MAX_CLOCK_FREQUENCY\t\t= "
-      << d.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>()       << std::endl
-      << "CL_DEVICE_MAX_COMPUTE_UNITS\t\t= "
-      << d.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>()         << std::endl
-      << "CL_DEVICE_MAX_CONSTANT_ARGS\t\t= "
-      << d.getInfo<CL_DEVICE_MAX_CONSTANT_ARGS>()         << std::endl
-      << "CL_DEVICE_MAX_MEM_ALLOC_SIZE\t\t= "
-      << d.getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>()        << std::endl
-      << "CL_DEVICE_MAX_PARAMETER_SIZE\t\t= "
-      << d.getInfo<CL_DEVICE_MAX_PARAMETER_SIZE>()        << std::endl
-      << "CL_DEVICE_MAX_WORK_GROUP_SIZE\t\t= "
-      << d.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>()       << std::endl
-      << "CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS\t= "
-      << d.getInfo<CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS>()  << std::endl
-      << "CL_DEVICE_MAX_WORK_ITEM_SIZES\t\t= ";
+  o << "CL_DEVICE_EXTENSIONS\t\t\t= "
+    << d.getInfo<CL_DEVICE_EXTENSIONS>()                << std::endl
+    << "CL_DEVICE_GLOBAL_MEM_SIZE\t\t= "
+    << d.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>()           << std::endl
+    << "CL_DEVICE_LOCAL_MEM_SIZE\t\t= "
+    << d.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>()            << std::endl
+    << "CL_DEVICE_MAX_CLOCK_FREQUENCY\t\t= "
+    << d.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>()       << std::endl
+    << "CL_DEVICE_MAX_COMPUTE_UNITS\t\t= "
+    << d.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>()         << std::endl
+    << "CL_DEVICE_MAX_CONSTANT_ARGS\t\t= "
+    << d.getInfo<CL_DEVICE_MAX_CONSTANT_ARGS>()         << std::endl
+    << "CL_DEVICE_MAX_MEM_ALLOC_SIZE\t\t= "
+    << d.getInfo<CL_DEVICE_MAX_MEM_ALLOC_SIZE>()        << std::endl
+    << "CL_DEVICE_MAX_PARAMETER_SIZE\t\t= "
+    << d.getInfo<CL_DEVICE_MAX_PARAMETER_SIZE>()        << std::endl
+    << "CL_DEVICE_MAX_WORK_GROUP_SIZE\t\t= "
+    << d.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>()       << std::endl
+    << "CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS\t= "
+    << d.getInfo<CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS>()  << std::endl
+    << "CL_DEVICE_MAX_WORK_ITEM_SIZES\t\t= ";
 
-    std::vector<size_t> wis = d.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
-    std::vector<size_t>::iterator it = wis.begin();
+  std::vector<size_t> wis = d.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
+  std::vector<size_t>::iterator it = wis.begin();
 
-    o << "[";
-    while ( it != wis.end() ) o << *it << ( ++it != wis.end() ? ", " : "" );
-    o << "]"                                              << std::endl;
+  o << "[";
+  while ( it != wis.end() ) o << *it << ( ++it != wis.end() ? ", " : "" );
+  o << "]"                                              << std::endl;
 
-    o << "CL_DEVICE_NAME\t\t\t\t= "
-      << d.getInfo<CL_DEVICE_NAME>()                      << std::endl
-      << "CL_DEVICE_VENDOR\t\t\t= "
-      << d.getInfo<CL_DEVICE_VENDOR>()                    << std::endl
-      << "CL_DEVICE_VERSION\t\t\t= "
-      << d.getInfo<CL_DEVICE_VERSION>()                   << std::endl
-      << "CL_DRIVER_VERSION\t\t\t= "
-      << d.getInfo<CL_DRIVER_VERSION>()                   << std::endl
-      << "CL_DEVICE_EXTENSIONS\t\t\t= "
-      << d.getInfo<CL_DEVICE_EXTENSIONS>();
+  o << "CL_DEVICE_NAME\t\t\t\t= "
+    << d.getInfo<CL_DEVICE_NAME>()                      << std::endl
+    << "CL_DEVICE_VENDOR\t\t\t= "
+    << d.getInfo<CL_DEVICE_VENDOR>()                    << std::endl
+    << "CL_DEVICE_VERSION\t\t\t= "
+    << d.getInfo<CL_DEVICE_VERSION>()                   << std::endl
+    << "CL_DRIVER_VERSION\t\t\t= "
+    << d.getInfo<CL_DRIVER_VERSION>()                   << std::endl
+    << "CL_DEVICE_EXTENSIONS\t\t\t= "
+    << d.getInfo<CL_DEVICE_EXTENSIONS>();
 
-    return o;
+  return o;
 }
 
 class Cell
 {
-public:
-    class API :
-        public APITraits::HasFixedCoordsOnlyUpdate,
-        public APITraits::HasStencil<Stencils::VonNeumann<3, 1> >,
-        public APITraits::HasCubeTopology<3>
-    {};
+  public:
+    typedef Stencils::VonNeumann<3, 1> Stencil;
+    typedef Topologies::Cube<3>::Topology Topology;
+    class API : public CellAPITraits::Fixed
+  {};
 
     inline explicit Cell(const double& v = 0) :
         temp(v)
@@ -92,61 +91,65 @@ public:
 template<typename CELL>
 class MyFutureOpenCLStepper
 {
-public:
-    typedef typename APITraits::SelectTopology<CELL>::Value Topology;
+  public:
+    const static int DIM = CELL::Topology::DIM;
+
+    typedef typename CELL::Topology Topology;
     typedef DisplacedGrid<CELL, Topology>  GridType;
     const static int DIM = Topology::DIM;
 
     MyFutureOpenCLStepper(const CoordBox<DIM> box) :
-        box(box),
-        hostGrid(box)
+      box(box),
+      hostGrid(box)
+  {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get ( &platforms );
+
+    std::cerr << "# of Platforms: " << platforms.size() << std::endl;
+
+    // for ( auto platform = platforms.begin(); platform != platforms.end(); ++platform )
+    for ( auto & platform : platforms )
+      std::cerr << platform;
+
+    std::vector<cl::Device> devices;
+
+    platforms[0].getDevices ( CL_DEVICE_TYPE_ALL, &devices );
+
+    std::cerr << "# of Devices: " << devices.size() << std::endl;
+
+    for ( auto & device : devices )
+      std::cerr << device;
+
+    // todo: allocate deviceGridOld, deviceGridNew via OpenCL on device
+
+
+    std::cerr << "x: " << hostGrid.getDimensions().x() << std::endl;
+    std::cerr << "y: " << hostGrid.getDimensions().y() << std::endl;
+    std::cerr << "z: " << hostGrid.getDimensions().z() << std::endl;
+    std::cerr << "prod: " << hostGrid.getDimensions().prod() << std::endl;
+    std::cerr << "sizeof ( CELL ): " << sizeof ( CELL ) << std::endl;
+
+    // todo: specify OpenCL platform, device via constructor
+  }
+
+  void regionToVec(const Region<DIM>& region, SuperVector<int> *coordsX, SuperVector<int> *coordsY, SuperVector<int> *coordsZ)
     {
-       std::vector<cl::Platform> platforms;
-       cl::Platform::get ( &platforms );
-
-       std::cerr << "# of Platforms: " << platforms.size() << std::endl;
-
-       for ( auto & platform : platforms )
-           std::cerr << platform;
-
-       std::vector<cl::Device> devices;
-
-       platforms[0].getDevices ( CL_DEVICE_TYPE_ALL, &devices );
-
-       std::cerr << "# of Devices: " << devices.size() << std::endl;
-
-       for ( auto & device : devices )
-           std::cerr << device;
-
-        // todo: allocate deviceGridOld, deviceGridNew via OpenCL on device
-
-       std::cerr << "x: " << hostGrid.getDimensions().x() << std::endl;
-       std::cerr << "y: " << hostGrid.getDimensions().y() << std::endl;
-       std::cerr << "z: " << hostGrid.getDimensions().z() << std::endl;
-       std::cerr << "prod: " << hostGrid.getDimensions().prod() << std::endl;
-       std::cerr << "sizeof ( CELL ): " << sizeof ( CELL ) << std::endl;
-
-        // todo: specify OpenCL platform, device via constructor
-    }
-
-    void regionToVec(const Region<DIM>& region, SuperVector<int> *coordsX, SuperVector<int> *coordsY, SuperVector<int> *coordsZ)
-    {
-        // todo: iterate through region and add all coordinates to the corresponding vectors
+      // todo: iterate through region and add all coordinates to the corresponding vectors
     }
 
     template<typename GRID>
-    void setGridRegion(const GRID& grid, const Region<DIM>& region)
-    {
+      void setGridRegion(const GRID& grid, const Region<DIM>& region)
+      {
         // todo: copy all coords in region from grid (on host) to deviceGridOld
-    }
+      }
 
     template<typename GRID>
-    void getGridRegion(const GRID *grid, const Region<DIM>& region)
-    {
+      void getGridRegion(const GRID *grid, const Region<DIM>& region)
+      {
         // todo: copy all coords in region from deviceGridOld (on host) to grid
-    }
+      }
 
-private:
+  private:
     CoordBox<DIM> box;
     GridType hostGrid;
 
@@ -156,10 +159,11 @@ private:
 
 int main(int argc, char **argv)
 {
-    MyFutureOpenCLStepper<Cell> stepper(
-        CoordBox<3>(
-            Coord<3>(10, 10, 10),
-            Coord<3>(20, 30, 40)));
-    std::cout << "test: " << sizeof(stepper) << "\n";
-    return 0;
+  MyFutureOpenCLStepper<Cell> stepper(
+      CoordBox<3>(
+        Coord<3>(10, 10, 10),
+        Coord<3>(20, 30, 40)));
+  std::cout << "test: " << sizeof(stepper) << "\n";
+
+  return 0;
 }
