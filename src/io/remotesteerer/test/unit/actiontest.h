47382374a287a2d8de0f74fabfdc4caf5ab5f92e
Andreas Schaefer <gentryx@gmx.de>
2013-06-04 09:58:32 +0200

    Action should rather interface with Pipe

--- src/io/remotesteerer/test/unit/actiontest.h
+++ src/io/remotesteerer/test/unit/actiontest.h
@@ -1,9 +1,9 @@
 #include <cxxtest/TestSuite.h>
 #include <libgeodecomp/io/remotesteerer/action.h>
-#include <libgeodecomp/io/remotesteerer/commandserver.h>
+#include <libgeodecomp/io/remotesteerer/pipe.h>
 
 using namespace LibGeoDecomp;
-using namespace LibGeoDecomp::RemoteSteererHelpers;;
+using namespace LibGeoDecomp::RemoteSteererHelpers;
 
 namespace LibGeoDecomp {
 
@@ -17,42 +17,27 @@ public:
             RemoteSteererHelpers::Action<int>("this is but a dummy action", "mock")
         {}
 
-        virtual void operator()(const StringOps::StringVec& parameters, CommandServer<int> *server)
+        virtual void operator()(const StringOps::StringVec& parameters, Pipe *pipe)
         {
-            server->sendMessage("MockAction mocks you!");
+            pipe->addSteeringFeedback("MockAction mocks you!");
         }
-    };
-
-    class MockCommandServer : public CommandServer<int>
-    {
-    public:
-        MockCommandServer() :
-            CommandServer<int>(12345, CommandServer<int>::FunctionMap(), 0)
-        {}
-
-        void sendMessage(const std::string& message)
-        {
-            messages << message;
-        }
-
-        StringOps::StringVec messages;
     };
 
     void testBasic()
     {
-        MockCommandServer server;
-        {
-            MockAction action;
-            TS_ASSERT_EQUALS("this is but a dummy action", action.helpMessage());
-            TS_ASSERT_EQUALS("mock", action.key());
-            StringOps::StringVec parameters;
-            parameters << "mock"
-                       << "arg0"
-                       << "arg1";
-            std::cout << "calling action\n";
-            action(parameters, &server);
-            std::cout << "returning\n";
-        }
+        Pipe pipe;
+        MockAction action;
+
+        TS_ASSERT_EQUALS("this is but a dummy action", action.helpMessage());
+        TS_ASSERT_EQUALS("mock", action.key());
+        StringOps::StringVec parameters;
+        parameters << "mock"
+                   << "arg0"
+                   << "arg1";
+        action(parameters, &pipe);
+        StringOps::StringVec feedback = pipe.retrieveSteeringFeedback();
+        TS_ASSERT_EQUALS(feedback.size(), 1);
+        TS_ASSERT_EQUALS(feedback[0], "MockAction mocks you!");
     }
 };
 
