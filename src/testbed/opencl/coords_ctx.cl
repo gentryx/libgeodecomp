39987af4a48bdbfb9d66e96091a792b00bc36d09
Jochen Keil <jochen.keil@gmail.com>
2013-06-18 08:54:35 +0200

    Rename neighbors_per_point => num_neighbors
    --HG--
    branch : for_andi

--- src/testbed/opencl/coords_ctx.cl
+++ src/testbed/opencl/coords_ctx.cl
@@ -1,6 +1,6 @@
 typedef struct {
   int    num_points;
-  int    neighbors_per_point;
+  int    num_neighbors;
   int3   points_size;
   __constant int3 * points;
   __constant int  * indices;
--- src/testbed/opencl/main.cpp
+++ src/testbed/opencl/main.cpp
@@ -11,7 +11,7 @@ using namespace LibGeoDecomp;
 
 typedef struct {
   cl_int    num_points;
-  cl_int    neighbors_per_point;
+  cl_int    num_neighbors;
   cl_int3   points_size;
   cl_int3 * points;
   cl_int  * indices;
@@ -135,7 +135,7 @@ class MyFutureOpenCLStepper {
     coords_ctx coords;
 
     coords.num_points = hostGrid.getDimensions().prod();
-    coords.neighbors_per_point = num_neighbors;
+    coords.num_neighbors = num_neighbors;
     coords.points_size = { hostGrid.getDimensions().x(),
                            hostGrid.getDimensions().y(),
                            0 };
