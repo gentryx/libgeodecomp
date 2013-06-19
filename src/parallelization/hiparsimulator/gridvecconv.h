4d3a8d2fa9c74ae6be2c5d4320c1cae61aff7a5e
Thomas Heller <thom.heller@gmail.com>
2013-06-19 22:08:38 +0200

    Coding style compliance

--- src/parallelization/hiparsimulator/gridvecconv.h
+++ src/parallelization/hiparsimulator/gridvecconv.h
@@ -14,7 +14,7 @@ class GridVecConv
 public:
     template<typename GRID_TYPE>
     static SuperVector<typename GRID_TYPE::CellType> gridToVector(
-        const GRID_TYPE& grid, 
+        const GRID_TYPE& grid,
         const Region<GRID_TYPE::DIM>& region)
     {
         SuperVector<typename GRID_TYPE::CellType> ret(region.size());
@@ -24,18 +24,21 @@ public:
 
     template<typename GRID_TYPE>
     static void gridToVector(
-        const GRID_TYPE& grid, 
+        const GRID_TYPE& grid,
         SuperVector<typename GRID_TYPE::CellType> *vec,
         const Region<GRID_TYPE::DIM>& region)
     {
-        if (vec->size() != std::size_t(region.size()))
+        if (vec->size() != std::size_t(region.size())) {
             throw std::logic_error("region doesn't match vector size");
-        
-        if(vec->size() == 0) return;
+        }
+
+        if(vec->size() == 0) {
+            return;
+        }
 
         typename GRID_TYPE::CellType *dest = &(*vec)[0];
-        
-        for (typename Region<GRID_TYPE::DIM>::StreakIterator i = region.beginStreak(); 
+
+        for (typename Region<GRID_TYPE::DIM>::StreakIterator i = region.beginStreak();
              i != region.endStreak(); ++i) {
             const typename GRID_TYPE::CellType *start = &grid[i->origin];
             std::copy(start, start + i->length(), dest);
@@ -45,17 +48,21 @@ public:
 
     template<typename VEC_TYPE, typename GRID_TYPE, typename REGION_TYPE>
     static void vectorToGrid(
-        const VEC_TYPE& vec, 
-        GRID_TYPE *grid, 
+        const VEC_TYPE& vec,
+        GRID_TYPE *grid,
         const REGION_TYPE& region)
     {
-        if (vec.size() != std::size_t(region.size()))
+        if (vec.size() != std::size_t(region.size())) {
             throw std::logic_error("region doesn't match vector size");
-        
-        if(vec.size() == 0) return;
+        }
+
+        if(vec.size() == 0) {
+            return;
+        }
 
         const typename GRID_TYPE::CellType *source = &vec[0];
-        for (typename Region<GRID_TYPE::DIM>::StreakIterator i = region.beginStreak(); 
+
+        for (typename Region<GRID_TYPE::DIM>::StreakIterator i = region.beginStreak();
              i != region.endStreak(); ++i) {
             unsigned length = i->length();
             const typename GRID_TYPE::CellType *end = source + length;
