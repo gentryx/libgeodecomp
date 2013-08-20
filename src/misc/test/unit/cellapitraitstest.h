4309b8f5010775a025521c2075f1f02c177d042a
Andreas Schaefer <gentryx@gmx.de>
2013-08-20 08:49:50 +0200

    adding tests for CellAPITraits to steer illustrate interface usage

--- src/misc/test/unit/cellapitraitstest.h
+++ src/misc/test/unit/cellapitraitstest.h
@@ -3,26 +3,42 @@
 
 using namespace LibGeoDecomp;
 
-std::ostringstream myCellAPITestEvents;
+std::ostringstream myTestEvents;
 
-class MyDummyCell
+class MySimpleDummyCell
 {
 public:
-    typedef Topologies::Cube<2>::Topology Topology;
     typedef Stencils::Moore<2, 1> Stencil;
 
     class API : public CellAPITraits::Base
     {};
 
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, unsigned nanoStep)
+    {
+        myTestEvents << "MySimpleDummyCell::update()\n";
+    }
+};
+
+class MyFancyDummyCell
+{
+public:
+    typedef Stencils::Moore<3, 1> Stencil;
+
+    class API :
+        public CellAPITraits::Base,
+        public CellAPITraitsFixme::HasTorusTopology<3>
+    {};
+
     static unsigned nanoSteps()
     {
-        return 1;
+        return 3;
     }
 
     template<typename NEIGHBORHOOD>
     void update(const NEIGHBORHOOD& hood, unsigned nanoStep)
     {
-        myCellAPITestEvents << "MyDummyCell::update()\n";
+        myTestEvents << "MyFancyDummyCell::update()\n";
     }
 };
 
@@ -31,17 +47,43 @@ namespace LibGeoDecomp {
 class CellAPITraitsTest : public CxxTest::TestSuite
 {
 public:
-    void testBasic()
+    void setUp()
     {
-        std::cout << "foo-------------------------------------------------------------------------------\n";
-        Coord<2> gridDim(10, 5);
-        Grid<MyDummyCell> gridOld(gridDim);
-        Grid<MyDummyCell> gridNew(gridDim);
-        Streak<2> streak(Coord<2>(0, 0), 4);
-        Coord<2> targetOffset(0, 0);
-        UpdateFunctor<MyDummyCell>()(streak, targetOffset, gridOld, &gridNew, 0);
+        myTestEvents.str("");
+    }
 
-        std::cout << myCellAPITestEvents.str() << "\n";
+    void testBasicCallback1()
+    {
+        Coord<2> gridDim(10, 5);
+        typedef CellAPITraitsFixme::SelectTopology<MySimpleDummyCell>::Value Topology;
+        Grid<MySimpleDummyCell, Topology> gridOld(gridDim);
+        Grid<MySimpleDummyCell, Topology> gridNew(gridDim);
+        Streak<2> streak(Coord<2>(0, 0), 5);
+        Coord<2> targetOffset(0, 0);
+        UpdateFunctor<MySimpleDummyCell>()(streak, targetOffset, gridOld, &gridNew, 0);
+
+        std::string expected;
+        for (int i = 0; i < streak.length(); ++i) {
+            expected += "MySimpleDummyCell::update()\n";
+        }
+        TS_ASSERT_EQUALS(myTestEvents.str(), expected);
+    }
+
+    void testBasicCallback2()
+    {
+        Coord<3> gridDim(10, 5, 10);
+        typedef CellAPITraitsFixme::SelectTopology<MyFancyDummyCell>::Value Topology;
+        Grid<MyFancyDummyCell, Topology> gridOld(gridDim);
+        Grid<MyFancyDummyCell, Topology> gridNew(gridDim);
+        Streak<3> streak(Coord<3>(0, 0, 0), 4);
+        Coord<3> targetOffset(0, 0, 0);
+        UpdateFunctor<MyFancyDummyCell>()(streak, targetOffset, gridOld, &gridNew, 0);
+
+        std::string expected;
+        for (int i = 0; i < streak.length(); ++i) {
+            expected += "MyFancyDummyCell::update()\n";
+        }
+        TS_ASSERT_EQUALS(myTestEvents.str(), expected);
     }
 };
 
