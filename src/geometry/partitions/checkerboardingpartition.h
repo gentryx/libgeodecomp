304109a1a2b2c622dba03959dbfbe534c08802de
Dominik Thoennes <dominik.thoennes@fau.de>
2013-12-22 21:49:40 +0100

    ready version of checkerboardingpartition including tests

--- src/geometry/partitions/checkerboardingpartition.h
+++ src/geometry/partitions/checkerboardingpartition.h
@@ -19,44 +19,12 @@ public:
         dimensions(dimensions)
     {}
 
-    Coord<DIM> getNodeGridDim(const std::size_t totalNodes) const
-    {
-        if (DIM == 2) {
-            long factor = 1;
-            for(long i = 2; i <= sqrt(totalNodes); ++i){
-                if(totalNodes % i == 0){
-                    factor = i;
-                }
-            }
-            return Coord<DIM>(totalNodes/factor,factor);
-        } else if (DIM == 3) {
-            long factorX = 1;
-            long factorY = 1;
-            for(unsigned long i = 2; i <= sqrt(totalNodes); ++i){
-                if(totalNodes % i == 0){
-                    for(unsigned long j = 2; j <= (totalNodes / i); ++j){
-                        if(totalNodes / i % j == 0 && j >= i){
-                            factorX = i;
-                            factorY = j;
-                        }
-                    }
-                }
-            }
-            Coord<DIM> ret = Coord<DIM>::diagonal(1);
-            ret[0] = factorX;
-            ret[1] = factorY;
-            ret[2] = totalNodes/factorX/factorY;
-            return ret;
-        }
-
-    }
-
     Region<DIM> getRegion(const std::size_t node) const
     {
 
         Coord<DIM> nodeGridDim = getNodeGridDim(weights.size());
         Coord<DIM> logicalCoord(node % nodeGridDim.x(),
-                                node / nodeGridDim.x());
+                               (node % (nodeGridDim.x() * nodeGridDim.y()))/ nodeGridDim.x());
         if (DIM > 2){
             logicalCoord[2] = node / (nodeGridDim.x() * nodeGridDim.y());
         }
@@ -67,7 +35,6 @@ public:
             realStart[i] = logicalCoord[i] * dimensions[i] / nodeGridDim[i];
             realEnd[i] = (logicalCoord[i]+1) * dimensions[i] / nodeGridDim[i];
         }
-
         Region<DIM> r;
         r << CoordBox<DIM>(origin + realStart, realEnd - realStart);
 
@@ -75,6 +42,38 @@ public:
     }
 
 private:
+    Coord<DIM> getNodeGridDim(const std::size_t totalNodes) const
+    {
+        Coord<DIM> ret = Coord<DIM>::diagonal(1);
+        if (DIM == 2) {
+            for(long i = 2; i <= sqrt(totalNodes); ++i){
+                if(totalNodes % i == 0){
+                    ret[0] = i;
+                }
+            }
+            ret[1] = totalNodes / ret[0];
+            return ret;
+
+        } else if (DIM == 3) {
+            for(unsigned long i = 2; i <= sqrt(totalNodes); ++i){
+                if(totalNodes % i == 0){
+                    ret[0] = i;
+                    ret[1] = totalNodes/i;
+                    for(unsigned long j = 2; j <= sqrt(totalNodes/ret[0]); ++j){
+                        if(totalNodes / i % j == 0){
+                            ret[0] = i;
+                            ret[1] = j;
+                        }
+                    }
+                }
+            }
+            ret[2] = totalNodes/ret[0]/ret[1];
+            return ret;
+        }
+
+    }
+
+
     using Partition<DIM>::startOffsets;
     using Partition<DIM>::weights;
 
