5f1981cd50d61e87fe93d458a06248754f232fdd
Andreas Schaefer <gentryx@gmx.de>
2013-12-30 02:03:32 +0100

    got rid of an annoying compiler warning caused by nvcc vs. our ancient cxxtest version

--- src/fixtures/testguard.h
+++ src/fixtures/testguard.h
@@ -1,4 +1,11 @@
 #include <libgeodecomp/config.h>
+
+// include MPI header first to skirt troubles with Intel MPI and standard C library
 #ifdef LIBGEODECOMP_FEATURE_MPI
 #include <mpi.h>
+#endif
+
+// nvcc complains about some type traits if we don't define this macro
+#ifdef __CUDACC__
+#define CXXTEST_NO_COPY_CONST
 #endif
