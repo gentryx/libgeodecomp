837ef6f5d0a7d88a1ebe36b3977a8d155034f16f
Dominik Thoennes <dominik.thoennes@fau.de>
2014-02-05 21:47:21 +0100

    beatifull code

--- src/geometry/partitions/scotchpartition.h
+++ src/geometry/partitions/scotchpartition.h
@@ -11,173 +11,181 @@
 
 namespace LibGeoDecomp {
 
-template<int DIM>
-class ScotchPartition : public Partition<DIM>
-{
-public:
-    using Partition<DIM>::startOffsets;
-    using Partition<DIM>::weights;
-
-    inline ScotchPartition(
-        const Coord<DIM>& origin = Coord<DIM>(),
-        const Coord<DIM>& dimensions = Coord<DIM>(),
-        const long& offset = 0,
-        const std::vector<std::size_t>& weights = std::vector<std::size_t>(2)) :
-        Partition<DIM>(offset, weights),
-        origin(origin),
-        dimensions(dimensions)
+    template<int DIM>
+        class ScotchPartition : public Partition<DIM>
         {
-            if(DIM == 3){
-                cellNbr = dimensions[0]
-                    * dimensions[1]
-                    * dimensions[2];
-            } else {
-                cellNbr = dimensions[0]
-                    * dimensions[1];
-            }
-            indices = new SCOTCH_Num[cellNbr];
-            initIndices();
-            regions = new Region<DIM>[weights.size()];
-            createRegions();
-        }
+        public:
+            using Partition<DIM>::startOffsets;
+            using Partition<DIM>::weights;
 
-    Region<DIM> getRegion(const std::size_t node) const
-    {
-        return regions[node];
-    }
-
-private:
-    Coord<DIM> origin;
-    Coord<DIM> dimensions;
-    SCOTCH_Num * indices;
-    SCOTCH_Num cellNbr;
-    Region<DIM> * regions;
-
-    std::vector<std::pair <int,int> > * boxes;
-
-    void initIndices(){
-        SCOTCH_Arch arch;
-        SCOTCH_archInit(&arch);
-        SCOTCH_Num * velotabArch;
-        SCOTCH_Num const vertnbrArch = weights.size();
-        velotabArch = new SCOTCH_Num[weights.size()];
-        for(int i = 0;i<vertnbrArch;++i){
-            velotabArch[i] = weights[i];
-        }
-        SCOTCH_archCmpltw (&arch,vertnbrArch,velotabArch);
-
-        SCOTCH_Strat * straptr = SCOTCH_stratAlloc();;
-        SCOTCH_stratInit(straptr);
-        //SCOTCH_stratGraphMapBuild(straptr,SCOTCH_STRATRECURSIVE,vertnbrArch,0);
-        /*if(SCOTCH_stratGraphMap(straptr,"d")!=0){
-            std::cerr << "stratGraphMap failed" << std::endl;
-            }*/
-
-
-        SCOTCH_Graph grafdat;
-        SCOTCH_graphInit (&grafdat);
-
-        SCOTCH_Num edgenbrGra = 2 * (dimensions[0] * (dimensions[1] - 1) +
-                                           (dimensions[0] - 1) * dimensions[1]);
-
-        if(DIM == 3){
-            edgenbrGra = edgenbrGra
-                * dimensions[2]
-                + 2 * (dimensions[0] * dimensions[1] * (dimensions[2] - 1));
-        }
-
-        SCOTCH_Num * verttabGra;
-        SCOTCH_Num * edgetabGra;
-        verttabGra = new SCOTCH_Num[cellNbr + 1];
-        edgetabGra = new SCOTCH_Num[edgenbrGra];
-
-        int pointer = 0;
-        int xyArea = dimensions[0] * dimensions[1];
-        for(int i = 0;i < cellNbr;++i){
-            verttabGra[i] = pointer;
-            if(i%dimensions[0] != 0){
-                edgetabGra[pointer] = i-1;
-                pointer++;
-            }
-            if(i%dimensions[0] != (dimensions[0]-1)){
-                edgetabGra[pointer] = i+1;
-                pointer++;
-            }
-            if(!((i % xyArea) < dimensions[0])){
-                edgetabGra[pointer] = i - dimensions[0];
-                pointer++;
-            }
-            if(!((i % xyArea) >= dimensions[0] * (dimensions[1]-1))){
-                edgetabGra[pointer] = i + dimensions[0];
-                pointer++;
-            }
-            if(DIM == 3 && i >= (dimensions[0] * dimensions[1])){
-                edgetabGra[pointer] = i - (dimensions[0] * dimensions[1]);
-                pointer++;
-            }
-            if(DIM == 3 && i < (dimensions[0] * dimensions[1]) * (dimensions[2] - 1)){
-                edgetabGra[pointer] = i + (dimensions[0] * dimensions[1]);
-                pointer++;
-            }
-        }
-        verttabGra[cellNbr] = pointer;
-
-
-        SCOTCH_graphBuild(
-                          &grafdat,
-                          0,
-                          cellNbr,
-                          verttabGra,
-                          verttabGra +1,
-                          NULL,
-                          NULL,
-                          edgenbrGra,
-                          edgetabGra,
-                          NULL
-                          );
-
-        SCOTCH_graphMap (&grafdat,&arch,straptr,indices);
-    }
-
-    void createRegions(){
-        int rank = indices[0];
-        int length = 0;
-        int start = 0;
-        for(int i = 1;i < cellNbr+1;++i){
-            if(rank == indices[i] && i < cellNbr && i%dimensions[0]!=0){
-                length++;
-            } else {
-                length++;
-                Coord<DIM> startCoord;
-                Coord<DIM> lengthCoord;
-                lengthCoord[0] = length;
-                lengthCoord[1] = 1;
-                if(DIM == 3){
-                    startCoord[0] = origin[0]
-                        + (start % (dimensions[0] * dimensions[1]))
-                        % dimensions[0];
-                    startCoord[1] = origin[1]
-                        + (start % (dimensions[0] * dimensions[1]))
-                        / dimensions[0];
-                    startCoord[2] = origin[2] + start
-                        / (dimensions[0]
-                           * dimensions[1]);
-                    lengthCoord[2] = 1;
-                } else {
-                    startCoord[0] = origin[0] + start%dimensions[0];
-                    startCoord[1] = origin[1] + start/dimensions[0];
+            inline ScotchPartition(
+                const Coord<DIM>& origin = Coord<DIM>(),
+                const Coord<DIM>& dimensions = Coord<DIM>(),
+                const long& offset = 0,
+                const std::vector<std::size_t>& weights = std::vector<std::size_t>(2)) :
+                Partition<DIM>(offset, weights),
+                origin(origin),
+                dimensions(dimensions)
+                {
+                    if(DIM == 3){
+                        cellNbr = dimensions[0] *
+                            dimensions[1] *
+                            dimensions[2];
+                    } else {
+                        cellNbr = dimensions[0] *
+                            dimensions[1];
+                    }
+                    indices = new SCOTCH_Num[cellNbr];
+                    initIndices();
+                    regions = new Region<DIM>[weights.size()];
+                    createRegions();
                 }
 
+            Region<DIM> getRegion(const std::size_t node) const
+                {
+                    return regions[node];
+                }
 
-                regions[rank] <<
-                    CoordBox<DIM>(startCoord,lengthCoord);
-                rank = indices[i];
-                start = i;
-                length = 0;
+        private:
+            Coord<DIM> origin;
+            Coord<DIM> dimensions;
+            SCOTCH_Num *indices;
+            SCOTCH_Num cellNbr;
+            Region<DIM> * regions;
+
+            std::vector<std::pair <int,int> > * boxes;
+
+            void initIndices()
+            {
+                SCOTCH_Arch arch;
+                SCOTCH_archInit(&arch);
+                SCOTCH_Num *velotabArch;
+                SCOTCH_Num const vertnbrArch = weights.size();
+                velotabArch = new SCOTCH_Num[weights.size()];
+                for(int i = 0; i < vertnbrArch; ++i){
+                    velotabArch[i] = weights[i];
+                }
+                SCOTCH_archCmpltw(&arch, vertnbrArch, velotabArch);
+
+                SCOTCH_Strat *straptr = SCOTCH_stratAlloc();;
+                SCOTCH_stratInit(straptr);
+                /*SCOTCH_stratGraphMapBuild(straptr,SCOTCH_STRATRECURSIVE,vertnbrArch,0);
+                if(SCOTCH_stratGraphMap(straptr,"d")!=0){
+                std::cerr << "stratGraphMap failed" << std::endl;
+                }*/
+
+
+                SCOTCH_Graph grafdat;
+                SCOTCH_graphInit (&grafdat);
+
+                SCOTCH_Num edgenbrGra = 2 * (dimensions[0] *
+                                             (dimensions[1] - 1) +
+                                             (dimensions[0] - 1) *
+                                             dimensions[1]);
+
+                if(DIM == 3){
+                    edgenbrGra = edgenbrGra
+                        * dimensions[2]
+                        + 2 * (dimensions[0] *
+                               dimensions[1] *
+                               (dimensions[2] - 1));
+                }
+
+                SCOTCH_Num *verttabGra;
+                SCOTCH_Num *edgetabGra;
+                verttabGra = new SCOTCH_Num[cellNbr + 1];
+                edgetabGra = new SCOTCH_Num[edgenbrGra];
+
+                int pointer = 0;
+                int xyArea = dimensions[0] * dimensions[1];
+                for(int i = 0; i < cellNbr; ++i){
+                    verttabGra[i] = pointer;
+                    if(i%dimensions[0] != 0){
+                        edgetabGra[pointer] = i-1;
+                        pointer++;
+                    }
+                    if(i%dimensions[0] != (dimensions[0] - 1)){
+                        edgetabGra[pointer] = i+1;
+                        pointer++;
+                    }
+                    if(!((i % xyArea) < dimensions[0])){
+                        edgetabGra[pointer] = i - dimensions[0];
+                        pointer++;
+                    }
+                    if(!((i % xyArea) >= dimensions[0] * (dimensions[1] - 1))){
+                        edgetabGra[pointer] = i + dimensions[0];
+                        pointer++;
+                    }
+                    if(DIM == 3 && i >= (dimensions[0] * dimensions[1])){
+                        edgetabGra[pointer] = i - (dimensions[0] * dimensions[1]);
+                        pointer++;
+                    }
+                    if(DIM == 3 && i < (dimensions[0] * dimensions[1]) * (dimensions[2] - 1)){
+                        edgetabGra[pointer] = i + (dimensions[0] * dimensions[1]);
+                        pointer++;
+                    }
+                }
+                verttabGra[cellNbr] = pointer;
+
+                SCOTCH_graphBuild(
+                                  &grafdat,
+                                  0,
+                                  cellNbr,
+                                  verttabGra,
+                                  verttabGra +1,
+                                  NULL,
+                                  NULL,
+                                  edgenbrGra,
+                                  edgetabGra,
+                                  NULL
+                                  );
+
+                SCOTCH_graphMap(&grafdat, &arch, straptr, indices);
             }
-        }
-    }
- };
+
+            void createRegions()
+            {
+                int rank = indices[0];
+                int length = 0;
+                int start = 0;
+                for(int i = 1; i < cellNbr + 1; ++i){
+                    if((rank == indices[i]) &&
+                       (i < cellNbr) &&
+                       (i % dimensions[0] != 0)){
+                        length++;
+                    } else {
+                        length++;
+                        Coord<DIM> startCoord;
+                        Coord<DIM> lengthCoord;
+                        lengthCoord[0] = length;
+                        lengthCoord[1] = 1;
+                        if(DIM == 3){
+                            startCoord[0] = origin[0] +
+                                (start %
+                                 (dimensions[0] * dimensions[1])) %
+                                dimensions[0];
+                            startCoord[1] = origin[1] +
+                                (start % (dimensions[0] * dimensions[1])) /
+                                dimensions[0];
+                            startCoord[2] = origin[2] + start /
+                                (dimensions[0] *
+                                 dimensions[1]);
+                            lengthCoord[2] = 1;
+                        } else {
+                            startCoord[0] = origin[0] + start % dimensions[0];
+                            startCoord[1] = origin[1] + start / dimensions[0];
+                        }
+
+
+                        regions[rank] <<
+                            CoordBox<DIM>(startCoord, lengthCoord);
+                        rank = indices[i];
+                        start = i;
+                        length = 0;
+                    }
+                }
+            }
+        };
 }
 
 #endif
--- src/testbed/partitiontest/main.cpp
+++ src/testbed/partitiontest/main.cpp
@@ -22,22 +22,28 @@ int main(int argc, char **argv)
 
     MPI_Init(&argc, &argv);
     Coord<3> origin(0,0);
-    Coord<3> dimensions(120,512,100);
+    Coord<3> dimensions(120,128,100);
     std::vector<std::size_t> weights;
-    weights << 999 << 999 << 120*512*100-999*2;
+    weights << 999 << 999 << 120*128*100-999*2;
     ScotchPartition<3> sp(origin,dimensions, 0, weights);
     PTScotchPartition<3> ptp(origin,dimensions, 0, weights);
     CheckerboardingPartition<3> cp(origin,dimensions, 0, weights);
     ZCurvePartition<3> zp(origin,dimensions, 0, weights);
     StripingPartition<3> stripingP(origin,dimensions, 0, weights);
     RecursiveBisectionPartition<3> rP(origin,dimensions, 0, weights);
+    Region<3> regP;
+    Region<3> regPTS;
+    Region<3> regC;
+    Region<3> regZ;
+    Region<3> regStrip;
+    Region<3> regRec;
     for(unsigned int i = 0;i < weights.size(); ++i){
-        Region<3> regP = sp.getRegion(i);
-        Region<3> regPTS = ptp.getRegion(i);
-        Region<3> regC = cp.getRegion(i);
-        Region<3> regZ = zp.getRegion(i);
-        Region<3> regStrip = stripingP.getRegion(i);
-        Region<3> regRec = rP.getRegion(i);
+        regP = sp.getRegion(i);
+        regPTS = ptp.getRegion(i);
+        regC = cp.getRegion(i);
+        regZ = zp.getRegion(i);
+        regStrip = stripingP.getRegion(i);
+        regRec = rP.getRegion(i);
         std::cout << "Scotch: ";
         print(regP);
         std::cout << "PTScotch";
@@ -51,6 +57,6 @@ int main(int argc, char **argv)
         std::cout << "RecursiveBisection: ";
         print(regRec);
         std::cout << std::endl;
-    }
+        }
     MPI_Finalize();
 }
