d7aa55412a9ff0727f43183f6385e18e87377a2d
Dominik Thoennes <dominik.thoennes@fau.de>
2014-02-09 17:07:23 +0100

    changed partitiontest; removed one test in scotchpartitiontest.h

--- src/testbed/partitiontest/main.cpp
+++ src/testbed/partitiontest/main.cpp
@@ -7,6 +7,7 @@
 #include <libgeodecomp/geometry/partitions/recursivebisectionpartition.h>
 #include <iostream>
 #include <fstream>
+#include <limits.h>
 
 
 using namespace LibGeoDecomp;
@@ -24,6 +25,27 @@ void print(Region<2> reg){
               << ((float)reg.expand(1).size() - reg.size()) / reg.size() <<std::endl;
 }
 
+void output(Partition<3> * part, std::ofstream &output,unsigned int number){
+    Region<3> reg;
+    unsigned int min,max,avg,tmp;
+    min = std::numeric_limits<int>::max();
+    max = 0;
+    avg = 0;
+    for(unsigned int j = 0;j < number; ++j){
+        reg = part->getRegion(j);
+        tmp = reg.expand(1).size() - reg.size();
+        if(tmp < min){
+            min = tmp;
+        }
+        if(tmp > max){
+            max = tmp;
+        }
+        avg += tmp;
+    }
+    avg /= number;
+    output << number << "\t" << min << "\t" << avg << "\t" << max << "\n";
+}
+
 int main(int argc, char **argv)
 {
 
@@ -32,11 +54,13 @@ int main(int argc, char **argv)
     int dim = atoi(argv[1]);
     Coord<3> dimensions(dim,dim,dim);
     std::vector<std::size_t> weights;
-    std::ofstream outputScotch,outputZCurve;
+    std::ofstream outputScotch,outputZCurve,outputRecBi,outputCheck;
     outputScotch.open("outputScotch.txt");
     outputZCurve.open("outputZCurve.txt");
-    for(int i = 4; i <= 200; i+=4){
-        std::cout << i << std::endl;
+    outputRecBi.open("outputRecBi.txt");
+    outputCheck.open("outputCheck.txt");
+    for(int i = 4; i <= 200; i+=2){
+        std::cout << "Round: " << i << std::endl;
         int remain = (dim*dim*dim)%i;
         int weight = (dim*dim*dim)/i;
         for(int j = 1; j <= i; ++j){
@@ -52,44 +76,21 @@ int main(int argc, char **argv)
         std::cout << "sum: " <<  sum << " " << weights.size() << std::endl;
         weights << 10 << 10;
         std::cout << "Scotch" << std::endl;
-        ScotchPartition<3> scotch(origin, dimensions, 0, weights);
-        Region<3> regScotch;
-        int min,max,avg,tmp;
-        min = dim*dim*dim;
-        max = 0;
-        avg = 0;
-        for(unsigned int j = 0;j < weights.size(); ++j){
-            regScotch = scotch.getRegion(j);
-            tmp = regScotch.expand(1).size() - regScotch.size();
-            if(tmp < min){
-                min = tmp;
-            }
-            if(tmp > max){
-                max = tmp;
-            }
-            avg += tmp;
-        }
-        avg /= weights.size();
-        outputScotch << i << "\t" << min << "\t" << avg << "\t" << max << "\n";
+        Partition<3> *scotch = new ScotchPartition<3>(origin, dimensions, 0, weights);
+        output(scotch,outputScotch,weights.size());
+        delete scotch;
+        std::cout << "ZCurve" << std::endl;
+        Partition<3> *zCurve = new ZCurvePartition<3>(origin, dimensions, 0, weights);
+        output(zCurve,outputZCurve,weights.size());
+        delete zCurve;
         std::cout << "RecBi" << std::endl;
-        ZCurvePartition<3> zCurve(origin, dimensions, 0, weights);
-        Region<3> regzCurve;
-        min = dim*dim*dim;
-        max = 0;
-        avg = 0;
-        for(unsigned int j = 0;j < weights.size(); ++j){
-            regzCurve = zCurve.getRegion(j);
-            tmp = regzCurve.expand(1).size() - regzCurve.size();
-            if(tmp < min){
-                min = tmp;
-            }
-            if(tmp > max){
-                max = tmp;
-            }
-            avg += tmp;
-        }
-        avg /= weights.size();
-        outputZCurve << i << "\t" << min << "\t" << avg << "\t" << max << "\n";
+        Partition<3> *recBi = new RecursiveBisectionPartition<3>(origin, dimensions, 0, weights);
+        output(recBi,outputRecBi,weights.size());
+        delete recBi;
+        std::cout << "Checker" << std::endl;
+        Partition<3> *checker = new CheckerboardingPartition<3>(origin, dimensions, 0, weights);
+        output(checker,outputCheck,weights.size());
+        delete checker;
         weights.clear();
     }
     /*ScotchPartition<3> sp(origin,dimensions, 0, weights);
