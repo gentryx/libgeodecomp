d5c85fdeabb65603a0f5b89abfaa5fa70cd3dc6a
Andreas Schaefer <gentryx@gmx.de>
2014-04-25 03:52:52 +0200

    update delegation in MultiContainerCell is now operational

--- src/storage/multicontainercell.h
+++ src/storage/multicontainercell.h
@@ -1,13 +1,67 @@
 #ifndef LIBGEODECOMP_STORAGE_MULTICONTAINERCELL_H
 #define LIBGEODECOMP_STORAGE_MULTICONTAINERCELL_H
 
+#include <libgeodecomp/io/logger.h>
 #include <libgeodecomp/storage/containercell.h>
 #include <boost/preprocessor/seq.hpp>
+
+#define DECLARE_MULTI_NEIGHBORHOOD_ADAPTER_ADAPTER(INDEX, CELL, MEMBER) \
+    class AdapterHelper ## INDEX                                        \
+    {                                                                   \
+    public:                                                             \
+        AdapterHelper ## INDEX(                                         \
+            const NEIGHBORHOOD *hood) :                                 \
+            hood(hood)                                                  \
+        {}                                                              \
+                                                                        \
+        const BOOST_PP_SEQ_ELEM(0, MEMBER)& operator[](                 \
+            const int& id) const                                        \
+        {                                                               \
+            const BOOST_PP_SEQ_ELEM(0, MEMBER) *res =                   \
+                (*hood)[Coord<DIM>()].BOOST_PP_SEQ_ELEM(2, MEMBER)[id]; \
+                                                                        \
+            if (res) {                                                  \
+                return *res;                                            \
+            }                                                           \
+                                                                        \
+            CoordBox<DIM> box(Coord<DIM>::diagonal(-1),                 \
+                              Coord<DIM>::diagonal(3));                 \
+            for (typename CoordBox<DIM>::Iterator i = box.begin();      \
+                 i != box.end();                                        \
+                 ++i) {                                                 \
+                                                                        \
+                if (*i != Coord<DIM>()) {                               \
+                    res = (*hood)[*i].BOOST_PP_SEQ_ELEM(2, MEMBER)[id]; \
+                    if (res) {                                          \
+                        return *res;                                    \
+                    }                                                   \
+                }                                                       \
+            }                                                           \
+                                                                        \
+            LOG(ERROR, "could not find id " << id                       \
+                << " in neighborhood AdapterHelper" << INDEX);          \
+            throw std::logic_error("id not found");                     \
+        }                                                               \
+                                                                        \
+    private:                                                            \
+        const NEIGHBORHOOD *hood;                                       \
+                                                                        \
+    };
+
+#define DECLARE_MULTI_NEIGHBORHOOD_ADAPTER_MEMBER(INDEX, CELL, MEMBER)  \
+    AdapterHelper ## INDEX                                              \
+        BOOST_PP_SEQ_ELEM(2, MEMBER);
+
+#define DECLARE_MULTI_NEIGHBORHOOD_ADAPTER_INIT(INDEX, CELL, MEMBER)    \
+    BOOST_PP_SEQ_ELEM(2, MEMBER)(hood),
 
 #define DECLARE_MULTI_CONTAINER_CELL_MEMBER(INDEX, CELL, MEMBER)        \
     LibGeoDecomp::ContainerCell<BOOST_PP_SEQ_ELEM(0, MEMBER),           \
                                 BOOST_PP_SEQ_ELEM(1, MEMBER)>           \
         BOOST_PP_SEQ_ELEM(2, MEMBER);
+
+#define DECLARE_MULTI_CONTAINER_CELL_UPDATE(INDEX, CELL, MEMBER)        \
+    BOOST_PP_SEQ_ELEM(2, MEMBER).updateCargo(multiHood, nanoStep);
 
 /**
  * This cell is a wrapper around ContainerCell to allow user code to
@@ -18,17 +72,59 @@
  *
  * See the unit tests for examples of how to use this class.
  */
-#define DECLARE_MULTI_CONTAINER_CELL(NAME, MEMBERS)     \
-    class NAME                                          \
-    {                                                   \
-    public:                                             \
-    friend class MultiContainerCellTest;                \
-                                                        \
-    BOOST_PP_SEQ_FOR_EACH(                              \
-        DECLARE_MULTI_CONTAINER_CELL_MEMBER,            \
-        NAME,                                           \
-        MEMBERS);                                       \
-                                                        \
+#define DECLARE_MULTI_CONTAINER_CELL(NAME, MEMBERS)                     \
+    class NAME                                                          \
+    {                                                                   \
+    public:                                                             \
+        friend class MultiContainerCellTest;                            \
+                                                                        \
+        typedef typename APITraits::SelectTopology<NAME>::Value Topology; \
+        const static int DIM = Topology::DIM;                           \
+                                                                        \
+        template<typename NEIGHBORHOOD>                                 \
+        class MultiNeighborhoodAdapter                                  \
+        {                                                               \
+        public:                                                         \
+                                                                        \
+            BOOST_PP_SEQ_FOR_EACH(                                      \
+                DECLARE_MULTI_NEIGHBORHOOD_ADAPTER_ADAPTER,             \
+                NAME,                                                   \
+                MEMBERS)                                                \
+                                                                        \
+            MultiNeighborhoodAdapter(const NEIGHBORHOOD *hood) :        \
+                BOOST_PP_SEQ_FOR_EACH(                                  \
+                    DECLARE_MULTI_NEIGHBORHOOD_ADAPTER_INIT,            \
+                    NAME,                                               \
+                    MEMBERS)                                            \
+                hood(hood)                                              \
+            {}                                                          \
+                                                                        \
+            BOOST_PP_SEQ_FOR_EACH(                                      \
+                DECLARE_MULTI_NEIGHBORHOOD_ADAPTER_MEMBER,              \
+                NAME,                                                   \
+                MEMBERS)                                                \
+                                                                        \
+            const NEIGHBORHOOD *hood;                                   \
+        };                                                              \
+                                                                        \
+        BOOST_PP_SEQ_FOR_EACH(                                          \
+            DECLARE_MULTI_CONTAINER_CELL_MEMBER,                        \
+            NAME,                                                       \
+            MEMBERS)                                                    \
+                                                                        \
+        template<class NEIGHBORHOOD>                                    \
+        inline void update(const NEIGHBORHOOD& hood,                    \
+                           const int& nanoStep)                         \
+        {                                                               \
+            *this = hood[Coord<DIM>()];                                 \
+            MultiNeighborhoodAdapter<NEIGHBORHOOD> multiHood(&hood);    \
+                                                                        \
+            BOOST_PP_SEQ_FOR_EACH(                                      \
+                DECLARE_MULTI_CONTAINER_CELL_UPDATE,                    \
+                NAME,                                                   \
+                MEMBERS)                                                \
+                                                                        \
+        }                                                               \
     };
 
 #endif
--- src/storage/test/unit/multicontainercelltest.h
+++ src/storage/test/unit/multicontainercelltest.h
@@ -1,4 +1,6 @@
+#include <libgeodecomp/storage/grid.h>
 #include <libgeodecomp/storage/multicontainercell.h>
+#include <libgeodecomp/storage/updatefunctor.h>
 
 #include <cxxtest/TestSuite.h>
 
@@ -9,6 +11,31 @@ namespace LibGeoDecomp {
 DECLARE_MULTI_CONTAINER_CELL(DummyContainer,            \
                              ((std::string)(5)(labels)) \
                              ((double)(7)(prices))      \
+                             )
+
+class SimpleNode
+{
+public:
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        std::cout << "SimpleNode::update()\n";
+    }
+};
+
+class SimpleElement
+{
+public:
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, int nanoStep)
+    {
+        std::cout << "SimpleElement::update()\n";
+    }
+};
+
+DECLARE_MULTI_CONTAINER_CELL(SimpleContainer,                   \
+                             ((SimpleNode)(30)(nodes))          \
+                             ((SimpleElement)(10)(elements))    \
                              )
 
 class MultiContainerCellTest : public CxxTest::TestSuite
@@ -37,6 +64,29 @@ public:
         TS_ASSERT_EQUALS(*cell.prices[11], -0.11);
         TS_ASSERT_EQUALS(*cell.prices[12], -0.12);
         TS_ASSERT_EQUALS(*cell.prices[13], -0.13);
+    }
+
+    void testUpdate()
+    {
+        Coord<2> dim(10, 5);
+        Grid<SimpleContainer> gridOld(dim);
+        Grid<SimpleContainer> gridNew(dim);
+
+        SimpleContainer c;
+        c.nodes.insert(1, SimpleNode());
+        c.nodes.insert(5, SimpleNode());
+        gridOld[Coord<2>(3, 4)] = c;
+
+        SimpleContainer d;
+        d.nodes.insert(6, SimpleNode());
+        d.elements.insert(1, SimpleElement());
+        d.elements.insert(7, SimpleElement());
+        d.elements.insert(9, SimpleElement());
+        gridOld[Coord<2>(2, 4)] = d;
+
+        Region<2> region;
+        region << CoordBox<2>(Coord<2>(), dim);
+        UpdateFunctor<SimpleContainer>()(region, Coord<2>(), Coord<2>(), gridOld, &gridNew, 0);
     }
 };
 
