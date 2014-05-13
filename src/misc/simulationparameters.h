7e0a07c8a6a8d7b5f7f50dd2bb768e99b4710141
Andreas Schaefer <gentryx@gmx.de>
2014-05-13 17:28:27 +0200

    added first version of SimulationParameters class

---
+++ src/misc/simulationparameters.h
@@ -0,0 +1,178 @@
+#ifndef LIBGEODECOMP_MISC_SIMULATIONPARAMETERS_H
+#define LIBGEODECOMP_MISC_SIMULATIONPARAMETERS_H
+
+#include <boost/shared_ptr.hpp>
+#include <stdexcept>
+
+namespace LibGeoDecomp {
+
+namespace SimulationParametersHelpers {
+
+class Parameter
+{
+public:
+    virtual ~Parameter()
+    {}
+
+    virtual operator std::string() const
+    {
+        throw std::logic_error("illegal cast to std::string");
+    }
+
+    virtual operator bool() const
+    {
+        throw std::logic_error("illegal cast to bool");
+    }
+
+    virtual operator int() const
+    {
+        throw std::logic_error("illegal cast to int");
+    }
+
+    virtual operator double() const
+    {
+        throw std::logic_error("illegal cast to double");
+    }
+
+
+    virtual void operator=(const std::string& other)
+    {
+        throw std::logic_error("illegal assignment from string");
+    }
+
+    virtual void operator=(const char *other)
+    {
+        *this = std::string(other);
+    }
+
+    virtual void operator=(const bool& other)
+    {
+        throw std::logic_error("illegal assignment from bool");
+    }
+
+    virtual void operator=(const int& other)
+    {
+        throw std::logic_error("illegal assignment from int");
+    }
+
+    virtual void operator=(const double& other)
+    {
+        throw std::logic_error("illegal assignment from double");
+    }
+
+    virtual bool operator==(const std::string& other) const
+    {
+        return false;
+    }
+
+    virtual bool operator==(const char *other) const
+    {
+        return *this == std::string(other);
+    }
+
+    virtual bool operator==(const bool& other) const
+    {
+        return false;
+    }
+
+    virtual bool operator==(const int& other) const
+    {
+        return false;
+    }
+
+    virtual bool operator==(const double& other) const
+    {
+        return false;
+    }
+
+    template<typename OTHER_TYPE>
+    bool operator!=(const OTHER_TYPE& other) const
+    {
+        return !(*this == other);
+    }
+};
+
+template<typename VALUE_TYPE>
+class TypedParameter : public Parameter
+{
+public:
+    TypedParameter(const VALUE_TYPE& current) :
+        current(current)
+    {}
+
+    virtual operator VALUE_TYPE() const
+    {
+        return current;
+    }
+
+    virtual void operator=(const VALUE_TYPE& other)
+    {
+        current = other;
+    }
+
+    virtual bool operator==(const VALUE_TYPE& other) const
+    {
+        return current == other;
+    }
+
+protected:
+    VALUE_TYPE current;
+};
+
+template<typename VALUE_TYPE>
+class Interval : public TypedParameter<VALUE_TYPE>
+{
+public:
+    Interval(const VALUE_TYPE minimum, const VALUE_TYPE maximum) :
+        TypedParameter<VALUE_TYPE>(minimum),
+        minimum(minimum),
+        maximum(maximum)
+    {}
+
+private:
+    VALUE_TYPE minimum;
+    VALUE_TYPE maximum;
+};
+
+template<typename VALUE_TYPE>
+class DiscreteSet : public TypedParameter<VALUE_TYPE>
+{
+public:
+    DiscreteSet(const std::vector<VALUE_TYPE>& elements) :
+        TypedParameter<VALUE_TYPE>(elements.front()),
+        elements(elements)
+    {}
+
+private:
+    std::vector<VALUE_TYPE> elements;
+};
+
+}
+
+class SimulationParameters
+{
+public:
+    template<typename VALUE_TYPE>
+    void addParameter(const std::string& name, const VALUE_TYPE& minimum, const VALUE_TYPE& maximum)
+    {
+        parameters[name].reset(new SimulationParametersHelpers::Interval<VALUE_TYPE>(minimum, maximum));
+    }
+
+    template<typename VALUE_TYPE>
+    void addParameter(const std::string& name, const std::vector<VALUE_TYPE>& elements)
+    {
+        parameters[name].reset(new SimulationParametersHelpers::DiscreteSet<VALUE_TYPE>(elements));
+    }
+
+    SimulationParametersHelpers::Parameter& operator[](const std::string& name)
+    {
+        return *parameters[name];
+    }
+
+private:
+    std::map<std::string, boost::shared_ptr<SimulationParametersHelpers::Parameter> > parameters;
+};
+
+}
+
+#endif
---
+++ src/misc/test/unit/simulationparameterstest.h
@@ -0,0 +1,114 @@
+#include <libgeodecomp/misc/simulationparameters.h>
+#include <libgeodecomp/misc/stdcontaineroverloads.h>
+#include <cxxtest/TestSuite.h>
+
+using namespace LibGeoDecomp;
+
+namespace LibGeoDecomp {
+
+class SimulationFactoryTest : public CxxTest::TestSuite
+{
+public:
+
+    void testBasic()
+    {
+        SimulationParameters params;
+        params.addParameter("foo", 1, 5);
+
+        std::vector<std::string> set1;
+        std::vector<bool> set2;
+        std::vector<int> set3;
+        std::vector<double> set4;
+
+        set1 << "United we stand"
+             << "Together we can't fall"
+             << "Now and forever"
+             << "I found where I belong";
+
+        set2 << false << true;
+
+        set3 << 9
+             << 3
+             << 7
+             << 1;
+
+        set4 << 3.1
+             << 1.3
+             << 1.5
+             << 2.0
+             << 2.1
+             << 2.2
+             << 3.3
+             << 3.4
+             << 3.5;
+
+        params.addParameter("bar1", set1);
+        params.addParameter("bar2", set2);
+        params.addParameter("bar3", set3);
+        params.addParameter("bar4", set4);
+
+        TS_ASSERT(params["bar1"] != "Now and Forever");
+        TS_ASSERT(params["bar1"] == "United we stand");
+        TS_ASSERT(params["bar1"] != true);
+        TS_ASSERT(params["bar1"] != false);
+        TS_ASSERT(params["bar1"] != 3);
+        TS_ASSERT(params["bar1"] != 9);
+        TS_ASSERT(params["bar1"] != 2.2);
+        TS_ASSERT(params["bar1"] != 3.1);
+
+        TS_ASSERT(params["bar2"] != "Now and Forever");
+        TS_ASSERT(params["bar2"] != "United we stand");
+        TS_ASSERT(params["bar2"] != true);
+        TS_ASSERT(params["bar2"] == false);
+        TS_ASSERT(params["bar2"] != 3);
+        TS_ASSERT(params["bar2"] != 9);
+        TS_ASSERT(params["bar2"] != 2.2);
+        TS_ASSERT(params["bar2"] != 3.1);
+
+        TS_ASSERT(params["bar3"] != "Now and Forever");
+        TS_ASSERT(params["bar3"] != "United we stand");
+        TS_ASSERT(params["bar3"] != true);
+        TS_ASSERT(params["bar3"] != false);
+        TS_ASSERT(params["bar3"] != 3);
+        TS_ASSERT(params["bar3"] == 9);
+        TS_ASSERT(params["bar3"] != 2.2);
+        TS_ASSERT(params["bar3"] != 3.1);
+
+        TS_ASSERT(params["bar4"] != "Now and Forever");
+        TS_ASSERT(params["bar4"] != "United we stand");
+        TS_ASSERT(params["bar4"] != true);
+        TS_ASSERT(params["bar4"] != false);
+        TS_ASSERT(params["bar4"] != 3);
+        TS_ASSERT(params["bar4"] != 9);
+        TS_ASSERT(params["bar4"] != 2.2);
+        TS_ASSERT(params["bar4"] == 3.1);
+
+        params["bar1"] = "Now and Forever";
+        params["bar2"] = true;
+        params["bar3"] = 3;
+        params["bar4"] = 2.2;
+
+        TS_ASSERT(params["bar1"] == "Now and Forever");
+        TS_ASSERT(params["bar1"] != true);
+        TS_ASSERT(params["bar1"] != 3);
+        TS_ASSERT(params["bar1"] != 2.2);
+
+        TS_ASSERT(params["bar2"] != "Now and Forever");
+        TS_ASSERT(params["bar2"] == true);
+        TS_ASSERT(params["bar2"] != 3);
+        TS_ASSERT(params["bar2"] != 2.2);
+
+        TS_ASSERT(params["bar3"] != "Now and Forever");
+        TS_ASSERT(params["bar3"] != true);
+        TS_ASSERT(params["bar3"] == 3);
+        TS_ASSERT(params["bar3"] != 2.2);
+
+        TS_ASSERT(params["bar4"] != "Now and Forever");
+        TS_ASSERT(params["bar4"] != true);
+        TS_ASSERT(params["bar4"] != 3);
+        TS_ASSERT(params["bar4"] == 2.2);
+
+    }
+};
+
+}
