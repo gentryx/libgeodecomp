80a872097cf60574de4ff4f4417d87857e695abd
Andreas Schaefer <gentryx@gmx.de>
2014-05-29 14:24:43 +0200

    forgot to commit tests for new Selector functionality

---
+++ src/io/test/parallel_mpi_2/selectortest.h
@@ -0,0 +1,61 @@
+#include <libgeodecomp/communication/mpilayer.h>
+#include <libgeodecomp/io/selector.h>
+#include <libgeodecomp/misc/apitraits.h>
+#include <libgeodecomp/misc/stdcontaineroverloads.h>
+#include <libgeodecomp/storage/soagrid.h>
+
+using namespace LibGeoDecomp;
+
+namespace LibGeoDecomp {
+
+class FooCell
+{
+public:
+    class API : public APITraits::HasOpaqueMPIDataType<FooCell>
+    {};
+
+    static MPI_Datatype MPIDataType;
+
+    double d;
+    char c;
+    int i;
+};
+
+
+MPI_Datatype FooCell::MPIDataType = MPI_DATATYPE_NULL;
+
+class SelectorTest : public CxxTest::TestSuite
+{
+public:
+
+    void testMPIType()
+    {
+        MPI_Datatype mpiType;
+
+        mpiType = SelectorHelpers::GetMPIDatatype<int>()();
+        TS_ASSERT_EQUALS(mpiType, MPI_INT);
+
+        mpiType = SelectorHelpers::GetMPIDatatype<char>()();
+        TS_ASSERT_EQUALS(mpiType, MPI_CHAR);
+
+        mpiType = SelectorHelpers::GetMPIDatatype<double>()();
+        TS_ASSERT_EQUALS(mpiType, MPI_DOUBLE);
+
+        mpiType = SelectorHelpers::GetMPIDatatype<FooCell>()();
+        TS_ASSERT_EQUALS(mpiType, FooCell::MPIDataType);
+
+        TS_ASSERT_THROWS(SelectorHelpers::GetMPIDatatype<SelectorTest>()(), std::invalid_argument);
+
+        Selector<FooCell> selector1(&FooCell::d, "d");
+        Selector<FooCell> selector2(&FooCell::c, "c");
+
+        mpiType = selector1.mpiDatatype();
+        TS_ASSERT_EQUALS(mpiType, MPI_DOUBLE);
+
+        mpiType = selector2.mpiDatatype();
+        TS_ASSERT_EQUALS(mpiType, MPI_CHAR);
+    }
+
+};
+
+}
