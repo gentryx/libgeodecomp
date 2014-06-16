072de78e07adb558f630092f6fe422a6d2c8e73a
Christopher Bross <none@none>
2014-06-16 12:47:46 +0200

    first version of sell c sigma container
    --HG--
    branch : sellCSigma

--- src/storage/sellcsigmasparsematrixcontainer.h
+++ src/storage/sellcsigmasparsematrixcontainer.h
@@ -5,6 +5,9 @@
 #include <utility>
 #include <assert.h>
 
+#include <iostream>
+
+
 
 
 namespace LibGeoDecomp {
@@ -28,44 +31,63 @@ public:
         int const chunk (row/C);
         int const offset (row%C);
 
+std::cout << "Get row: row="<< row << " chunk=" << chunk << " offset=" << offset << " chunkOffset=" << chunkOffset[chunk] << " Werte=";
         for (int index = chunkOffset[chunk] + offset;
                 index < chunkOffset[chunk+1]; index += C){
             vec.push_back( std::pair<int, VALUETYPE>
                             (column[index], values[index]) );
+std::cout << "(" << vec.back().first << ","<< vec.back().second << "), ";
         }
-
+std::cout << std::endl;
         return vec;
     }
 
+    // Row [0:N-1]; Col [0:N-1]
     void addPoint(int const row , int const col, VALUETYPE value){
         assert(row >= 0 && col >= 0);
 
         int const chunk (row/C);
 
-        if ( row > rowLength.size() ){
-            rowLength.resize(row);
-            chunkLength.resize(chunk);
-            chunkOffset.resize(chunk);
+std::cout << "Add point: row: " << row << " chunk: " << chunk << " col=" << col << " val=" << value;
+
+        if ( row >= rowLength.size() ){
+//std::cout << "rezie row" << std::endl;
+            rowLength.resize(row+1);
+        }
+        if ( chunk >= chunkLength.size() ){
+//std::cout << "rezie chunk" << std::endl;
+            int oldNumberOfChunks = chunkLength.size();
+
+            chunkLength.resize(chunk+1);
+            chunkOffset.resize(chunk+2);
+
+            for( int i = oldNumberOfChunks; i < chunkOffset.size(); ++i ){
+                chunkOffset[i] = values.size();
+            }
         }
 
+std::cout << " row length: " << rowLength[row] << " chunk Length: " << chunkLength[chunk] << " chunkOffset: " << chunkOffset[chunk] << std::endl;
 
         //// Fall 1: Zeile ist nicht die längste in Chunk
         if ( rowLength[row] < chunkLength[chunk] ){
+std::cout << "fall 1";
             std::vector<int>::iterator itCol = column.begin()
                     + chunkOffset[chunk] + row % C;
-            //while ( col > *it && -1 != *it ){}
-            while ( col > *itCol ){
+
+            while ( col > *itCol && -1 != *itCol ){
                 itCol += C;
             }
             assert(col != *itCol); // TODO fehler werfen? überschreiben?
             
             //// Fall 1.a Wert hinten in der Zeile einfügen auf "Auffüllwerte"
             if ( -1 == *itCol ){
+std::cout << "a" << std::endl;
                 values[itCol - column.begin()] = value;
                 *itCol = col;
             }
             else{
-            //// Fall 1.b We)t in die mitte Einfügen
+            //// Fall 1.b Wert in die mitte Einfügen TODO fixen
+std::cout << "b" << std::endl;
                 int index = itCol - column.begin();
 
                 for (int i = index + C; i < chunkOffset[chunk +1]; i+=C){
@@ -84,52 +106,88 @@ public:
         }
         else{
         //// Fall 2: Zeile ist die Längste in Chunk
-            std::vector<int>::iterator itCol = column.begin()
-                    + chunkOffset[chunk] + row % C;
-            std::vector<int>::iterator itColEnd = column.begin()
-                    + chunkOffset[chunk+1] + row % C;
-            while ( col > *itCol && itCol != itColEnd ){
-                itCol += C;
+std::cout << "fall 2";
+
+            int offset = chunkOffset[chunk] + row % C;
+            int offsetEnd = chunkOffset[chunk+1];
+
+            while (offset < offsetEnd && col > column[offset]){
+                offset += C;
             }
-            assert(col != *itCol); // TODO fehler werfen? überschreiben?
+//std::cout << "offset:" << offset << " offsetEnd:" << offsetEnd << std::endl;
+            if (offset >= offsetEnd ){ //TODO schöner mit einem fall schreiben
+std::cout << "a" << std::endl;
+                offset = offsetEnd;
 
-            typename std::vector<VALUETYPE>::iterator itVal = values.begin()
-                    + ( itCol - column.begin() );
+                std::vector<int>::iterator itCol = column.begin() + offset;
+                typename
+                std::vector<VALUETYPE>::iterator itVal = values.begin() + offset;
 
-            int       newCol [] = {col, -1, -1};
-            VALUETYPE newVal [] = {value, VALUETYPE(), VALUETYPE()};
-            //itVal = values.insert(itVal, newVal, newVal+3);
-            //itCol = column.insert(itCol, newCol, newCol+3);
-            itCol = column.insert(itCol, -1);
+                for (int i=C; i > 0; --i){
+                        itCol = column.insert(itCol, -1);               
+                        itVal = values.insert(itVal, VALUETYPE());
+                }
+                *(itCol + (row%C)) = col;
+                *(itVal + (row%C)) = value;
+            }
+            else if (offset < offsetEnd ){
+std::cout << "b" << std::endl;
+                assert(col != column[offset]); //TODO fehler werfen?
 
-            //// fix order
-            int index = itVal - values.begin();
-            int i = 0;
-            while ( index < chunkOffset[chunk + 1] ){
-                values[index] = values[index + C];
-                column[index] = column[index + C];
+                std::vector<int>::iterator itCol = column.begin() + offset;
+                typename
+                std::vector<VALUETYPE>::iterator itVal = values.begin() + offset;
 
-                if (++i < C-1){
+                for (int i=0; i < C; ++i){
+                    itCol = column.insert(itCol, -1);               
+                    itVal = values.insert(itVal, VALUETYPE());
+                }
+                *itVal = value;
+                *itCol = col;
+
+                //// fix order
+                int index = itVal - values.begin();
+                int i = 0;
+                while ( index < chunkOffset[chunk + 1] ){
                     ++index;
+
+                    if (i < C-1){
+                        ++i;
+                    }
+                    else{
+                        i = 0;
+                        continue;
+                    }
+
+                    values[index] = values[index + C];
+                    column[index] = column[index + C];
                 }
-                else{
-                    index += 2;
-                    i = 0;
-                }
-            }
-            for (i = 0; i < C; ++i ){
-                if(i != row % C){
-                    values[index + i] = VALUETYPE();
-                    column[index + i] = -1;
+                for (i = 0; i < C; ++i ){
+                    if(i != row % C){
+                        values[index + i] = VALUETYPE();
+                        column[index + i] = -1;
+                    }
                 }
             }
 
             ++rowLength[row];
             chunkLength[chunk] = rowLength[row];
-            for (int ch = chunk; ch < chunkOffset.size(); ++ch){
+            for (int ch = chunk+1; ch < chunkOffset.size(); ++ch){
                 chunkOffset[ch] += C;
             }
         }
+
+std::cout << "col: ";
+for (int i=0; i<column.size(); ++i){
+    std::cout << column[i] << " ";
+}
+std::cout << std::endl;
+std::cout << "values: ";
+for (int i=0; i<values.size(); ++i){
+    std::cout << values[i] << " ";
+}
+std::cout << std::endl;
+
     }
 
 private:
--- src/storage/test/unit/sellcsigmasparsematrixcontainertest.h
+++ src/storage/test/unit/sellcsigmasparsematrixcontainertest.h
@@ -13,7 +13,7 @@ public:
     // test with a 8x8 diagonal Matrix, C = 1; Sigma = 1
     void testGetRow_one()
     {
-        std::cout << "fooooooooooooooo" << std::endl;
+std::cout << "\n\n\nTEST 1: C=1 Sigma=1 Diagonalmatrix sotiert" <<std::endl;
         SellCSigmaSparseMatrixContainer<int, 1, 1> smc;
 
         /* add a test 8x8 Matrix:
@@ -61,6 +61,91 @@ public:
         std::pair<int, int> pair7 (7,8);
         row7.push_back(pair7);
 
+        TS_ASSERT_EQUALS(
+                smc.getRow(0),
+                row0
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(1),
+                row1
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(2),
+                row2
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(3),
+                row3
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(4),
+                row4
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(5),
+                row5
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(6),
+                row6
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(7),
+                row7
+                );
+    }
+
+    // test with a 8x8 diagonal Matrix, C = 1; Sigma = 1
+    // randome addPoints
+    void testGetRow_two()
+    {
+std::cout << "\n\n\nTEST 2: C=1 Sigma=1 Diagonalmatrix" <<std::endl;
+        SellCSigmaSparseMatrixContainer<int, 1, 1> smc;
+
+        /* add a test 8x8 Matrix:
+         * 1 0 0 0 0 0 0 0
+         * 0 2 0 0 0 0 0 0
+         * 0 0 3 0 0 0 0 0
+         * 0 0 0 4 0 0 0 0
+         * 0 0 0 0 5 0 0 0
+         * 0 0 0 0 0 6 0 0
+         * 0 0 0 0 0 0 7 0
+         * 0 0 0 0 0 0 0 8
+         */
+
+        smc.addPoint (1, 1, 2);
+        smc.addPoint (0, 0, 1);
+        smc.addPoint (6, 6, 7);
+        smc.addPoint (4, 4, 5);
+        smc.addPoint (7, 7, 8);
+        smc.addPoint (2, 2, 3);
+        smc.addPoint (5, 5, 6);
+        smc.addPoint (3, 3, 4);
+
+        std::vector< std::pair<int, int> > row0;
+        std::pair<int, int> pair0 (0,1);
+        row0.push_back(pair0);
+        std::vector< std::pair<int, int> > row1;
+        std::pair<int, int> pair1 (1,2);
+        row1.push_back(pair1);
+        std::vector< std::pair<int, int> > row2;
+        std::pair<int, int> pair2 (2,3);
+        row2.push_back(pair2);
+        std::vector< std::pair<int, int> > row3;
+        std::pair<int, int> pair3 (3,4);
+        row3.push_back(pair3);
+        std::vector< std::pair<int, int> > row4;
+        std::pair<int, int> pair4 (4,5);
+        row4.push_back(pair4);
+        std::vector< std::pair<int, int> > row5;
+        std::pair<int, int> pair5 (5,6);
+        row5.push_back(pair5);
+        std::vector< std::pair<int, int> > row6;
+        std::pair<int, int> pair6 (6,7);
+        row6.push_back(pair6);
+        std::vector< std::pair<int, int> > row7;
+        std::pair<int, int> pair7 (7,8);
+        row7.push_back(pair7);
 
         TS_ASSERT_EQUALS(
                 smc.getRow(0),
@@ -96,52 +181,182 @@ public:
                 );
     }
 
-    // test with a 4x4 Matrix, C = 1; Sigma = 1
-/*    void testGetRow_one()
+    // test with a 8x8 diagonal Matrix, C = 2; Sigma = 1
+    // randome addPoints
+    void testGetRow_three()
     {
-        SellCSigmaSparseMatrixContainer<int, 1, 1> SMC();
+std::cout << "\n\n\nTEST 3: C=2 Sigma=1 Diagonalmatrix" <<std::endl;
 
-         * add a test 8x8 Matrix:
-         *  | 0 1 2 3
-         *--+----------
-         * 0| 1 2 3 0 
-         * 1| 0 0 0 0 
-         * 2| 0 4 5 0 
-         * 3| 6 7 8 9 
-         *
+        int const C (2);
+        int const SIGMA (1);
+        SellCSigmaSparseMatrixContainer<int, C, SIGMA> smc;
 
-        SMC.addPoint (0, 0, 1);
-        SMC.addPoint (0, 1, 2);
-        SMC.addPoint (0, 2, 3);
-        SMC.addPoint (2, 1, 4);
-        SMC.addPoint (2, 2, 5);
-        SMC.addPoint (3, 0, 6);
-        SMC.addPoint (3, 1, 7);
-        SMC.addPoint (3, 2, 8);
-        SMC.addPoint (3, 3, 9);
+        /* add a test 8x8 Matrix:
+         * 1 0 0 0 0 0 0 0
+         * 0 2 0 0 0 0 0 0
+         * 0 0 3 0 0 0 0 0
+         * 0 0 0 4 0 0 0 0
+         * 0 0 0 0 5 0 0 0
+         * 0 0 0 0 0 6 0 0
+         * 0 0 0 0 0 0 7 0
+         * 0 0 0 0 0 0 0 8
+         */
+
+        smc.addPoint (5, 5, 6);
+        smc.addPoint (1, 1, 2);
+        smc.addPoint (4, 4, 5);
+        smc.addPoint (2, 2, 3);
+        smc.addPoint (0, 0, 1);
+        smc.addPoint (6, 6, 7);
+        smc.addPoint (7, 7, 8);
+        smc.addPoint (3, 3, 4);
+
+        std::vector< std::pair<int, int> > row0;
+        std::pair<int, int> pair0 (0,1);
+        row0.push_back(pair0);
+        std::vector< std::pair<int, int> > row1;
+        std::pair<int, int> pair1 (1,2);
+        row1.push_back(pair1);
+        std::vector< std::pair<int, int> > row2;
+        std::pair<int, int> pair2 (2,3);
+        row2.push_back(pair2);
+        std::vector< std::pair<int, int> > row3;
+        std::pair<int, int> pair3 (3,4);
+        row3.push_back(pair3);
+        std::vector< std::pair<int, int> > row4;
+        std::pair<int, int> pair4 (4,5);
+        row4.push_back(pair4);
+        std::vector< std::pair<int, int> > row5;
+        std::pair<int, int> pair5 (5,6);
+        row5.push_back(pair5);
+        std::vector< std::pair<int, int> > row6;
+        std::pair<int, int> pair6 (6,7);
+        row6.push_back(pair6);
+        std::vector< std::pair<int, int> > row7;
+        std::pair<int, int> pair7 (7,8);
+        row7.push_back(pair7);
 
         TS_ASSERT_EQUALS(
-                SMC.getRow(0),
-                std::vector< std::pair<int, int> > ( std::pair<int, int>(0,1) )
+                smc.getRow(0),
+                row0
                 );
         TS_ASSERT_EQUALS(
-                SMC.getRow(1),
-                std::vector< std::pair<int, int> > ( )
+                smc.getRow(1),
+                row1
                 );
         TS_ASSERT_EQUALS(
-                SMC.getRow(2),
-                std::vector< std::pair<int, int> > ( std::pair<int, int>(2,3) )
+                smc.getRow(2),
+                row2
                 );
         TS_ASSERT_EQUALS(
-                SMC.getRow(3),
-                std::vector< std::pair<int, int> > ( std::pair<int, int>(3,4) )
+                smc.getRow(3),
+                row3
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(4),
+                row4
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(5),
+                row5
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(6),
+                row6
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(7),
+                row7
                 );
     }
-*/
 
+    // test with a 8x8 diagonal Matrix, C = 2; Sigma = 1
+    void testGetRow_fore()
+    {
+std::cout << "\n\n\nTEST 4 C=2 Sigma=1 Diagonalmatrix sotiert" <<std::endl;
+        int const C (2);
+        int const SIGMA (1);
+        SellCSigmaSparseMatrixContainer<int, C, SIGMA> smc;
 
+        /* add a test 8x8 Matrix:
+         * 1 0 0 0 0 0 0 0
+         * 0 2 0 0 0 0 0 0
+         * 0 0 3 0 0 0 0 0
+         * 0 0 0 4 0 0 0 0
+         * 0 0 0 0 5 0 0 0
+         * 0 0 0 0 0 6 0 0
+         * 0 0 0 0 0 0 7 0
+         * 0 0 0 0 0 0 0 8
+         */
 
+        smc.addPoint (0, 0, 1);
+        smc.addPoint (1, 1, 2);
+        smc.addPoint (2, 2, 3);
+        smc.addPoint (3, 3, 4);
+        smc.addPoint (4, 4, 5);
+        smc.addPoint (5, 5, 6);
+        smc.addPoint (6, 6, 7);
+        smc.addPoint (7, 7, 8);
 
-;
+        std::vector< std::pair<int, int> > row0;
+        std::pair<int, int> pair0 (0,1);
+        row0.push_back(pair0);
+        std::vector< std::pair<int, int> > row1;
+        std::pair<int, int> pair1 (1,2);
+        row1.push_back(pair1);
+        std::vector< std::pair<int, int> > row2;
+        std::pair<int, int> pair2 (2,3);
+        row2.push_back(pair2);
+        std::vector< std::pair<int, int> > row3;
+        std::pair<int, int> pair3 (3,4);
+        row3.push_back(pair3);
+        std::vector< std::pair<int, int> > row4;
+        std::pair<int, int> pair4 (4,5);
+        row4.push_back(pair4);
+        std::vector< std::pair<int, int> > row5;
+        std::pair<int, int> pair5 (5,6);
+        row5.push_back(pair5);
+        std::vector< std::pair<int, int> > row6;
+        std::pair<int, int> pair6 (6,7);
+        row6.push_back(pair6);
+        std::vector< std::pair<int, int> > row7;
+        std::pair<int, int> pair7 (7,8);
+        row7.push_back(pair7);
+
+        TS_ASSERT_EQUALS(
+                smc.getRow(0),
+                row0
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(1),
+                row1
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(2),
+                row2
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(3),
+                row3
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(4),
+                row4
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(5),
+                row5
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(6),
+                row6
+                );
+        TS_ASSERT_EQUALS(
+                smc.getRow(7),
+                row7
+                );
+    }
+
+};
 
 }
