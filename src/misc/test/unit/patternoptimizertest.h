9cac0f5ae6bc44d14644f644fc79c4d43da8df94
Mathias_Lifebook_E_Series <none@none>
2014-07-13 13:05:03 +0200

    add vergessen

---
+++ src/misc/test/unit/patternoptimizertest.h
@@ -0,0 +1,44 @@
+#include <libgeodecomp/misc/patternoptimizer.h>
+
+
+// Mathias is testing
+#define MATHIAS_DBG
+
+
+using namespace LibGeoDecomp;
+
+namespace LibGeoDecomp {
+
+
+class PatternOptimizerTest : public CxxTest::TestSuite
+{
+public:
+    class GoalFunction : public Optimizer::Evaluator
+    {
+    public:
+        double operator()(SimulationParameters params)
+        {
+            int x = params["x"];
+            int y = params["y"];
+
+            return 1000 - ((x - 5) * (x - 5)) - (y * y);
+        }
+    };
+
+    void testBasic()
+    {
+        SimulationParameters params;
+        params.addParameter("x",   0, 20);
+        params.addParameter("y", -10, 10);
+#ifdef MATHIAS_DBG
+std::cout<< std::endl <<"PatternOptimizer is running!"<< std::endl;
+#endif //MATHIAS_DBG
+        PatternOptimizer optimizer(params);
+        GoalFunction eval;
+        optimizer(5000, eval);
+
+        TS_ASSERT_EQUALS(1000, optimizer.fitness);
+    }
+
+};
+}
