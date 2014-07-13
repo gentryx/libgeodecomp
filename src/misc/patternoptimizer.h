7cb09497b4a6376eccc24135993152bdd193086d
Mathias_Lifebook_E_Series <none@none>
2014-07-13 10:45:39 +0200

    Schrittweite gefixt, konfigurierbare DBG Ausgaben erstellt

--- src/misc/patternoptimizer.h
+++ src/misc/patternoptimizer.h
@@ -5,6 +5,7 @@
 //#include <libgeodecomp/misc/simulationparameters.h>
 #include <cmath>
 
+//#define MATHIAS_DBG
 
 #define STEP_FACTOR 6
 #define MAX_STEPS 8
@@ -24,27 +25,38 @@ private:
 	bool reduceStepwidth();
 	std::vector<SimulationParameters> genPattern(SimulationParameters middle);
 	std::size_t getMaxPos(std::vector<SimulationParameters> pattern, Evaluator& eval);
+	void printPattern(std::vector<SimulationParameters> pattern);
 };
 
 PatternOptimizer::PatternOptimizer(SimulationParameters params):
 	Optimizer(params)//,
 {
 	for(std::size_t i = 0; i < params.size();++i) {
-		// stepwith.size() == minStepwidth.size() == params.size() 
 		double dimsize = Optimizer::params[i].getMax()
 			- Optimizer::params[i].getMin();
-		//stepwidth[i]= dimsize / STEP_FACTOR;// to rounding is parameters job!!!
 		stepwidth.push_back(dimsize/STEP_FACTOR);
-		//minStepwidth[i]=stepwidth[i] / std::pow(2, MAX_STEPS);
-		minStepwidth.push_back(stepwidth[i]);
+		double tmp = stepwidth[i]/std::pow(2,MAX_STEPS);
+		// wenn die minStepwidht kleiner ist als die Granularity 
+		// kann diese nicht mehr als Abbruchkriterium dienen, deshalb 
+		// minStepwidth >= Granularity
+		if(tmp < params[i].getGranularity())
+			tmp=params[i].getGranularity();
+		minStepwidth.push_back(tmp);
 	}
 }
 
 // wenn alle parameter am minimum sind wird false zurueck gegeben
 bool PatternOptimizer::reduceStepwidth() 			{
+#ifdef MATHIAS_DBG
+	std::cout<< "reduceStepwidth:" << std::endl;
+#endif // MATHIAS_DBG	
+
 	bool allWasMin =true;  
 	for(size_t i = 0;i < stepwidth.size(); ++i)
 	{
+#ifdef MATHIAS_DBG
+		std::cout<< "Dimension "<< i << ": " << stepwidth[i];
+#endif // MATHIAS_DBG
 		if(stepwidth[i] <= minStepwidth[i])
 		{
 			stepwidth[i] = minStepwidth[i];
@@ -54,6 +66,9 @@ bool PatternOptimizer::reduceStepwidth() 			{
 		if(stepwidth[i]<= minStepwidth[i])
 			stepwidth[i] = minStepwidth[i];
 		allWasMin = false;
+#ifdef MATHIAS_DBG
+		std::cout<< " --> " << stepwidth[i]<< std::endl;
+#endif // MATHIAS_DBG
 	}
 	return !allWasMin;
 }
@@ -63,9 +78,6 @@ std::vector<SimulationParameters> PatternOptimizer::genPattern(SimulationParamet
 	{
 		std::vector<SimulationParameters> result(middle.size()*2+1);
 		result[0]=middle;
-		for(std::size_t j = 0; j < middle.size(); ++j)
-			std::cout << middle[j].getValue();
-		std::cout << std::endl;
 		for(std::size_t i = 0; i < middle.size(); ++i)
 		{
 			SimulationParameters tmp1(middle),tmp2(middle);
@@ -73,10 +85,6 @@ std::vector<SimulationParameters> PatternOptimizer::genPattern(SimulationParamet
 			tmp2[i]+= (stepwidth[i] * -1.0);
 			result[1+i*2] = tmp1;
 			result[2+i*2] = tmp2;
-			for(std::size_t j = 0; j < middle.size(); ++j)
-				std::cout << " - " << result[1+i*2][j].getValue()<< " " <<result[2+i*2][j].getValue();
-			std::cout<< std::endl;
-		std::cout<< "stepwidth " << stepwidth[i]<< std::endl; 
 		}	
 		return result;
 	}
@@ -87,7 +95,6 @@ std::size_t PatternOptimizer::getMaxPos(std::vector<SimulationParameters> patter
 	double newFitness;
 	for(std::size_t i = 0; i< pattern.size(); ++i)
 	{
-		//std::cout<< "Optimizer::fitness: "<< Optimizer::fitness <<"pattern " << i << " fitnes: " << eval(pattern[i])<< std::endl;
 		newFitness = eval(pattern[i]);
 		if(newFitness>=Optimizer::fitness)
 		{
@@ -107,19 +114,31 @@ void PatternOptimizer::operator()(int steps,Evaluator & eval)
 	for (int k = 0; k< 100; ++k)
 	{
 		pattern = genPattern(middle);
+#ifdef MATHIAS_DBG
+		printPattern(pattern);
+#endif // MATHIAS_DBG
 		std::size_t maxPos = getMaxPos(pattern, eval);
-		if(maxPos == 0)			//center was the Maximum
+		if(maxPos == 0)			// center was the Maximum
 		{
-			std::cout<<"reduceStepwidth" << std::endl;
-			if(reduceStepwidth())	// abort ariterion
-				break;	
-		}else{
-			std::cout<<"select next middle "<< maxPos << std::endl;
-			std::cout<< pattern[maxPos][0].getValue()<<" "<< pattern[maxPos][1].getValue()<< std::endl;
+			if(!reduceStepwidth())	// abort test
+				break;
+		}else{					// do next step
 			middle = pattern[maxPos];
-			std::cout<< middle[0].getValue()<<" "<< middle[1].getValue()<< std::endl;
 		}
 	}
+}
+void PatternOptimizer::printPattern(std::vector<SimulationParameters> pattern){
+	std::cout<< std::endl;
+	for(std::size_t i = 0; i<pattern.size(); ++i){
+		if(i==0)
+			std::cout<< "Middle:		";
+		else
+			std::cout<< "Direction: " << i<< " :	";
+		for(std::size_t j = 0; j < pattern[i].size(); ++j)
+			std::cout <<  pattern[i][j].getValue()<< ", " <<pattern[i][j].getValue() << " - ";
+		std::cout<< std::endl;
+	}
+	std::cout<< std::endl;
 }
 
 
