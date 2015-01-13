3f76f63a04f47e668d3894bde4143157ff9837ed
Andreas Schaefer <gentryx@gmx.de>
2015-01-13 10:42:46 +0100

    updated LibFlatArray

---
+++ lib/libflatarray/test/short_vec_additional_test.cpp
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2014 Andreas Schäfer
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <libflatarray/short_vec.hpp>
