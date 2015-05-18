b17cb7ce1651bc0f6408c776e665b5d27c975010
Kurt Kanzenbach <kurt@kmk-computers.de>
2015-05-18 12:15:59 +0200

    unstructured-grid-test: coding style

--- src/storage/test/unit/unstructuredgridtest.h
+++ src/storage/test/unit/unstructuredgridtest.h
@@ -50,7 +50,8 @@ private:
     int val;
 };
 
-std::ostream& operator<< (std::ostream& out, MyDummyElement const & val){
+std::ostream& operator<< (std::ostream& out, const MyDummyElement& val)
+{
     out << val();
     return out;
 }
@@ -63,46 +64,51 @@ class UnstructuredGridTest : public CxxTest::TestSuite
     UnstructuredGrid<MyDummyElement>* testGrid;
 #endif
 public:
-    void setUp(){
+    void setUp()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
         testGrid = new UnstructuredGrid<MyDummyElement>(Coord<1>(10));
 
-        for (int i=0; i < testGrid->getDimensions().x(); ++i){
+        for (int i = 0; i < testGrid->getDimensions().x(); ++i) {
             (*testGrid)[Coord<1>(i)] = MyDummyElement(i);
         }
 #endif
     }
 
-    void tearDown(){
+    void tearDown()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
         delete testGrid;
 #endif
     }
 
 
-    void testDefaultConstructor(){
+    void testDefaultConstructor()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
         UnstructuredGrid<MyDummyElement> g;
         TS_ASSERT_EQUALS(0, (int)g.getDimensions().x());
 #endif
     }
 
-    void testConstructorDefaultInit(){
+    void testConstructorDefaultInit()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
-        UnstructuredGrid<int> g (Coord<1>(10), 1, -1);
+        UnstructuredGrid<int> g(Coord<1>(10), 1, -1);
         TS_ASSERT_EQUALS(Coord<1>(10), g.getDimensions());
         TS_ASSERT_EQUALS(1,  g[4 ]);
         TS_ASSERT_EQUALS(-1, g[11]);
 #endif
     }
 
-    void testOperatorEqual1(){
+    void testOperatorEqual1()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
         int dim = testGrid->getDimensions().x();
         UnstructuredGrid<MyDummyElement> other =
             UnstructuredGrid<MyDummyElement>(Coord<1>(dim));
 
-        for(int i = 0; i < dim; ++i) {
+        for (int i = 0; i < dim; ++i) {
             other[i] = testGrid->get(Coord<1>(i));
         }
 
@@ -113,47 +119,50 @@ public:
 #endif
     }
 
-    void testAssimentOperator(){
+    void testAssimentOperator()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
         UnstructuredGrid<int> a (Coord<1>(100));
-        for (int i = 0; i<100; i++){
+        for (int i = 0; i < 100; ++i) {
             a[Coord<1>(i)] = i * 200;
         }
 
         UnstructuredGrid<int> b;
         b = a;
 
-        TS_ASSERT_EQUALS(a,a);
-        TS_ASSERT_EQUALS(a,b);
+        TS_ASSERT_EQUALS(a, a);
+        TS_ASSERT_EQUALS(a, b);
 
         b[Coord<1>(55)] = -666;
-        TS_ASSERT_DIFFERS(a,b);
+        TS_ASSERT_DIFFERS(a, b);
 #endif
     }
 
-    void testSetterGetter(){
+    void testSetterGetter()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
-        UnstructuredGrid<int> a (Coord<1>(100));
-        for (int i = 0; i<100; i++){
+        UnstructuredGrid<int> a(Coord<1>(100));
+        for (int i = 0; i < 100; ++i) {
             a[Coord<1>(i)] = i * 200;
         }
 
         UnstructuredGrid<int> b;
         b = a;
 
-        TS_ASSERT_EQUALS(a,a);
-        TS_ASSERT_EQUALS(a,b);
+        TS_ASSERT_EQUALS(a, a);
+        TS_ASSERT_EQUALS(a, b);
 
         b.set(Coord<1>(55),-666);
-        TS_ASSERT_DIFFERS(a,b);
+        TS_ASSERT_DIFFERS(a, b);
 
-        TS_ASSERT_EQUALS(-666,b.get(Coord<1>(55)));
+        TS_ASSERT_EQUALS(-666, b.get(Coord<1>(55)));
 #endif
     }
 
-    void testEdgeCell(){
+    void testEdgeCell()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
-        UnstructuredGrid<int> foo (Coord<1>(100), 1, -1);
+        UnstructuredGrid<int> foo(Coord<1>(100), 1, -1);
         TS_ASSERT_EQUALS(foo.getEdgeElement(), -1);
         TS_ASSERT_EQUALS(foo[Coord<1>(-1)], -1);
 
@@ -167,7 +176,8 @@ public:
 #endif
     }
 
-    void testAdjacencyMatrix(){
+    void testAdjacencyMatrix()
+    {
 #ifdef LIBGEODECOMP_WITH_CPP14
         const int DIM = 128;
         UnstructuredGrid<int, 2, double, 4, 1> *grid =
@@ -177,7 +187,7 @@ public:
         SellCSigmaSparseMatrixContainer<double,4,1> matrix0 (DIM);
         SellCSigmaSparseMatrixContainer<double,4,1> matrix1 (DIM);
 
-        for (int i=0; i<DIM; ++i){
+        for (int i = 0; i < DIM; ++i) {
             grid->set(Coord<1>(i), i);
 
             adjacenc0 [Coord<2>(i,abs(i*57)     %DIM)] = i   /DIM;
