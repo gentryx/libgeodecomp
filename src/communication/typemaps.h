ebb52dcfb70587f1bd95eb67545721dbf10fda83
Andreas Schaefer <gentryx@gmx.de>
2015-05-19 13:16:19 +0200

    regenerated Typemaps, fixed return type of initializeMapsIfUninitialized() to void

--- src/communication/typemaps.h
+++ src/communication/typemaps.h
@@ -67,7 +67,7 @@ public:
     /**
      * Avoids duplicate initialization
      */
-    static inline bool initializeMapsIfUninitialized()
+    static inline void initializeMapsIfUninitialized()
     {
         if (!initialized()) {
             initializeMaps();
