43292869ed008f659b9c861542e5c6b84942f4fd
Kurt Kanzenbach <kurt@kmk-computers.de>
2015-06-01 12:55:49 +0200

    unstructured-soa-nb: added SoA neighborhood for scalar updates due to loop peeling

--- src/storage/unstructuredsoaneighborhood.h
+++ src/storage/unstructuredsoaneighborhood.h
@@ -8,6 +8,7 @@
 #include <libflatarray/soa_accessor.hpp>
 
 #include <libgeodecomp/geometry/coord.h>
+#include <libgeodecomp/storage/unstructuredneighborhood.h>
 #include <libgeodecomp/storage/unstructuredsoagrid.h>
 
 #include <iterator>
@@ -241,6 +242,115 @@ public:
 
     // public cell members
     VALUE_TYPE *sumPtr;
+};
+
+/**
+ * This neighborhood is used in SoA cells in update() function. Update()
+ * may be called due to loop peeling. We cannot use UnstructuredNeighborhood,
+ * since the []-operator is different and grid type differs. SIGMA = 1.
+ */
+template<typename CELL, std::size_t MATRICES = 1,
+         typename VALUE_TYPE = double, int C = 64, int SIGMA = 1>
+class UnstructuredSoAScalarNeighborhood
+{
+private:
+    using Grid = UnstructuredSoAGrid<CELL, MATRICES, VALUE_TYPE, C, SIGMA>;
+    using Iterator = UnstructuredGridHelpers::Iterator<VALUE_TYPE, C, SIGMA>;
+    const Grid& grid;
+    long xOffset;               /**< initial offset for updateLineX function */
+    int currentChunk;           /**< current chunk */
+    int chunkOffset;            /**< offset inside current chunk: 0 <= x < C */
+    int currentMatrixID;        /**< current id for matrices */
+
+    /**
+     * If xOffset is changed, the current chunk and chunkOffset
+     * may change. This function updates the internal data structures
+     * accordingly.
+     *
+     * @param difference amount which is added to xOffset
+     */
+    inline
+    void updateIndices(int difference)
+    {
+        xOffset += difference;
+        const int newChunkOffset = chunkOffset + difference;
+
+        // update chunk and offset, if necessary
+        if (newChunkOffset >= C) {
+            ++currentChunk;
+            chunkOffset = 0;
+            return;
+        }
+
+        chunkOffset += difference;
+    }
+public:
+    inline
+    UnstructuredSoAScalarNeighborhood(const Grid& grid, long startX) :
+        grid(grid),
+        xOffset(startX),
+        currentChunk(startX / C),
+        chunkOffset(startX % C)
+    {}
+
+    inline
+    const CELL operator[](int index) const
+    {
+        return grid[index];
+    }
+
+    inline
+    UnstructuredSoAScalarNeighborhood& operator++()
+    {
+        updateIndices(1);
+        return *this;
+    }
+
+    inline
+    UnstructuredSoAScalarNeighborhood operator++(int)
+    {
+        UnstructuredSoAScalarNeighborhood tmp(*this);
+        operator++();
+        return tmp;
+    }
+
+    inline
+    const long& index() const { return xOffset; }
+
+    inline
+    long& index() { return xOffset; }
+
+    inline
+    UnstructuredSoAScalarNeighborhood& weights()
+    {
+        // default neighborhood is for matrix 0
+        return weights(0);
+    }
+
+    inline
+    UnstructuredSoAScalarNeighborhood& weights(std::size_t matrixID)
+    {
+        currentMatrixID = matrixID;
+
+        return *this;
+    }
+
+    inline
+    Iterator begin() const
+    {
+        const auto& matrix = grid.getAdjacency(currentMatrixID);
+        int index = matrix.chunkOffsetVec()[currentChunk] + chunkOffset;
+        return Iterator(matrix, index);
+    }
+
+    inline
+    const Iterator end() const
+    {
+        const auto& matrix = grid.getAdjacency(currentMatrixID);
+        int index = matrix.chunkOffsetVec()[currentChunk] + chunkOffset;
+        index += C * matrix.rowLengthVec()[xOffset];
+        return Iterator(matrix, index);
+    }
 };
 
 }
