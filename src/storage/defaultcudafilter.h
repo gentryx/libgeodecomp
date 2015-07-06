22f15479f17791a889d603935e46c1fb99acfb7f
Andreas Schaefer <gentryx@gmx.de>
2015-07-06 09:31:43 +0200

    DefaultCUDAFiler now handles all combinations of memory locations (host/device) for AoS correctly

--- src/storage/defaultcudafilter.h
+++ src/storage/defaultcudafilter.h
@@ -2,6 +2,7 @@
 #define LIBGEODECOMP_STORAGE_DEFAULTCUDAFILTER_H
 
 #include <libgeodecomp/config.h>
+#include <libgeodecomp/storage/defaultfilter.h>
 #include <libgeodecomp/storage/filter.h>
 
 #ifdef LIBGEODECOMP_WITH_CUDA
@@ -72,18 +73,12 @@ void runAggregateMemberKernel(
     dim3 blockDim(32, 1, 1);
     CUDAUtil::checkForError();
 
-    std::size_t byteSize = num * sizeof(MEMBER);
-    MEMBER *deviceBuffer;
-    cudaMalloc(&deviceBuffer, byteSize);
-
     aggregateMember<CELL, EXTERNAL, MEMBER><<<gridDim, blockDim>>>(
         source,
-        deviceBuffer,
+        target,
         MemberPointerWrapper<MEMBER CELL:: *>(memberPointer),
         num);
 
-    cudaMemcpy(target, deviceBuffer, byteSize, cudaMemcpyDeviceToHost);
-    cudaFree(deviceBuffer);
     CUDAUtil::checkForError();
 }
 
@@ -98,25 +93,23 @@ void runDistributeMemberKernel(
     dim3 blockDim(32, 1, 1);
     CUDAUtil::checkForError();
 
-    std::size_t byteSize = num * sizeof(MEMBER);
-    MEMBER *deviceBuffer;
-    cudaMalloc(&deviceBuffer, byteSize);
-    cudaMemcpy(deviceBuffer, source, byteSize, cudaMemcpyHostToDevice);
-
     distributeMember<CELL, EXTERNAL, MEMBER><<<gridDim, blockDim>>>(
-        deviceBuffer,
+        source,
         target,
         MemberPointerWrapper<MEMBER CELL:: *>(memberPointer),
         num);
 
-    cudaFree(deviceBuffer);
     CUDAUtil::checkForError();
 }
 
 }
 
 /**
- * CUDA-aware re-implementation of DefaultFilter
+ * CUDA-aware re-implementation of DefaultFilter. Originally this
+ * functionality was to be merged into the DefaultFilter. However, the
+ * DefaultFilter needs to be available in compilation units which are
+ * not handled by NVCC, so conditionally calling kernels there is no
+ * option (no, not even with weak symbols).
  */
 template<typename CELL, typename MEMBER, typename EXTERNAL>
 class DefaultCUDAFilter : public Filter<CELL, MEMBER, EXTERNAL>
@@ -154,7 +147,54 @@ public:
         std::size_t num,
         MEMBER CELL:: *memberPointer)
     {
-        DefaultCUDAFilterHelpers::runDistributeMemberKernel(source, target, memberPointer, num);
+        if ((sourceLocation == MemoryLocation::CUDA_DEVICE) &&
+            (targetLocation == MemoryLocation::CUDA_DEVICE)) {
+            DefaultCUDAFilterHelpers::runDistributeMemberKernel(source, target, memberPointer, num);
+            return;
+        }
+
+        if ((sourceLocation == MemoryLocation::HOST) &&
+            (targetLocation == MemoryLocation::CUDA_DEVICE)) {
+            std::size_t byteSize = num * sizeof(MEMBER);
+            MEMBER *deviceBuffer;
+            cudaMalloc(&deviceBuffer, byteSize);
+            cudaMemcpy(deviceBuffer, source, byteSize, cudaMemcpyHostToDevice);
+
+            DefaultCUDAFilterHelpers::runDistributeMemberKernel(deviceBuffer, target, memberPointer, num);
+            cudaFree(deviceBuffer);
+            return;
+
+        }
+
+        if ((sourceLocation == MemoryLocation::CUDA_DEVICE) &&
+            (targetLocation == MemoryLocation::HOST)) {
+            std::vector<EXTERNAL> hostBuffer(num);
+            cudaMemcpy(&hostBuffer[0], source, num * sizeof(EXTERNAL), cudaMemcpyDeviceToHost);
+
+            DefaultFilter<CELL, MEMBER, EXTERNAL>().copyMemberInImpl(
+                &hostBuffer[0],
+                MemoryLocation::HOST,
+                target,
+                targetLocation,
+                num,
+                memberPointer);
+            return;
+        }
+
+        if ((sourceLocation == MemoryLocation::HOST) &&
+            (targetLocation == MemoryLocation::HOST)) {
+            DefaultFilter<CELL, MEMBER, EXTERNAL>().copyMemberInImpl(
+                source,
+                sourceLocation,
+                target,
+                targetLocation,
+                num,
+                memberPointer);
+            return;
+        }
+
+
+        throw std::logic_error("Unsupported combination of sourceLocation and targetLocation in DefaultCUDAFilter::copyMemberInImpl()");
     }
 
     void copyMemberOutImpl(
@@ -165,7 +205,52 @@ public:
         std::size_t num,
         MEMBER CELL:: *memberPointer)
     {
-        DefaultCUDAFilterHelpers::runAggregateMemberKernel(source, target, memberPointer, num);
+        if ((sourceLocation == MemoryLocation::CUDA_DEVICE) &&
+            (targetLocation == MemoryLocation::CUDA_DEVICE)) {
+            DefaultCUDAFilterHelpers::runAggregateMemberKernel(source, target, memberPointer, num);
+            return;
+        }
+
+        if ((sourceLocation == MemoryLocation::CUDA_DEVICE) &&
+            (targetLocation == MemoryLocation::HOST)) {
+            std::size_t byteSize = num * sizeof(MEMBER);
+            MEMBER *deviceBuffer;
+            cudaMalloc(&deviceBuffer, byteSize);
+
+            DefaultCUDAFilterHelpers::runAggregateMemberKernel(source, deviceBuffer, memberPointer, num);
+
+            cudaMemcpy(target, deviceBuffer, byteSize, cudaMemcpyDeviceToHost);
+            cudaFree(deviceBuffer);
+            return;
+        }
+
+        if ((sourceLocation == MemoryLocation::HOST) &&
+            (targetLocation == MemoryLocation::CUDA_DEVICE)) {
+            std::vector<EXTERNAL> hostBuffer(num);
+            DefaultFilter<CELL, MEMBER, EXTERNAL>().copyMemberOutImpl(
+                source,
+                sourceLocation,
+                &hostBuffer[0],
+                MemoryLocation::HOST,
+                num,
+                memberPointer);
+            cudaMemcpy(target, &hostBuffer[0], num * sizeof(EXTERNAL), cudaMemcpyHostToDevice);
+            return;
+        }
+
+        if ((sourceLocation == MemoryLocation::HOST) &&
+            (targetLocation == MemoryLocation::HOST)) {
+            DefaultFilter<CELL, MEMBER, EXTERNAL>().copyMemberOutImpl(
+                source,
+                sourceLocation,
+                target,
+                targetLocation,
+                num,
+                memberPointer);
+            return;
+        }
+
+        throw std::logic_error("Unsupported combination of sourceLocation and targetLocation in DefaultCUDAFilter::copyMemberOutImpl()");
     }
 };
 
--- src/storage/test/unit/defaultcudafiltertest.h
+++ src/storage/test/unit/defaultcudafiltertest.h
@@ -19,10 +19,10 @@ class DefaultCUDAFilterTest : public CxxTest::TestSuite
 {
 public:
 
-    void testCudaAoSFromHostToDeviceAndFromDeviceToHost()
+    void testCudaAoSWithGridOnDeviceBuffersOnHost()
     {
 #ifdef LIBGEODECOMP_WITH_CUDA
-        // TEST 1: From Host to Device:
+        // TEST 1: Copy Out (Host to Device)
         CUDAArray<TestCell<2> > deviceCellVec(40);
         std::vector<TestCell<2> > hostCellVec(40);
 
@@ -48,7 +48,7 @@ public:
         }
 
 
-        // TEST 2: From Device to Host:
+        // TEST 2: Copy In (Device to Host)
 
         // new data
         for (std::size_t i = 0; i < hostBuffer.size(); ++i) {
@@ -72,17 +72,162 @@ public:
 #endif
     }
 
-    void testCudaAoSFromHostToHost()
+    void testCudaAoSWithGridOnDeviceBuffersOnDevice()
     {
 #ifdef LIBGEODECOMP_WITH_CUDA
-        // fixme
+        // TEST 1: Copy Out (Device to Device)
+        CUDAArray<TestCell<2> > deviceCellVec(1903);
+        std::vector<TestCell<2> > hostCellVec(1903);
+
+        CUDAArray<double> deviceBuffer(1903, -1);
+        std::vector<double> hostBuffer(1903, -1);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            hostCellVec[i].testValue = i + 0.0513;
+        }
+        deviceCellVec.load(&hostCellVec[0]);
+
+        DefaultCUDAFilter<TestCell<2>, double, double> filter;
+
+        filter.copyMemberOutImpl(
+            deviceCellVec.data(),
+            MemoryLocation::CUDA_DEVICE,
+            deviceBuffer.data(),
+            MemoryLocation::CUDA_DEVICE,
+            1903,
+            &TestCell<2>::testValue);
+
+        deviceBuffer.save(&hostBuffer[0]);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            TS_ASSERT_EQUALS(i + 0.0513, hostBuffer[i]);
+        }
+
+        // TEST 2: Copy In (Device to Device)
+
+        // new data
+        for (std::size_t i = 0; i < hostBuffer.size(); ++i) {
+            hostBuffer[i] = 333 + i;
+        }
+
+        deviceBuffer.load(&hostBuffer[0]);
+
+        filter.copyMemberInImpl(
+            deviceBuffer.data(),
+            MemoryLocation::CUDA_DEVICE,
+            deviceCellVec.data(),
+            MemoryLocation::CUDA_DEVICE,
+            1903,
+            &TestCell<2>::testValue);
+
+        deviceCellVec.save(&hostCellVec[0]);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            TS_ASSERT_EQUALS(333 + i, hostCellVec[i].testValue);
+        }
+
 #endif
     }
 
-    void testCudaAoSFromDeviceToDevice()
+    void testCudaAoSWithGridOnHostBuffersOnHost()
     {
 #ifdef LIBGEODECOMP_WITH_CUDA
-        // fixme
+        // TEST 1: Copy Out (Host to Host)
+        std::vector<TestCell<2> > hostCellVec(69);
+
+        std::vector<double> hostBuffer(69, -1);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            hostCellVec[i].testValue = i + 0.666;
+        }
+
+        DefaultCUDAFilter<TestCell<2>, double, double> filter;
+
+        filter.copyMemberOutImpl(
+            hostCellVec.data(),
+            MemoryLocation::HOST,
+            hostBuffer.data(),
+            MemoryLocation::HOST,
+            69,
+            &TestCell<2>::testValue);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            TS_ASSERT_EQUALS(i + 0.666, hostBuffer[i]);
+        }
+
+
+        // TEST 2: Copy In (Host to Host)
+
+        // new data
+        for (std::size_t i = 0; i < hostBuffer.size(); ++i) {
+            hostBuffer[i] = 314 + i;
+        }
+
+        filter.copyMemberInImpl(
+            hostBuffer.data(),
+            MemoryLocation::HOST,
+            hostCellVec.data(),
+            MemoryLocation::HOST,
+            69,
+            &TestCell<2>::testValue);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            TS_ASSERT_EQUALS(314 + i, hostCellVec[i].testValue);
+        }
+
+#endif
+    }
+
+    void testCudaAoSWithGridOnHostBuffersOnDevice()
+    {
+#ifdef LIBGEODECOMP_WITH_CUDA
+        // TEST 1: Copy Out (Host to Device)
+        std::vector<TestCell<2> > hostCellVec(4711);
+
+        CUDAArray<double> deviceBuffer(4711, -1);
+        std::vector<double> hostBuffer(4711, -1);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            hostCellVec[i].testValue = i + 1799;
+        }
+
+        DefaultCUDAFilter<TestCell<2>, double, double> filter;
+
+        filter.copyMemberOutImpl(
+            hostCellVec.data(),
+            MemoryLocation::HOST,
+            deviceBuffer.data(),
+            MemoryLocation::CUDA_DEVICE,
+            4711,
+            &TestCell<2>::testValue);
+
+        deviceBuffer.save(&hostBuffer[0]);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            TS_ASSERT_EQUALS(i + 1799, hostBuffer[i]);
+        }
+
+        // TEST 2: Copy In (Device to Host)
+
+        // new data
+        for (std::size_t i = 0; i < hostBuffer.size(); ++i) {
+            hostBuffer[i] = 4444 + i;
+        }
+
+        deviceBuffer.load(&hostBuffer[0]);
+
+        filter.copyMemberInImpl(
+            deviceBuffer.data(),
+            MemoryLocation::CUDA_DEVICE,
+            hostCellVec.data(),
+            MemoryLocation::HOST,
+            4711,
+            &TestCell<2>::testValue);
+
+        for (std::size_t i = 0; i < hostCellVec.size(); ++i) {
+            TS_ASSERT_EQUALS(4444 + i, hostCellVec[i].testValue);
+        }
+
 #endif
     }
 
