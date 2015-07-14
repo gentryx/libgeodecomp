da15ed752188738f989c05b5350589a5c6ad73b7
Andreas Schaefer <gentryx@gmx.de>
2015-07-14 13:17:48 +0200

    adding generator for HPX Serialization
    --HG--
    rename : tools/typemapgenerator/template_serialization.h => tools/typemapgenerator/template_serialization_boost.h

---
+++ tools/typemapgenerator/template_serialization_hpx.h
@@ -0,0 +1,22 @@
+#ifndef NAMESPACE_GUARDSERIALIZATION_H
+#define NAMESPACE_GUARDSERIALIZATION_H
+
+HEADERS
+
+NAMESPACE_BEGIN
+class Serialization
+{
+public:
+    HPX_SERIALIZATIION_DEFINITIONS
+};
+NAMESPACE_END
+
+namespace hpx {
+namespace serialization {
+
+HPX_NAMESPACE_LINK
+
+}
+}
+
+#endif
---
+++ tools/typemapgenerator/test/unit/hpxgeneratortest.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+load 'hpxgenerator.rb'
+load 'mpiparser.rb'
+
+class TestHPXGenerator < Test::Unit::TestCase
+  def setup
+    @generator = HPXGenerator.new("./")
+    @parser = MPIParser.new("./test/fixtures/doc/xml/")
+    @classes = %w{Wheel Tire Rim Car Label}
+    @resolved_classes, @resolved_parents, @template_params, @class_sortation, @headers =
+      @parser.shallow_resolution(@classes)
+  end
+
+  def test_generate_hpx_serialize_function
+    classes = %w{Car}
+    members = @parser.shallow_resolution(classes)[0]
+
+    actual_def = @generator.generate_hpx_serialize_function(classes[0],
+                                                              members[classes[0]],
+                                                              @resolved_parents[classes[0]],
+                                                              @template_params[classes[0]])
+    expected_def = File.read("./test/fixtures/references/hpxserialize.h")
+
+    assert_equal(expected_def, actual_def)
+  end
+end
