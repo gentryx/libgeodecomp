da7c4a165af37593e0804dbc66ade2a8399605a7
Andreas Schaefer <gentryx@gmx.de>
2015-07-16 14:15:17 +0200

    extended test coverage for TracingBalancer

--- src/loadbalancer/test/parallel_hpx_1/tracingbalancertest.h
+++ src/loadbalancer/test/parallel_hpx_1/tracingbalancertest.h
@@ -49,7 +49,7 @@ class TracingBalancerTest : public CxxTest::TestSuite
 {
 public:
 
-    void testSerialization()
+    void testSerializationByValue()
     {
         std::ostringstream voidStream;
         TracingBalancer balancer1(new HPXSerializationTest::TestBalancer(4711));
@@ -72,6 +72,35 @@ public:
         LoadBalancer::LoadVec loads;
 
         weights2 = balancer2.balance(weights1, loads);
+        TS_ASSERT_EQUALS(weights2.size(), 3);
+        TS_ASSERT_EQUALS(weights2[0], 4711);
+        TS_ASSERT_EQUALS(weights2[1], 47110);
+        TS_ASSERT_EQUALS(weights2[2], 471100);
+    }
+
+    void testSerializationViaSmartPointer()
+    {
+        std::ostringstream voidStream;
+        boost::shared_ptr<LoadBalancer> balancer1(new TracingBalancer(new HPXSerializationTest::TestBalancer(4711)));
+        boost::shared_ptr<LoadBalancer> balancer2(new TracingBalancer(0, voidStream));
+
+        std::vector<char> buffer;
+        hpx::serialization::output_archive outputArchive(buffer);
+
+        outputArchive << balancer1;
+
+        hpx::serialization::input_archive inputArchive(buffer);
+        inputArchive >> balancer2;
+
+        LoadBalancer::WeightVec weights1;
+        weights1 << 1
+                 << 10
+                 << 100;
+
+        LoadBalancer::WeightVec weights2;
+        LoadBalancer::LoadVec loads;
+
+        weights2 = balancer2->balance(weights1, loads);
         TS_ASSERT_EQUALS(weights2.size(), 3);
         TS_ASSERT_EQUALS(weights2[0], 4711);
         TS_ASSERT_EQUALS(weights2[1], 47110);
