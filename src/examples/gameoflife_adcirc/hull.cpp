c957915f16672cd206ce5a1e9934ebbc48b18c24
Andreas Schaefer <gentryx@gmx.de>
2015-07-16 12:41:43 +0200

    coding style

--- src/examples/gameoflife_adcirc/hull.cpp
+++ src/examples/gameoflife_adcirc/hull.cpp
@@ -15,43 +15,48 @@
 #include <libgeodecomp/misc/apitraits.h>
 #include <libgeodecomp/storage/image.h>
 
-
 using namespace LibGeoDecomp;
 
 double cross(const FloatCoord<2> &o, const FloatCoord<2> &a, const FloatCoord<2> &b)
 {
-    return (a[0]-o[0])*(b[1]-o[1])-(a[1]-o[1])*(b[0]-o[0]);
+    return
+        (a[0] - o[0]) * (b[1] - o[1]) -
+        (a[1] - o[1]) * (b[0] - o[0]);
 }
 
 
-bool floatCoordCompare(const FloatCoord<2> &a, const FloatCoord<2> &b)
+bool floatCoordCompare(const FloatCoord<2>& a, const FloatCoord<2>& b)
 {
     return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
 };
 
 std::vector<FloatCoord<2> > convexHull(std::vector<FloatCoord<2> > *points)
 {
-    int k = 0;
+    std::size_t k = 0;
     std::vector<FloatCoord<2> > points_sorted = *points;
-    std::vector<FloatCoord<2> > hull(2*points_sorted.size());
-    int leftMostID=0;
-    
+    std::vector<FloatCoord<2> > hull(2 * points_sorted.size());
+
     std::sort(points_sorted.begin(), points_sorted.end(), floatCoordCompare);
 
-    int n = points_sorted.size();
+    std::size_t n = points_sorted.size();
 
-    for (int i=0; i<n; i++){        
-        while (k >= 2 && cross(hull[k-2], hull[k-1], points_sorted[i]) <= 0) k--;
+    for (std::size_t i = 0; i < n; ++i) {
+        while ((k >= 2) &&
+               cross(hull[k-2], hull[k-1], points_sorted[i]) <= 0) {
+            --k;
+        }
         hull[k++] = points_sorted[i];
     }
 
-    for (int i=n-2, t=k+1; i>=0; i--){
-        while (k>=t && cross(hull[k-2], hull[k-1], points_sorted[i]) <=0 ) k--;
-        hull[k++]=points_sorted[i];
+    for (std::size_t i = n - 2, t = k + 1; i >= 0; --i) {
+        while ((k >= t) &&
+               cross(hull[k-2], hull[k-1], points_sorted[i]) <=0) {
+             --k;
+        }
+        hull[k++] = points_sorted[i];
     }
 
     hull.resize(k);
 
     return hull;
 }
-    
