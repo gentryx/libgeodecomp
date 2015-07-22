d8055e493b662fc71b40cb510d7769c8de8242cf
Andreas Schaefer <gentryx@gmx.de>
2015-07-22 16:09:11 +0200

    added test for serialization of templated child classes of ParallelWriters

--- src/io/test/parallel_hpx_1/parallelwritertest.h
+++ src/io/test/parallel_hpx_1/parallelwritertest.h
@@ -35,7 +35,6 @@ public:
 
     ParallelWriter<double> *clone() const
     {
-        std::cout << "DANGER!\n";
         return 0;
     }
 
@@ -54,8 +53,64 @@ private:
     double cargo;
 };
 
+template<typename CARGO>
+class HumbleTestWriter : public ParallelWriter<CARGO>
+{
+public:
+    template <typename Archive>
+    friend void serialize(Archive & archive, HumbleTestWriter& writer, unsigned);
+
+    friend class LibGeoDecomp::ParallelWriterTest;
+
+    using typename ParallelWriter<CARGO>::GridType;
+    using typename ParallelWriter<CARGO>::RegionType;
+    using typename ParallelWriter<CARGO>::CoordType;
+
+    HumbleTestWriter(std::string prefix = "", unsigned period = 1, double d = 0) :
+        ParallelWriter<CARGO>(prefix, period),
+        cargo(d)
+    {}
+
+    virtual void grid(GridBase<double, 2> *target)
+    {}
+
+    ParallelWriter<CARGO> *clone() const
+    {
+        return 0;
+    }
+
+    void stepFinished(
+        const GridType& grid,
+        const RegionType& validRegion,
+        const CoordType& globalDimensions,
+        unsigned step,
+        WriterEvent event,
+        std::size_t rank,
+        bool lastCall)
+    {}
+
+
+    HPX_SERIALIZATION_POLYMORPHIC_TEMPLATE_SEMIINTRUSIVE(HumbleTestWriter)
+private:
+    double cargo;
+};
+
 template <typename Archive>
 void serialize(Archive & archive, FancyTestWriter& writer, unsigned)
+{
+    archive & hpx::serialization::base_object<ParallelWriter<double> >(writer);
+    archive & writer.cargo;
+}
+
+template <typename Archive, typename Cargo>
+void serialize(Archive & archive, HumbleTestWriter<Cargo>& writer, unsigned)
+{
+    archive & hpx::serialization::base_object<ParallelWriter<Cargo> >(writer);
+    archive & writer.cargo;
+}
+
+template <typename Archive>
+void serialize(Archive & archive, HumbleTestWriter<double>& writer, unsigned)
 {
     archive & hpx::serialization::base_object<ParallelWriter<double> >(writer);
     archive & writer.cargo;
@@ -64,6 +119,12 @@ void serialize(Archive & archive, FancyTestWriter& writer, unsigned)
 }
 
 HPX_SERIALIZATION_REGISTER_CLASS(HPXSerializationTest::FancyTestWriter);
+HPX_SERIALIZATION_REGISTER_CLASS_TEMPLATE(
+    (template <typename CARGO>),
+    (HPXSerializationTest::HumbleTestWriter<CARGO>));
+HPX_TRAITS_NONINTRUSIVE_POLYMORPHIC_TEMPLATE(
+    (template <typename CARGO>),
+    (HPXSerializationTest::HumbleTestWriter<CARGO>));
 
 namespace LibGeoDecomp {
 
@@ -116,6 +177,32 @@ public:
         TS_ASSERT_EQUALS(writer2->getPeriod(), 20);
         TS_ASSERT_EQUALS(dynamic_cast<HPXSerializationTest::FancyTestWriter*>(writer2.get())->cargo, 6644);
     }
+
+    void testSerializationOfTemplatedChildClass()
+    {
+        boost::shared_ptr<LibGeoDecomp::ParallelWriter<double> > writer1(
+            new HPXSerializationTest::HumbleTestWriter<double>("Astrid", 40, 2211));
+
+        boost::shared_ptr<LibGeoDecomp::ParallelWriter<double> > writer2(
+            new HPXSerializationTest::HumbleTestWriter<double>("Higgs",  50, 4433));
+
+        TS_ASSERT_EQUALS(writer2->getPrefix(), "Higgs");
+        TS_ASSERT_EQUALS(writer2->getPeriod(), 50);
+        TS_ASSERT_EQUALS(dynamic_cast<HPXSerializationTest::HumbleTestWriter<double>*>(writer2.get())->cargo, 4433);
+
+        std::vector<char> buffer;
+        hpx::serialization::output_archive outputArchive(buffer);
+
+        outputArchive << writer1;
+
+        hpx::serialization::input_archive inputArchive(buffer);
+        inputArchive >> writer2;
+
+        TS_ASSERT_EQUALS(writer2->getPrefix(), "Astrid");
+        TS_ASSERT_EQUALS(writer2->getPeriod(), 40);
+        TS_ASSERT_EQUALS(dynamic_cast<HPXSerializationTest::HumbleTestWriter<double>*>(writer2.get())->cargo, 2211);
+    }
+
 };
 
 }
