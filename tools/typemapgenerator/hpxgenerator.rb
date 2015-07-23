2e4665aba78c32f3a57b60611831c1d53ac17695
Andreas Schaefer <gentryx@gmx.de>
2015-07-23 08:45:59 +0200

    correct parameter names in TypemapGenerator

--- tools/typemapgenerator/hpxgenerator.rb
+++ tools/typemapgenerator/hpxgenerator.rb
@@ -19,21 +19,20 @@ class HPXGenerator
   def class_registrations(options)
     ret = []
 
-    options.classes.each do |klass|
-      if options.template_parameters[klass].size == 0
-        # if options.is_abstract[klass]
+    options.topological_class_sortation.each do |klass|
+      next if !options.wants_polymorphic_serialization[klass]
+
+      if options.template_params[klass].size == 0
         ret.push "HPX_TRAITS_NONINTRUSIVE_POLYMORPHIC(#{klass});"
-        # else
+        # if !options.is_abstract[klass]
         #   ret.push "HPX_SERIALIZATION_REGISTER_CLASS(#{klass});"
         # end
       else
-        params1 = render_template_params1(options.template_parameters[klass])
-        params2 = render_template_params2(options.template_parameters[klass])
+        params1 = render_template_params1(options.template_params[klass])
+        params2 = render_template_params2(options.template_params[klass])
         ret.push "HPX_TRAITS_NONINTRUSIVE_POLYMORPHIC_TEMPLATE((template <#{params1}>), (#{klass}#{params2}));"
         if !options.is_abstract[klass]
           ret.push "HPX_SERIALIZATION_REGISTER_CLASS_TEMPLATE((template <#{params1}>), (#{klass}#{params2}));"
-        # else
-        #   ret.push "HPX_TRAITS_NONINTRUSIVE_POLYMORPHIC(#{klass});"
         end
       end
     end
@@ -44,8 +43,8 @@ class HPXGenerator
   def class_registrations_source(options)
     ret = []
 
-    options.classes.each do |klass|
-      if options.template_parameters[klass].size == 0
+    options.topological_class_sortation.each do |klass|
+      if options.template_params[klass].size == 0
         if !options.is_abstract[klass]
           ret.push "HPX_SERIALIZATION_REGISTER_CLASS(#{klass})"
         end
