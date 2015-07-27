bf168362308ca716a589cd2fa9270357b0d485f8
Andreas Schaefer <gentryx@gmx.de>
2015-07-28 01:32:34 +0200

    added polymorphic test for PPMWriter

--- src/io/test/parallel_hpx_1/ppmwritertest.h
+++ src/io/test/parallel_hpx_1/ppmwritertest.h
@@ -36,7 +36,7 @@ public:
         hpx::serialization::input_archive inputArchive(buffer);
         inputArchive >> writer2;
 
-        QuickPalette<double> palette = static_cast<
+        QuickPalette<double> palette = dynamic_cast<
             SimpleCellPlotterHelpers::CellToColor<TestCell<2>, double, QuickPalette<double> >*>(
                 &*writer2.plotter.cellPlotter.cellToColor.filter)->palette;
 
@@ -46,6 +46,37 @@ public:
         TS_ASSERT_EQUALS(Color(255, 0, 0), palette[ 1]);
     }
 
+    void testSerializationOfWriterViaSharedPtr()
+    {
+        boost::shared_ptr<Writer<TestCell<2> > > writer1(new PPMWriter<TestCell<2> >(
+            &TestCell<2>::testValue,
+            5.0,
+            6.0,
+            "bingo",
+            4711,
+            Coord<2>(1, 2)));
+
+        boost::shared_ptr<Writer<TestCell<2> > > writer2;
+
+        std::vector<char> buffer;
+        hpx::serialization::output_archive outputArchive(buffer);
+
+        outputArchive << writer1;
+
+        hpx::serialization::input_archive inputArchive(buffer);
+        inputArchive >> writer2;
+
+        PPMWriter<TestCell<2> > *writer3 = dynamic_cast<PPMWriter<TestCell<2> >*>(&*writer2);
+
+        QuickPalette<double> palette = dynamic_cast<
+            SimpleCellPlotterHelpers::CellToColor<TestCell<2>, double, QuickPalette<double> >*>(
+                &*writer3->plotter.cellPlotter.cellToColor.filter)->palette;
+
+        TS_ASSERT_EQUALS(Color::BLACK,     palette[4]);
+        TS_ASSERT_EQUALS(Color::WHITE,     palette[7]);
+        TS_ASSERT_EQUALS(Color(0, 0, 255), palette[5]);
+        TS_ASSERT_EQUALS(Color(255, 0, 0), palette[6]);
+    }
 };
 
 }
