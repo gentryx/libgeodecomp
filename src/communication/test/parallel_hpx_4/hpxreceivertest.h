8d92f956a75bb752bd88a9b01fccfe1835a92e60
Andreas Schaefer <gentryx@gmx.de>
2015-08-26 03:29:38 +0200

    reworking tests

--- src/communication/test/parallel_hpx_4/hpxreceivertest.h
+++ src/communication/test/parallel_hpx_4/hpxreceivertest.h
@@ -3,12 +3,19 @@
 #include <libgeodecomp/communication/hpxreceiver.h>
 #include <libgeodecomp/misc/stringops.h>
 
-typedef LibGeoDecomp::HPXReceiver<std::vector<int> > ReceiverType;
+// fixme: find unified macro for this, or even better: define template magic for this, akin to serialization macros
+typedef LibGeoDecomp::HPXReceiver<std::vector<int> > ReceiverType1;
+typedef LibGeoDecomp::HPXReceiver<hpx::serialization::serialize_buffer<char> > ReceiverType2;
 
-typedef hpx::components::simple_component<ReceiverType> ReceiverTypeComponent;
-HPX_REGISTER_COMPONENT(ReceiverTypeComponent, ReceiverTypeComponentVectorInt );
-typedef ReceiverType::receiveAction ReceiverTypeComponentVectorIntReceiveActionReceiveAction;
-HPX_REGISTER_ACTION(ReceiverTypeComponentVectorIntReceiveActionReceiveAction);
+typedef hpx::components::simple_component<ReceiverType1> ReceiverType1Component;
+HPX_REGISTER_COMPONENT(ReceiverType1Component, ReceiverType1ComponentVectorInt );
+typedef ReceiverType1::receiveAction ReceiverType1ComponentVectorIntReceiveActionReceiveAction;
+HPX_REGISTER_ACTION(ReceiverType1ComponentVectorIntReceiveActionReceiveAction);
+
+typedef hpx::components::simple_component<ReceiverType2> ReceiverType2Component;
+HPX_REGISTER_COMPONENT(ReceiverType2Component, ReceiverType2ComponentVectorInt );
+typedef ReceiverType2::receiveAction ReceiverType2ComponentVectorIntReceiveActionReceiveAction;
+HPX_REGISTER_ACTION(ReceiverType2ComponentVectorIntReceiveActionReceiveAction);
 
 using namespace LibGeoDecomp;
 
@@ -17,27 +24,30 @@ namespace LibGeoDecomp {
 class HPXReceiverTest : public CxxTest::TestSuite
 {
 public:
+    void setUp()
+    {
+        rank = hpx::get_locality_id();
+        size = hpx::find_all_localities().size();
+
+        leftNeighbor  = (rank - 1 + size) % size;
+        rightNeighbor = (rank + 1       ) % size;
+
+        name      = "foo/" + StringOps::itoa(rank);
+        leftName  = "foo/" + StringOps::itoa(leftNeighbor);
+        rightName = "foo/" + StringOps::itoa(rightNeighbor);
+    }
+
     void testBasic()
     {
-        std::size_t rank = hpx::get_locality_id();
-        std::size_t size = hpx::find_all_localities().size();
+        boost::shared_ptr<ReceiverType1> receiver = ReceiverType1::make(name).get();
 
-        std::size_t leftNeighbor  = (rank - 1 + size) % size;
-        std::size_t rightNeighbor = (rank + 1       ) % size;
-
-        std::string name      = "foo/" + StringOps::itoa(rank);
-        std::string leftName  = "foo/" + StringOps::itoa(leftNeighbor);
-        std::string rightName = "foo/" + StringOps::itoa(rightNeighbor);
-
-        boost::shared_ptr<ReceiverType> receiver = ReceiverType::make(name).get();
-
-        hpx::id_type leftID  = ReceiverType::find(leftName ).get();
-        hpx::id_type rightID = ReceiverType::find(rightName).get();
+        hpx::id_type leftID  = ReceiverType1::find(leftName ).get();
+        hpx::id_type rightID = ReceiverType1::find(rightName).get();
 
         std::vector<int> sendVec;
         sendVec << rank;
-        hpx::apply(ReceiverType::receiveAction(), leftID,  10, sendVec);
-        hpx::apply(ReceiverType::receiveAction(), rightID, 11, sendVec);
+        hpx::apply(ReceiverType1::receiveAction(), leftID,  10, sendVec);
+        hpx::apply(ReceiverType1::receiveAction(), rightID, 11, sendVec);
 
         std::vector<int> fromLeft  = receiver->get(11).get();
         std::vector<int> fromRight = receiver->get(10).get();
@@ -47,8 +57,26 @@ public:
         TS_ASSERT_EQUALS(leftNeighbor,  fromLeft[0]);
         TS_ASSERT_EQUALS(rightNeighbor, fromRight[0]);
 
-        hpx::unregister_id_with_basename(name.c_str(), 0);
+        hpx::unregister_with_basename(name, 0);
     }
+
+    void testWithSerializationBuffer()
+    {
+        std::vector<char> sendToLeft( leftNeighbor  + 10, char(leftNeighbor));
+        std::vector<char> sendToRight(rightNeighbor + 10, char(rightNeighbor));
+
+        
+    }
+
+private:
+    std::size_t rank;
+    std::size_t size;
+    std::size_t leftNeighbor;
+    std::size_t rightNeighbor;
+
+    std::string name;
+    std::string leftName;
+    std::string rightName;
 };
 
 }
