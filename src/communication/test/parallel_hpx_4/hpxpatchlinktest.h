dba60fb11321b9090c5dd028c479703b84adcb57
Andreas Schaefer <gentryx@gmx.de>
2015-09-07 11:09:17 +0200

    extending HPXPatchLink test

--- src/communication/test/parallel_hpx_4/hpxpatchlinktest.h
+++ src/communication/test/parallel_hpx_4/hpxpatchlinktest.h
@@ -22,30 +22,42 @@ public:
         Coord<2> dim(30, 20);
         DisplacedGrid<double> grid(CoordBox<2>(Coord<2>(), dim), -1);
         Region<2> sendRegion;
-        Region<2> recvRegion;
+        Region<2> recvRegionLeft;
+        Region<2> recvRegionRight;
         Region<2> gridRegion;
         int timeStep = 10;
 
-        recvRegion << Streak<2>(Coord<2>(0, leftNeighbor), dim.x());
-        sendRegion << Streak<2>(Coord<2>(0, rank),         dim.x());
+        recvRegionLeft  << Streak<2>(Coord<2>(0, leftNeighbor),  dim.x());
+        recvRegionRight << Streak<2>(Coord<2>(0, rightNeighbor), dim.x());
+        sendRegion  << Streak<2>(Coord<2>(0, rank),          dim.x());
         gridRegion << CoordBox<2>(Coord<2>(), dim);
 
         for (auto&& i: sendRegion) {
             grid[i] = rank + timeStep;
         }
 
-        HPXPatchLink<DisplacedGrid<double> >::Provider provider(recvRegion, basename, leftNeighbor, rank);
-        HPXPatchLink<DisplacedGrid<double> >::Accepter accepter(sendRegion, basename, rank, rightNeighbor);
+        HPXPatchLink<DisplacedGrid<double> >::Provider providerLeft( recvRegionLeft,  basename, leftNeighbor,  rank);
+        HPXPatchLink<DisplacedGrid<double> >::Provider providerRight(recvRegionRight, basename, rightNeighbor, rank);
+        HPXPatchLink<DisplacedGrid<double> >::Accepter accepterLeft( sendRegion,      basename, rank, leftNeighbor);
+        HPXPatchLink<DisplacedGrid<double> >::Accepter accepterRight(sendRegion,      basename, rank, rightNeighbor);
 
-        // fixme: extend this to a bi-directional bus
-        provider.charge(10, 30, 2);
-        accepter.charge(10, 30, 2);
+        providerLeft.charge( 10, 30, 2);
+        providerRight.charge(10, 30, 2);
+        accepterLeft.charge( 10, 30, 2);
+        accepterRight.charge(10, 30, 2);
 
-        accepter.put(grid,  gridRegion, 10);
-        provider.get(&grid, gridRegion, 10);
+        accepterLeft.put(  grid, gridRegion, 10);
+        accepterRight.put( grid, gridRegion, 10);
+        providerLeft.get( &grid, gridRegion, 10);
+        providerRight.get(&grid, gridRegion, 10);
 
         double expectedValue = timeStep + leftNeighbor;
-        for (auto&& i: recvRegion) {
+        for (auto&& i: recvRegionLeft) {
+            TS_ASSERT_EQUALS(grid[i], expectedValue);
+        }
+
+        expectedValue = timeStep + rightNeighbor;
+        for (auto&& i: recvRegionRight) {
             TS_ASSERT_EQUALS(grid[i], expectedValue);
         }
         // fixme: test next send/recv
