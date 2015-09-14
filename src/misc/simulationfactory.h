3c632d7914b8a369048e4f810a971a1bac42b986
Mathias Schöll <mathias.schoell@fau.de>
2015-09-14 17:25:41 +0200

    implement the simulationfactory.h
    Some small changes to clone initializer, writers and steerers.
    Insert some loops to add writers and steerers.

--- src/misc/simulationfactory.h
+++ src/misc/simulationfactory.h
@@ -46,12 +46,17 @@ public:
 
     void addWriter(const ParallelWriter<CELL>& writer)
     {
-        parallelWriters.push_back(boost::shared_ptr<ParallelWriter<CELL> >(writer.clone));
+        parallelWriters.push_back(boost::shared_ptr<ParallelWriter<CELL> >(writer.clone()));
     }
 
     void addWriter(const Writer<CELL>& writer)
     {
-        writers.push_back(boost::shared_ptr<Writer<CELL> >(writer.clone));
+        writers.push_back(boost::shared_ptr<Writer<CELL> >(writer.clone()));
+    }
+    
+    void addSteerer(const Steerer<CELL>& steerer)
+    {
+        steerers.push_back(boost::shared_ptr<Steerer<CELL> >(steerer.clone()));
     }
 
     /**
@@ -60,13 +65,13 @@ public:
      */
     Simulator<CELL> *operator()()
     {
-        Simulator<CELL> *sim = buildSimulator(initializer, parameterSet);
+        Simulator<CELL> *sim = buildSimulator(initializer->clone(), parameterSet);
         return sim;
     }
 
     double operator()(const SimulationParameters& params)
     {
-        Simulator<CELL> *sim = buildSimulator(initializer, params);
+        Simulator<CELL> *sim = buildSimulator(initializer->clone(), params);
         Chronometer chrono;
 
         {
@@ -88,13 +93,19 @@ private:
     SimulationParameters parameterSet;
     std::vector<boost::shared_ptr<ParallelWriter<CELL> > > parallelWriters;
     std::vector<boost::shared_ptr<Writer<CELL> > > writers;
+    std::vector<boost::shared_ptr<Steerer<CELL> > > steerers;
 
     Simulator<CELL> *buildSimulator(
         Initializer<CELL> *initializer,
         const SimulationParameters& params) const
     {
-        if (params["Simulator"] == "SerialSimulator") {
-            return new SerialSimulator<CELL>(initializer);
+       if (params["Simulator"] == "SerialSimulator") {
+            SerialSimulator<CELL> *sSim = new SerialSimulator<CELL>(initializer);
+            for (unsigned i = 0; i < writers.size(); ++i)
+                sSim->addWriter(writers[i].get()->clone());
+            for (unsigned i = 0; i < steerers.size(); ++i)
+                sSim->addSteerer(steerers[i].get()->clone());
+            return sSim;
         }
 
         if (params["Simulator"] == "CacheBlockingSimulator") {
@@ -102,18 +113,32 @@ private:
             int wavefrontWidth  = params["WavefrontWidth"];
             int wavefrontHeight = params["WavefrontHeight"];
             Coord<2> wavefrontDim(wavefrontWidth, wavefrontHeight);
-            return new CacheBlockingSimulator<CELL>(initializer, pipelineLength, wavefrontDim);
+            CacheBlockingSimulator<CELL> *cbSim = 
+                new CacheBlockingSimulator<CELL>(
+                    initializer, 
+                    pipelineLength, 
+                    wavefrontDim);
+            for(unsigned i = 0; i < writers.size(); ++i){
+                cbSim->addWriter(writers[i].get()->clone());
+            for (unsigned i = 0; i < steerers.size(); ++i)
+                cbSim->addSteerer(steerers[i].get()->clone());
+            }
+            return cbSim;
         }
 
         //if (params["Simulator"] == "CudaSimulator") {
         //    Coord<3> blockSize(params["BlockDimX"], params["BlockDimY"], params["BlockDimZ"]);
-        //    return new CudaSimulator<CELL>(initializer, blockSize);
+        //    CudaSimulator<CELL> * cSim = new CudaSimulator<CELL>(initializer, blockSize);
+        //    for (unsigned i = 0; i < writers.size(); ++i)
+        //        cSim->addWriter(writers[i].get()->clone());
+        //    for (unsigned i = 0; i < steerers.size(); ++i)
+        //        cSim->addSteerer(steerers[i].get()->clone());
+        //    return cSim;
         //}
 
-        // FIXME: add writers here?
+        // FIXME: Something need to be done with the parallelWriters!
 
         throw std::invalid_argument("unknown Simulator type");
-
     }
 };
 
