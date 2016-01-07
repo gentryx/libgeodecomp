e3ab439e9d3c1bea518b4752c021dccb0f2ef27b
Andreas Schäfer <gentryx@gmx.de>
2016-01-07 23:42:21 +0100

    adding missing HPXReceiver registration for 1D CoordBoxes -- necessary for unstructured codes

--- src/communication/hpxreceiver.cpp
+++ src/communication/hpxreceiver.cpp
@@ -4,6 +4,7 @@
 #include <libgeodecomp/communication/hpxreceiver.h>
 #include <libgeodecomp/geometry/coordbox.h>
 
+typedef LibGeoDecomp::CoordBox<1> CoordBox1;
 typedef LibGeoDecomp::CoordBox<2> CoordBox2;
 typedef LibGeoDecomp::CoordBox<3> CoordBox3;
 
@@ -11,6 +12,7 @@ LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(char)
 LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(double)
 LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(float)
 LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(int)
+LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(CoordBox1)
 LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(CoordBox2)
 LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(CoordBox3)
 #endif
