240490ec34c13d2206e7504623ab5994a112f25c
koonschi <konstantin.kronfeldner@gmail.com>
2016-01-19 12:06:31 +0100

    Added conversion from edge weights to adjacency

--- src/geometry/adjacency.h
+++ src/geometry/adjacency.h
@@ -4,9 +4,41 @@
 #include <map>
 #include <vector>
 
+#include <libgeodecomp/geometry/coord.h>
+
 namespace LibGeoDecomp {
 
 typedef std::map<int, std::vector<int> > Adjacency;
+
+template<typename T>
+Adjacency MakeAdjacency(const std::map<Coord<2>, T> &weights)
+{
+    Adjacency result;
+
+    for(auto &p : weights)
+    {
+        // ptscotch doesn't like edges from nodes to themselves
+        if (p.first.x() == p.first.y()) continue;
+
+        {
+            auto &others = result[p.first.x()];
+            if (std::find(others.begin(), others.end(), p.first.y()) == others.end())
+            {
+                others.push_back(p.first.y());
+            }
+        }
+
+        {
+            auto &others = result[p.first.y()];
+            if (std::find(others.begin(), others.end(), p.first.x()) == others.end())
+            {
+                others.push_back(p.first.x());
+            }
+        }
+    }
+
+    return result;
+}
 
 }
 
