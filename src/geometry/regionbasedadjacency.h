b1a85a70810383b96e1c82f3af645ae36d6eed20
Andreas Schäfer <gentryx@gmx.de>
2016-02-15 12:52:51 +0100

    reverting to header-only RegionBasedAdjacency, which actually works if include order is correct

--- src/geometry/regionbasedadjacency.h
+++ src/geometry/regionbasedadjacency.h
@@ -1,6 +1,7 @@
 #ifndef LIBGEODECOMP_GEOMETRY_REGIONBASEDADJACENCY_H
 #define LIBGEODECOMP_GEOMETRY_REGIONBASEDADJACENCY_H
 
+#include <libgeodecomp/geometry/region.h>
 #include <libgeodecomp/misc/stdcontaineroverloads.h>
 #include <boost/shared_ptr.hpp>
 
@@ -33,36 +34,62 @@ class Region;
 class RegionBasedAdjacency
 {
 public:
-    RegionBasedAdjacency();
-
     /**
      * Insert a single edge (from, to) to the graph
      */
-    void insert(int from, int to);
+    void insert(int from, int to)
+    {
+        region << Coord<2>(to, from);
+    }
 
     /**
      * Insert all pairs (from, x) with x \in to, to the graph. We sort
      * parameter to to ensure fast, linear inserts.
      */
-    void insert(int from, std::vector<int> to);
+    void insert(int from, std::vector<int> to)
+    {
+        std::sort(to.begin(), to.end());
+        Region<2> buf;
+        for (std::vector<int>::const_iterator i = to.begin(); i != to.end(); ++i) {
+            buf << Coord<2>(*i, from);
+        }
+
+        region += buf;
+    }
 
     /**
      * Returns all x \in V with (node, x) \in E.
      */
-    void getNeighbors(int node, std::vector<int> *neighbors) const;
+    void getNeighbors(int node, std::vector<int> *neighbors) const
+    {
+        CoordBox<2> box = region.boundingBox();
+        int minX = box.origin.x();
+        int maxX = minX + box.dimensions.x();
+
+        for (Region<2>::StreakIterator i = region.streakIteratorOnOrAfter(Coord<2>(minX, node));
+             i != region.streakIteratorOnOrAfter(Coord<2>(maxX, node));
+             ++i) {
+            for (int j = i->origin.x(); j < i->endX; ++j) {
+                (*neighbors) << j;
+            }
+        }
+    }
 
     /**
      * Retrieves the number of edges in the adjacency
      */
-    std::size_t size() const;
+    std::size_t size() const
+    {
+        return region.size();
+    }
 
     const Region<2>& getRegion() const
     {
-        return *region;
+        return region;
     }
 
 private:
-    boost::shared_ptr<Region<2> > region;
+    Region<2> region;
 };
 
 }
