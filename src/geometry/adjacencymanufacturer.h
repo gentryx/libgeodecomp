b9e10cdea82c1950678b803369f1b15c7e1f157a
Andreas Schäfer <gentryx@gmx.de>
2016-03-07 23:47:44 +0100

    add helper class to decouple Adjacency creation from Cell type

---
+++ src/geometry/adjacencymanufacturer.h
@@ -0,0 +1,29 @@
+#ifndef LIBGEODECOMP_GEOMETRY_ADJACENCYMANUFACTURER_H
+#define LIBGEODECOMP_GEOMETRY_ADJACENCYMANUFACTURER_H
+
+#include <stdexcept>
+#include <libgeodecomp/geometry/adjacency.h>
+#include <libgeodecomp/geometry/region.h>
+
+namespace LibGeoDecomp {
+
+/**
+ * Wrapper to separate Adjacency creation from the Cell template
+ * parameter type in Initializers.
+ */
+template<int DIM>
+class AdjacencyManufacturer
+{
+public:
+    virtual ~AdjacencyManufacturer()
+    {}
+
+    virtual boost::shared_ptr<Adjacency> getAdjacency(const Region<DIM>& region) const
+    {
+        throw std::logic_error("getAdjacency() should have been implemented");
+    }
+};
+
+}
+
+#endif
