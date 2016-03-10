78fb434264611cd9be99d979d59b819769152275
Andreas Schäfer <gentryx@gmx.de>
2016-03-10 10:49:35 +0100

    add first HPX performance test

--- src/testbed/hpxperformancetests/main.cpp
+++ src/testbed/hpxperformancetests/main.cpp
@@ -6,6 +6,9 @@
 #include <libgeodecomp/loadbalancer/tracingbalancer.h>
 #include <libgeodecomp/parallelization/hpxsimulator.h>
 
+#include <libflatarray/testbed/cpu_benchmark.hpp>
+#include <libflatarray/testbed/evaluate.hpp>
+
 using namespace LibGeoDecomp;
 
 class ChargedParticle
@@ -36,6 +39,7 @@ LIBGEODECOMP_REGISTER_HPX_COMM_TYPE(ChargedParticle)
 class UnstructuredBusyworkCell
 {
 public:
+    static const int ITERATIONS = 10000;
     static const int C = 4;
     static const int SIGMA = 1;
 
@@ -54,12 +58,19 @@ public:
         cImag(0)
     {}
 
+    // template<typename HOOD_NEW, typename HOOD_OLD>
+    // static void updateLineX(HOOD_NEW& hoodNew, int indexEnd, HOOD_OLD& hoodOld, unsigned /* nanoStep */)
+    // {
+    // fixme
+    //     std::cout << "updateLineX\n";
+    // }
+
     template<typename HOOD>
-    void update(const HOOD& hood, int nanoStep)
+    void update(const HOOD& hood, int /* nanoStep */)
     {
         *this = hood[hood.index()];
 
-        for (int i = 0; i < 100; ++i) {
+        for (int i = 0; i < ITERATIONS; ++i) {
             cReal = cReal * cReal - cImag * cImag;
             cImag = 2 * cImag * cReal;
         }
@@ -144,27 +155,89 @@ private:
     }
 };
 
-void runSimulation()
+class HPXBusyworkCellIron : public CPUBenchmark
 {
-    // fixme: we need tests {update, updateLineX} x {AoS, SoA} x {fine-grained parallelism / no fine-grained parallelism}
-    typedef HpxSimulator<UnstructuredBusyworkCell, UnstructuredStripingPartition> SimulatorType;
+public:
+    std::string family()
+    {
+        return "HPXBusyworkCell";
+    }
 
-    int steps = 10000;
-    CellInitializer *init = new CellInitializer(Coord<2>(100, 50), steps);
+    std::string species()
+    {
+        return "iron";
+    }
 
-    SimulatorType sim(
-        init,
-        std::vector<double>(1, 1.0),
-        new TracingBalancer(new OozeBalancer()),
-        10,
-        1);
+    double performance(std::vector<int> rawDim)
+    {
+        Coord<2> dim(rawDim[0], rawDim[1]);
+        int steps = rawDim[2];
 
-    sim.run();
-}
+        typedef HpxSimulator<UnstructuredBusyworkCell, UnstructuredStripingPartition> SimulatorType;
 
-int hpx_main()
+        CellInitializer *init = new CellInitializer(dim, steps);
+
+        SimulatorType sim(
+            init,
+            std::vector<double>(1, 1.0),
+            new TracingBalancer(new OozeBalancer()),
+            steps,
+            1,
+            "hpxperformancetests_HPXBusyworkCellIron");
+
+        double seconds;
+        {
+            ScopedTimer t(&seconds);
+            sim.run();
+        }
+
+        double latticeUpdates = 1.0 * dim.prod() * steps;
+        double flopsPerLatticeUpdate = UnstructuredBusyworkCell::ITERATIONS * 5 + 4 * 4;
+        double gflops = latticeUpdates * flopsPerLatticeUpdate / seconds * 1e-9;
+
+        return gflops;
+    }
+
+    std::string unit()
+    {
+        return "GFLOP/s";
+    }
+};
+
+int hpx_main(int argc, char **argv)
 {
-    runSimulation();
+        // fixme: we need tests {update, updateLineX} x {AoS, SoA} x {fine-grained parallelism / no fine-grained parallelism} x {structured, unstructured} x {HPX, OpenMP, CUDA} x {memory bound, compute bound}
+
+    if ((argc < 3) || (argc == 4) || (argc > 5)) {
+        std::cerr << "usage: " << argv[0] << " [-n,--name SUBSTRING] REVISION CUDA_DEVICE \n"
+                  << "  - optional: only run tests whose name contains a SUBSTRING,\n"
+                  << "  - REVISION is purely for output reasons,\n"
+                  << "  - CUDA_DEVICE causes CUDA tests to run on the device with the given ID.\n";
+        return 1;
+    }
+
+    std::string name = "";
+    int argumentIndex = 1;
+    if (argc == 5) {
+        if ((std::string(argv[1]) == "-n") ||
+            (std::string(argv[1]) == "--name")) {
+            name = std::string(argv[2]);
+        }
+        argumentIndex = 3;
+    }
+    std::string revision = argv[argumentIndex + 0];
+
+    std::stringstream s;
+    s << argv[argumentIndex + 1];
+    int cudaDevice;
+    s >> cudaDevice;
+
+    LibFlatArray::evaluate eval(name, revision);
+    eval.print_header();
+
+    std::vector<int> size;
+    size << 100 << 100 << 100;
+    eval(HPXBusyworkCellIron(), size);
     return hpx::finalize();
 }
 
