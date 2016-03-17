a78a447bab7ee127423ab1c3563238b18631b76d
Andreas Schäfer <gentryx@gmx.de>
2016-03-17 09:00:17 +0100

    add more tests for SerializationBuffer

--- src/storage/test/unit/serializationbuffertest.h
+++ src/storage/test/unit/serializationbuffertest.h
@@ -26,6 +26,40 @@ public:
 
         TS_ASSERT_EQUALS(sizeof(SerializationBufferType::ElementType), sizeof(TestCell<2>));
     }
+
+    void testTestCell3D()
+    {
+        typedef SerializationBuffer<TestCell<3> > SerializationBufferType;
+
+        Region<3> region;
+        region << Streak<3>(Coord<3>(10, 10, 10), 30)
+               << Streak<3>(Coord<3>(15, 11, 10), 50)
+               << Streak<3>(Coord<3>(10, 11, 15), 90);
+
+        SerializationBufferType::BufferType buf = SerializationBufferType::create(region);
+        TS_ASSERT_EQUALS(buf.size(), region.size());
+
+        TS_ASSERT_EQUALS(SerializationBufferType::getData(buf), &buf[0]);
+
+        TS_ASSERT_EQUALS(sizeof(SerializationBufferType::ElementType), sizeof(TestCell<3>));
+    }
+
+    void testTestCellSoA()
+    {
+        typedef SerializationBuffer<TestCellSoA > SerializationBufferType;
+
+        Region<3> region;
+        region << Streak<3>(Coord<3>(10, 10, 10), 30)
+               << Streak<3>(Coord<3>(15, 11, 10), 50)
+               << Streak<3>(Coord<3>(10, 11, 15), 90);
+
+        SerializationBufferType::BufferType buf = SerializationBufferType::create(region);
+        TS_ASSERT_EQUALS(buf.size(), LibFlatArray::aggregated_member_size<TestCellSoA>::VALUE * region.size());
+
+        TS_ASSERT_EQUALS(SerializationBufferType::getData(buf), &buf[0]);
+
+        TS_ASSERT_EQUALS(sizeof(SerializationBufferType::ElementType), sizeof(char));
+    }
 };
 
 }
