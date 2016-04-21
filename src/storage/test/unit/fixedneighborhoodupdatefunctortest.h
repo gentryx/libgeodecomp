6ef1be2ac75cd25ff87f8719a73e5cb7089883f3
Andreas Schäfer <gentryx@gmx.de>
2016-04-21 13:09:02 +0200

    test torus cube with FixedNeighborhoodUpdateFunctor

--- src/storage/test/unit/fixedneighborhoodupdatefunctortest.h
+++ src/storage/test/unit/fixedneighborhoodupdatefunctortest.h
@@ -5,7 +5,7 @@
 
 using namespace LibGeoDecomp;
 
-class MySoATestCellWithTwoDoubles
+class MySoATestCellWithTwoDoublesTorus
 {
 public:
     class API :
@@ -17,7 +17,48 @@ public:
     {};
 
     inline
-    explicit MySoATestCellWithTwoDoubles(const double valA = 0, const double valB = 0) :
+    explicit MySoATestCellWithTwoDoublesTorus(const double valA = 0, const double valB = 0) :
+        valA(valA),
+        valB(valB)
+    {}
+
+    template<typename NEIGHBORHOOD>
+    void update(const NEIGHBORHOOD& hood, const int nanoStep)
+    {}
+
+    template<typename HOOD_OLD, typename HOOD_NEW>
+    static void updateLineX(HOOD_OLD& hoodOld, long indexEnd,
+                            HOOD_NEW& hoodNew, long /* nanoStep */)
+    {
+        for (; hoodOld.index() < indexEnd; hoodOld += 1, hoodNew += 1) {
+            double akku =
+                hoodOld[FixedCoord< 0,  0, -1>()].valA() +
+                hoodOld[FixedCoord< 0, -1,  0>()].valA() +
+                hoodOld[FixedCoord<-1,  0,  0>()].valA() +
+                hoodOld[FixedCoord< 1,  0,  0>()].valA() +
+                hoodOld[FixedCoord< 0,  1,  0>()].valA() +
+                hoodOld[FixedCoord< 0,  0,  1>()].valA();
+            hoodNew.valB() = akku;
+        }
+    }
+
+    double valA;
+    double valB;
+};
+
+class MySoATestCellWithTwoDoublesCube
+{
+public:
+    class API :
+          public LibGeoDecomp::APITraits::HasFixedCoordsOnlyUpdate,
+          public LibGeoDecomp::APITraits::HasUpdateLineX,
+          public LibGeoDecomp::APITraits::HasStencil<LibGeoDecomp::Stencils::Moore<3, 1> >,
+          public LibGeoDecomp::APITraits::HasCubeTopology<3>,
+          public LibGeoDecomp::APITraits::HasSoA
+    {};
+
+    inline
+    explicit MySoATestCellWithTwoDoublesCube(const double valA = 0, const double valB = 0) :
         valA(valA),
         valB(valB)
     {}
@@ -47,7 +88,12 @@ public:
 };
 
 LIBFLATARRAY_REGISTER_SOA(
-    MySoATestCellWithTwoDoubles,
+    MySoATestCellWithTwoDoublesTorus,
+    ((double)(valA))
+    ((double)(valB)))
+
+LIBFLATARRAY_REGISTER_SOA(
+    MySoATestCellWithTwoDoublesCube,
     ((double)(valA))
     ((double)(valB)))
 
@@ -58,17 +104,17 @@ class FixedNeighborhoodUpdateFunctorTest : public CxxTest::TestSuite
 {
 public:
 
-    void testBasic()
+    void testTorus()
     {
         CoordBox<3> box(Coord<3>(10, 20, 30), Coord<3>(200, 100, 50));
 
-        MySoATestCellWithTwoDoubles defaultCell(666, 777);
-        MySoATestCellWithTwoDoubles edgeCell(-1, -1);
-        SoAGrid<MySoATestCellWithTwoDoubles, Topologies::Torus<3>::Topology> gridOld(box, defaultCell, edgeCell);
-        SoAGrid<MySoATestCellWithTwoDoubles, Topologies::Torus<3>::Topology> gridNew(box, defaultCell, edgeCell);
+        MySoATestCellWithTwoDoublesTorus defaultCell(666, 777);
+        MySoATestCellWithTwoDoublesTorus edgeCell(-1, -1);
+        SoAGrid<MySoATestCellWithTwoDoublesTorus, Topologies::Torus<3>::Topology> gridOld(box, defaultCell, edgeCell);
+        SoAGrid<MySoATestCellWithTwoDoublesTorus, Topologies::Torus<3>::Topology> gridNew(box, defaultCell, edgeCell);
 
         for (CoordBox<3>::Iterator i = box.begin(); i != box.end(); ++i) {
-            gridOld.set(*i, MySoATestCellWithTwoDoubles(i->x() + i->y() * 1000.0 + i->z() * 1000 * 1000.0));
+            gridOld.set(*i, MySoATestCellWithTwoDoublesTorus(i->x() + i->y() * 1000.0 + i->z() * 1000 * 1000.0));
         }
 
         Region<3> region;
@@ -86,34 +132,92 @@ public:
         Coord<3> offsetOld = -boxOld.origin;
         Coord<3> offsetNew = -boxNew.origin;
         gridOld.callback(&gridNew, FixedNeighborhoodUpdateFunctor<
-                         MySoATestCellWithTwoDoubles,
+                         MySoATestCellWithTwoDoublesTorus,
                          UpdateFunctorHelpers::ConcurrencyNoP,
                          APITraits::SelectThreadedUpdate<void>::Value>(
                              &region, &offsetOld, &offsetNew, &boxNew.dimensions, 0, 0, 0));
 
         for (Region<3>::Iterator i = region.begin(); i != region.end(); ++i) {
-            MySoATestCellWithTwoDoubles cell = gridNew.get(*i);
+            MySoATestCellWithTwoDoublesTorus cell = gridNew.get(*i);
 
             Coord<3> sum;
-            sum += normalize(*i + Coord<3>( 0,  0, -1), box);
-            sum += normalize(*i + Coord<3>( 0, -1,  0), box);
-            sum += normalize(*i + Coord<3>(-1,  0,  0), box);
-            sum += normalize(*i + Coord<3>( 1,  0,  0), box);
-            sum += normalize(*i + Coord<3>( 0,  1,  0), box);
-            sum += normalize(*i + Coord<3>( 0,  0,  1), box);
+            sum += normalizeTorus(*i + Coord<3>( 0,  0, -1), box);
+            sum += normalizeTorus(*i + Coord<3>( 0, -1,  0), box);
+            sum += normalizeTorus(*i + Coord<3>(-1,  0,  0), box);
+            sum += normalizeTorus(*i + Coord<3>( 1,  0,  0), box);
+            sum += normalizeTorus(*i + Coord<3>( 0,  1,  0), box);
+            sum += normalizeTorus(*i + Coord<3>( 0,  0,  1), box);
 
             double expected = sum.x() + sum.y() * 1000.0 + sum.z() * 1000 * 1000.0;
             TS_ASSERT_EQUALS(cell.valB, expected);
         }
     }
 
-    // fixme: test with cube topo
+    void testCube()
+    {
+        CoordBox<3> box(Coord<3>(10, 20, 30), Coord<3>(200, 100, 50));
+
+        MySoATestCellWithTwoDoublesCube defaultCell1(666, 777);
+        MySoATestCellWithTwoDoublesCube defaultCell2(888, 999);
+        MySoATestCellWithTwoDoublesCube edgeCell(-1001001.0, -1);
+        SoAGrid<MySoATestCellWithTwoDoublesCube, Topologies::Cube<3>::Topology> gridOld(box, defaultCell1, edgeCell);
+        SoAGrid<MySoATestCellWithTwoDoublesCube, Topologies::Cube<3>::Topology> gridNew(box, defaultCell2, edgeCell);
+
+        for (CoordBox<3>::Iterator i = box.begin(); i != box.end(); ++i) {
+            gridOld.set(*i, MySoATestCellWithTwoDoublesCube(i->x() + i->y() * 1000.0 + i->z() * 1000 * 1000.0));
+        }
+
+        Region<3> region;
+        region
+            << Streak<3>(Coord<3>(10,  20, 30), 209)
+            << Streak<3>(Coord<3>(10,  21, 30), 210)
+            << Streak<3>(Coord<3>(10,  30, 30), 210)
+            << Streak<3>(Coord<3>(10,  30, 40), 210)
+            << Streak<3>(Coord<3>(25,  31, 40), 200)
+            << Streak<3>(Coord<3>(25, 119, 40), 200)
+            << Streak<3>(Coord<3>(10,  30, 49), 210);
+
+        CoordBox<3> boxNew = gridNew.boundingBox();
+        CoordBox<3> boxOld = gridOld.boundingBox();
+        Coord<3> offsetOld = -boxOld.origin + gridOld.getEdgeRadii();
+        Coord<3> offsetNew = -boxNew.origin + gridNew.getEdgeRadii();
+        gridOld.callback(&gridNew, FixedNeighborhoodUpdateFunctor<
+                         MySoATestCellWithTwoDoublesCube,
+                         UpdateFunctorHelpers::ConcurrencyNoP,
+                         APITraits::SelectThreadedUpdate<void>::Value>(
+                             &region, &offsetOld, &offsetNew, &boxNew.dimensions, 0, 0, 0));
+
+        for (Region<3>::Iterator i = region.begin(); i != region.end(); ++i) {
+            MySoATestCellWithTwoDoublesCube cell = gridNew.get(*i);
+
+            Coord<3> sum;
+            sum += normalizeCube(*i + Coord<3>( 0,  0, -1), box);
+            sum += normalizeCube(*i + Coord<3>( 0, -1,  0), box);
+            sum += normalizeCube(*i + Coord<3>(-1,  0,  0), box);
+            sum += normalizeCube(*i + Coord<3>( 1,  0,  0), box);
+            sum += normalizeCube(*i + Coord<3>( 0,  1,  0), box);
+            sum += normalizeCube(*i + Coord<3>( 0,  0,  1), box);
+
+            double expected = sum.x() + sum.y() * 1000.0 + sum.z() * 1000 * 1000.0;
+            TS_ASSERT_EQUALS(cell.valB, expected);
+        }
+    }
+
     // fixme: test with torus topo and displacements (as the z-curve would imply them with pars of the region sitting on opposite edges of the simulation space)
 
 private:
-    Coord<3> normalize(const Coord<3>& coord, const CoordBox<3>& box)
+    Coord<3> normalizeTorus(const Coord<3>& coord, const CoordBox<3>& box)
     {
         return box.origin + Topologies::Torus<3>::Topology::normalize(coord - box.origin, box.dimensions);
+    }
+
+    Coord<3> normalizeCube(const Coord<3>& coord, const CoordBox<3>& box)
+    {
+        if (!box.inBounds(coord)) {
+            return Coord<3>(-1, -1, -1);
+        }
+
+        return coord;
     }
 };
 
