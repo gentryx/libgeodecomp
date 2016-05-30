8107b5ae0c7192170b44423aab96e71680d0dbd8
Andreas Schäfer <gentryx@gmx.de>
2016-05-30 18:20:24 +0200

    add own string tokenizer, drop use of boost::split()

--- src/misc/stringops.h
+++ src/misc/stringops.h
@@ -2,8 +2,7 @@
 #define LIBGEODECOMP_MISC_STRINGOPS_H
 
 #include <libgeodecomp/misc/stringvec.h>
-
-#include <boost/algorithm/string.hpp>
+#include <set>
 #include <sstream>
 
 // sad but true: CodeGear's C++ compiler has troubles with the +
@@ -54,8 +53,32 @@ public:
     static StringVec tokenize(const std::string& string, const std::string& delimiters)
     {
         StringVec ret;
-        boost::split(ret, string, boost::is_any_of(delimiters), boost::token_compress_on);
-        del(ret, "");
+
+        std::set<std::size_t> startPositions;
+        std::set<std::size_t> endPositions;
+
+        startPositions.insert(0);
+        endPositions.insert(string.size());
+
+        for (std::string::const_iterator delimiter = delimiters.begin(); delimiter != delimiters.end(); ++delimiter) {
+            std::size_t pos = string.find(*delimiter, 0);
+
+            for (;
+                 (pos != std::string::npos) && (pos < string.length());
+                 pos = string.find(*delimiter, pos + 1)) {
+                endPositions.insert(pos);
+                startPositions.insert(pos + 1);
+            }
+        }
+
+        std::set<std::size_t>::iterator i = startPositions.begin();
+        std::set<std::size_t>::iterator j = endPositions.begin();
+
+        for (; i != startPositions.end(); ++i, ++j) {
+            if (*i != *j) {
+                ret << string.substr(*i, *j - *i);
+            }
+        }
 
         return ret;
     }
