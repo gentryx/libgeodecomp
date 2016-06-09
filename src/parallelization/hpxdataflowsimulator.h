2b3ec68c267719a514ff9402bfdc90059dc135a8
Andreas Schäfer <gentryx@gmx.de>
2016-06-09 11:47:23 +0200

    moving classes from test to header

--- src/parallelization/hpxdataflowsimulator.h
+++ src/parallelization/hpxdataflowsimulator.h
@@ -8,6 +8,97 @@
 
 namespace LibGeoDecomp {
 
+namespace HPXDataFlowSimulatorHelpers {
+
+template<typename MESSAGE>
+class Neighborhood
+{
+public:
+    // fixme: move semantics
+    inline Neighborhood(
+        std::vector<int> messageNeighborIDs,
+        std::vector<hpx::shared_future<MESSAGE> > messagesFromNeighbors,
+        const std::map<int, hpx::id_type>& remoteIDs) :
+        messageNeighborIDs(messageNeighborIDs),
+        messagesFromNeighbors(hpx::util::unwrapped(messagesFromNeighbors)),
+        remoteIDs(remoteIDs)
+    {}
+
+    const MESSAGE& operator[](int index) const
+    {
+        std::vector<int>::const_iterator i = std::find(messageNeighborIDs.begin(), messageNeighborIDs.end(), index);
+        if (i == messageNeighborIDs.end()) {
+            throw std::logic_error("ID not found for incoming messages");
+        }
+
+        return messagesFromNeighbors[i - messageNeighborIDs.begin()];
+    }
+
+    void send(int remoteCellID, const MESSAGE& message, int step) const
+    {
+        std::map<int, hpx::id_type>::const_iterator iter = remoteIDs.find(remoteCellID);
+        if (iter == remoteIDs.end()) {
+            throw std::logic_error("ID not found for outgoing messages");
+        }
+
+        hpx::apply(
+            typename HPXReceiver<MESSAGE>::receiveAction(),
+            iter->second,
+            step,
+            message);
+    }
+
+private:
+    std::vector<int> messageNeighborIDs;
+    std::vector<MESSAGE> messagesFromNeighbors;
+    std::map<int, hpx::id_type> remoteIDs;
+};
+
+// fixme: componentize
+template<typename CELL, typename MESSAGE>
+class CellComponent
+{
+public:
+    // fixme: move semantics
+    explicit CellComponent(CELL *cell = 0, int id = -1, std::vector<int> neighbors = std::vector<int>()) :
+        cell(cell),
+        id(id)
+    {
+        for (auto&& neighbor: neighbors) {
+            std::string linkName = endpointName(neighbor, id);
+            receivers[neighbor] = HPXReceiver<MESSAGE>::make(linkName).get();
+        }
+    }
+
+    // fixme: use move semantics here
+    void update(
+        std::vector<int> neighbors,
+        std::vector<hpx::shared_future<MESSAGE> > inputFutures,
+        const hpx::shared_future<void>& /* unused, just here to ensure
+                                           correct ordering of updates
+                                           per cell */,
+        int step)
+    {
+        Neighborhood<MESSAGE> hood(neighbors, inputFutures, remoteIDs);
+        cell->update(hood, step + 1);
+    }
+
+    static std::string endpointName(int sender, int receiver)
+    {
+        return "HPXDataflowSimulatorEndPoint_" +
+            StringOps::itoa(sender) +
+            "_to_" +
+            StringOps::itoa(receiver);
+
+    }
+
+    CELL *cell;
+    int id;
+    std::map<int, std::shared_ptr<HPXReceiver<MESSAGE> > > receivers;
+    std::map<int, hpx::id_type> remoteIDs;
+};
+
+}
 /**
  * Experimental Simulator based on (surprise surprise) HPX' dataflow
  operator. Primary use case (for now) is DGSWEM.
@@ -15,6 +106,10 @@ namespace LibGeoDecomp {
 template<typename CELL>
 class HPXDataflowSimulator
 {
+public:
+
+private:
+
 };
 
 }
--- src/parallelization/test/parallel_hpx_4_1/hpxdataflowsimulatortest.h
+++ src/parallelization/test/parallel_hpx_4_1/hpxdataflowsimulatortest.h
@@ -158,92 +158,6 @@ private:
      }
 };
 
-template<typename MESSAGE>
-class HPXDataflowNeighborhood
-{
-public:
-    // fixme: move semantics
-    inline HPXDataflowNeighborhood(
-        std::vector<int> messageNeighborIDs,
-        std::vector<hpx::shared_future<MESSAGE> > messagesFromNeighbors,
-        const std::map<int, hpx::id_type>& remoteIDs) :
-        messageNeighborIDs(messageNeighborIDs),
-        messagesFromNeighbors(hpx::util::unwrapped(messagesFromNeighbors)),
-        remoteIDs(remoteIDs)
-    {}
-
-    const MESSAGE& operator[](int index) const
-    {
-        std::vector<int>::const_iterator i = std::find(messageNeighborIDs.begin(), messageNeighborIDs.end(), index);
-        if (i == messageNeighborIDs.end()) {
-            throw std::logic_error("ID not found for incoming messages");
-        }
-
-        return messagesFromNeighbors[i - messageNeighborIDs.begin()];
-    }
-
-    void send(int remoteCellID, const DummyMessage& message, int step) const
-    {
-        std::map<int, hpx::id_type>::const_iterator iter = remoteIDs.find(remoteCellID);
-        if (iter == remoteIDs.end()) {
-            throw std::logic_error("ID not found for outgoing messages");
-        }
-
-        hpx::apply(
-            typename HPXReceiver<DummyMessage>::receiveAction(),
-            iter->second,
-            step,
-            message);
-    }
-
-private:
-    std::vector<int> messageNeighborIDs;
-    std::vector<MESSAGE> messagesFromNeighbors;
-    std::map<int, hpx::id_type> remoteIDs;
-};
-template<typename CELL>
-class TestComponent
-{
-public:
-    // fixme: move semantics
-    explicit TestComponent(CELL *cell = 0, int id = -1, std::vector<int> neighbors = std::vector<int>()) :
-        cell(cell),
-        id(id)
-    {
-        for (auto&& neighbor: neighbors) {
-            std::string linkName = TestComponent<DummyMessage>::endpointName(neighbor, id);
-            receivers[neighbor] = HPXReceiver<DummyMessage>::make(linkName).get();
-        }
-    }
-
-    // fixme: use move semantics here
-    void update(
-        std::vector<int> neighbors,
-        std::vector<hpx::shared_future<DummyMessage> > inputFutures,
-        const hpx::shared_future<void>& /* unused, just here to ensure
-                                           correct ordering of updates
-                                           per cell */,
-        int step)
-    {
-        HPXDataflowNeighborhood<DummyMessage> hood(neighbors, inputFutures, remoteIDs);
-        cell->update(hood, step + 1);
-    }
-
-    static std::string endpointName(int sender, int receiver)
-    {
-        return "HPXDataflowSimulatorEndPoint_" +
-            StringOps::itoa(sender) +
-            "_to_" +
-            StringOps::itoa(receiver);
-
-    }
-
-    CELL *cell;
-    int id;
-    std::map<int, std::shared_ptr<HPXReceiver<DummyMessage> > > receivers;
-    std::map<int, hpx::id_type> remoteIDs;
-};
-
 class HpxDataflowSimulatorTest : public CxxTest::TestSuite
 {
 public:
@@ -257,6 +171,7 @@ public:
         UnstructuredGrid<DummyModel> grid(initializer.gridBox());
         initializer.grid(&grid);
 
+        // move to simulator:
         typedef hpx::shared_future<void> UpdateResultFuture;
         typedef std::vector<UpdateResultFuture> TimeStepFutures;
 
@@ -268,6 +183,7 @@ public:
         Region<1> localRegion;
         CoordBox<1> box = initializer.gridBox();
         int rank = hpx::get_locality_id();
+        // fixme: don't hardcode num of localities
         for (int i = ((rank + 0) * box.dimensions.x() / 4);
              i <     ((rank + 1) * box.dimensions.x() / 4);
              ++i) {
@@ -277,24 +193,25 @@ public:
         boost::shared_ptr<Adjacency> adjacency = initializer.getAdjacency(localRegion);
 
         // fixme: instantiate components in agas and only hold ids of those
-        std::map<int, TestComponent<DummyModel> > components;
+        std::map<int, HPXDataFlowSimulatorHelpers::CellComponent<DummyModel, DummyMessage> > components;
         std::vector<int> neighbors;
 
         for (Region<1>::Iterator i = localRegion.begin(); i != localRegion.end(); ++i) {
             neighbors.clear();
             adjacency->getNeighbors(i->x(), &neighbors);
-            TestComponent<DummyModel> component(&grid[*i], i->x(), neighbors);
+            HPXDataFlowSimulatorHelpers::CellComponent<DummyModel, DummyMessage> component(&grid[*i], i->x(), neighbors);
             components[i->x()] = component;
         }
 
         for (Region<1>::Iterator i = localRegion.begin(); i != localRegion.end(); ++i) {
-            TestComponent<DummyModel>& component = components[i->x()];
+            HPXDataFlowSimulatorHelpers::CellComponent<DummyModel, DummyMessage>& component = components[i->x()];
 
             neighbors.clear();
             adjacency->getNeighbors(i->x(), &neighbors);
 
             for (auto j = neighbors.begin(); j != neighbors.end(); ++j) {
-                std::string linkName = TestComponent<DummyMessage>::endpointName(i->x(), *j);
+                std::string linkName = HPXDataFlowSimulatorHelpers::CellComponent<DummyMessage, DummyMessage>::endpointName(
+                    i->x(), *j);
                 component.remoteIDs[*j] = hpx::id_type(HPXReceiver<DummyMessage>::find(linkName).get());
             }
         }
@@ -323,7 +240,8 @@ public:
                         }
                 }
 
-                auto Operation = boost::bind(&TestComponent<DummyModel>::update, components[i->x()], _1, _2, _3, _4);
+                auto Operation = boost::bind(&HPXDataFlowSimulatorHelpers::CellComponent<DummyModel, DummyMessage>::update,
+                                             components[i->x()], _1, _2, _3, _4);
 
                 thisTimeStepFutures[index] = dataflow(
                     hpx::launch::async,
