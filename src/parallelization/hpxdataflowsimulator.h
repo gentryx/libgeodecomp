06415888286cacb0ee587293ac347e5a2a3b19e0
Andreas Schäfer <gentryx@gmx.de>
2016-06-09 13:31:51 +0200

    moving rest of prototype to HPXDataflowSimulator

--- src/parallelization/hpxdataflowsimulator.h
+++ src/parallelization/hpxdataflowsimulator.h
@@ -5,6 +5,7 @@
 #ifdef LIBGEODECOMP_WITH_HPX
 
 #include <libgeodecomp/communication/hpxreceiver.h>
+#include <libgeodecomp/parallelization/distributedsimulator.h>
 
 namespace LibGeoDecomp {
 
@@ -99,14 +100,129 @@ public:
 };
 
 }
+
+// fixme
+class DummyMessage;
+
 /**
  * Experimental Simulator based on (surprise surprise) HPX' dataflow
  operator. Primary use case (for now) is DGSWEM.
  */
 template<typename CELL>
-class HPXDataflowSimulator
+class HPXDataflowSimulator : public DistributedSimulator<CELL>
 {
 public:
+    using DistributedSimulator<CELL>::initializer;
+
+    inline HPXDataflowSimulator(Initializer<CELL> *initializer) :
+        DistributedSimulator<CELL>(initializer)
+    {}
+
+    void step()
+    {
+        // fixme
+    }
+
+    void run()
+    {
+        UnstructuredGrid<CELL> grid(initializer->gridBox());
+        initializer->grid(&grid);
+        // move to simulator:
+        typedef hpx::shared_future<void> UpdateResultFuture;
+        typedef std::vector<UpdateResultFuture> TimeStepFutures;
+
+        using hpx::dataflow;
+        using hpx::util::unwrapped;
+        TimeStepFutures lastTimeStepFutures;
+        TimeStepFutures thisTimeStepFutures;
+
+        Region<1> localRegion;
+        CoordBox<1> box = initializer->gridBox();
+        int rank = hpx::get_locality_id();
+        // fixme: don't hardcode num of localities
+        for (int i = ((rank + 0) * box.dimensions.x() / 4);
+             i <     ((rank + 1) * box.dimensions.x() / 4);
+             ++i) {
+            localRegion << Coord<1>(i);
+        }
+
+        boost::shared_ptr<Adjacency> adjacency = initializer->getAdjacency(localRegion);
+
+        // fixme: instantiate components in agas and only hold ids of those
+        std::map<int, HPXDataFlowSimulatorHelpers::CellComponent<CELL, DummyMessage> > components;
+        std::vector<int> neighbors;
+
+        for (Region<1>::Iterator i = localRegion.begin(); i != localRegion.end(); ++i) {
+            neighbors.clear();
+            adjacency->getNeighbors(i->x(), &neighbors);
+            HPXDataFlowSimulatorHelpers::CellComponent<CELL, DummyMessage> component(&grid[*i], i->x(), neighbors);
+            components[i->x()] = component;
+        }
+
+        for (Region<1>::Iterator i = localRegion.begin(); i != localRegion.end(); ++i) {
+            HPXDataFlowSimulatorHelpers::CellComponent<CELL, DummyMessage>& component = components[i->x()];
+
+            neighbors.clear();
+            adjacency->getNeighbors(i->x(), &neighbors);
+
+            for (auto j = neighbors.begin(); j != neighbors.end(); ++j) {
+                std::string linkName = HPXDataFlowSimulatorHelpers::CellComponent<DummyMessage, DummyMessage>::endpointName(
+                    i->x(), *j);
+                component.remoteIDs[*j] = hpx::id_type(HPXReceiver<DummyMessage>::find(linkName).get());
+            }
+        }
+
+        for (Region<1>::Iterator i = localRegion.begin(); i != localRegion.end(); ++i) {
+            lastTimeStepFutures << hpx::make_ready_future(UpdateResultFuture());
+        }
+        thisTimeStepFutures.resize(localRegion.size());
+
+        // fixme: add steerer/writer interaction
+        int maxTimeSteps = initializer->maxSteps();
+        for (int t = 0; t < maxTimeSteps; ++t) {
+            int index = 0;
+
+            for (Region<1>::Iterator i = localRegion.begin(); i != localRegion.end(); ++i) {
+
+                std::vector<hpx::shared_future<DummyMessage> > receiveMessagesFutures;
+                neighbors.clear();
+                adjacency->getNeighbors(i->x(), &neighbors);
+
+                    for (auto j = neighbors.begin(); j != neighbors.end(); ++j) {
+                        if (t > 0) {
+                            receiveMessagesFutures << components[i->x()].receivers[*j]->get(t);
+                        } else {
+                            int data = *j * 100 + i->x();
+                            receiveMessagesFutures <<  hpx::make_ready_future(DummyMessage(*j, i->x(), 0, data));
+                        }
+                }
+
+                auto Operation = boost::bind(&HPXDataFlowSimulatorHelpers::CellComponent<CELL, DummyMessage>::update,
+                                             components[i->x()], _1, _2, _3, _4);
+
+                thisTimeStepFutures[index] = dataflow(
+                    hpx::launch::async,
+                    Operation,
+                    neighbors,
+                    receiveMessagesFutures,
+                    lastTimeStepFutures[index],
+                    t);
+
+                ++index;
+            }
+
+            using std::swap;
+            swap(thisTimeStepFutures, lastTimeStepFutures);
+        }
+
+        hpx::when_all(lastTimeStepFutures).get();
+    }
+
+    std::vector<Chronometer> gatherStatistics()
+    {
+        // fixme
+        return std::vector<Chronometer>();
+    }
 
 private:
 
