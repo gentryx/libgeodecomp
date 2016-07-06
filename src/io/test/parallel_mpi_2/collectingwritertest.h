5553eb2042aa3e8712c3b14a74ace3b9ce401c32
Andreas Schäfer <gentryx@gmx.de>
2016-07-06 09:49:57 +0200

    add test for CollectingWriter with SoA

--- src/io/test/parallel_mpi_2/collectingwritertest.h
+++ src/io/test/parallel_mpi_2/collectingwritertest.h
@@ -1,6 +1,7 @@
 #include <libgeodecomp/io/collectingwriter.h>
 #include <libgeodecomp/io/testinitializer.h>
 #include <libgeodecomp/io/memorywriter.h>
+#include <libgeodecomp/loadbalancer/noopbalancer.h>
 #include <libgeodecomp/loadbalancer/randombalancer.h>
 #include <libgeodecomp/misc/testhelper.h>
 #include <libgeodecomp/parallelization/stripingsimulator.h>
@@ -21,10 +22,9 @@ public:
         LoadBalancer *balancer = MPILayer().rank()? 0 : new RandomBalancer;
         sim.reset(new StripingSimulator<TestCell<3> >(init, balancer));
 
+        writer = 0;
         if (MPILayer().rank() == 0) {
             writer = new MemoryWriter<TestCell<3> >(3);
-        } else {
-            writer = 0;
         }
 
         sim->addWriter(new CollectingWriter<TestCell<3> >(writer, 0));
@@ -55,6 +55,40 @@ public:
             // simulator will notify the writer of this time step
             // twice (WRITER_STEP_FINISHED and WRITER_ALL_DONE)
             TS_ASSERT_TEST_GRID(MemoryWriter<TestCell<3> >::GridType, writer->getGrids()[size - 1], cycle);
+        }
+    }
+
+    void testSoA()
+    {
+        TestInitializer<TestCellSoA> *init = new TestInitializer<TestCellSoA>();
+
+        LoadBalancer *balancer = MPILayer().rank()? 0 : new RandomBalancer;
+        StripingSimulator<TestCellSoA> sim(init, balancer);
+
+        MemoryWriter<TestCellSoA> *writer = 0;
+        if (MPILayer().rank() == 0) {
+            writer = new MemoryWriter<TestCellSoA>(3);
+        }
+
+        sim.addWriter(new CollectingWriter<TestCellSoA>(writer, 0));
+        sim.run();
+
+        if (MPILayer().rank() == 0) {
+            int size = writer->getGrids().size();
+            unsigned cycle = 0;
+
+            for (int i = 0; i < (size - 1); ++i) {
+                cycle = APITraits::SelectNanoSteps<TestCellSoA>::VALUE * i * 3;
+
+                TS_ASSERT_TEST_GRID(MemoryWriter<TestCellSoA>::GridType, writer->getGrids()[i], cycle);
+            }
+
+            cycle = APITraits::SelectNanoSteps<TestCellSoA>::VALUE * TestInitializer<TestCellSoA>().maxSteps();
+
+            // check the last grid with the same cycle counter as the
+            // simulator will notify the writer of this time step
+            // twice (WRITER_STEP_FINISHED and WRITER_ALL_DONE)
+            TS_ASSERT_TEST_GRID(MemoryWriter<TestCellSoA>::GridType, writer->getGrids()[size - 1], cycle);
         }
     }
 
