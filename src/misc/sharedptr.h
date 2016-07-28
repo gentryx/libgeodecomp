f2ca34330a1d19d19d0c790fb988fe4e3ac8681f
Andreas Schäfer <gentryx@gmx.de>
2016-07-28 03:18:55 +0200

    minor fix: SharedPtr needs to be a template

---
+++ src/misc/sharedptr.h
@@ -0,0 +1,30 @@
+#ifndef LIBGEODECOMP_MISC_SHAREDPTR_H
+#define LIBGEODECOMP_MISC_SHAREDPTR_H
+
+#ifdef LIBGEODECOMP_WITH_BOOST_SHARED_PTR
+
+#include <boost/shared_ptr.hpp>
+#else
+#include <memory>
+#endif
+
+namespace LibGeoDecomp {
+
+/**
+ * Selector for shared pointer implementations (std::shared_ptr vs.
+ * boost::shared_ptr).
+ */
+template<typename CARGO>
+class SharedPtr
+{
+public:
+#ifdef LIBGEODECOMP_WITH_BOOST_SHARED_PTR
+    typedef boost::shared_ptr<CARGO> Type;
+#else
+    typedef std::shared_ptr<CARGO> Type;
+#endif
+};
+
+}
+
+#endif
