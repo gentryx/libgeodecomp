2b21b3705f610888702a305144e859b976b6a1e4
Andreas Schäfer <gentryx@gmx.de>
2016-09-27 14:11:46 +0200

    add (empty) test for ReorderingUnstructuredGrid

---
+++ src/storage/test/unit/reorderingunstructuredgridtest.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cxxtest/TestSuite.h>
+#include <libgeodecomp/misc/unstructuredtestcell.h>
+
+using namespace LibGeoDecomp;
+
+namespace LibGeoDecomp {
+
+class ReorderingUnstructuredGridTest : public CxxTest::TestSuite
+{
+public:
+    void testBasic()
+    {
+        std::cout << "BÄM!\n";
+    }
+};
+
+}
