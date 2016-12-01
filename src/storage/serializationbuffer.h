a33e63b1abee6ce93168e9e43d26f501221b5464
Andreas Schäfer <gentryx@gmx.de>
2016-12-01 10:11:02 +0100

    temporary workaround for #46

--- src/storage/serializationbuffer.h
+++ src/storage/serializationbuffer.h
@@ -97,43 +97,45 @@ public:
 /**
  * see above
  */
-template<typename CELL>
-class Implementation<CELL, void, typename CELL::API::SupportsBoostSerialization>
-{
-public:
-    typedef std::vector<char> BufferType;
-    typedef char ElementType;
-    typedef typename APITraits::FalseType FixedSize;
+// temporarily disabled until #46 ( https://github.com/gentryx/libgeodecomp/issues/46 ) is fixed.
 
-    template<typename REGION>
-    static BufferType create(const REGION& region)
-    {
-        return BufferType();
-    }
+// template<typename CELL>
+// class Implementation<CELL, void, typename CELL::API::SupportsBoostSerialization>
+// {
+// public:
+//     typedef std::vector<char> BufferType;
+//     typedef char ElementType;
+//     typedef typename APITraits::FalseType FixedSize;
 
-    template<typename REGION>
-    static std::size_t storageSize(const REGION& region)
-    {
-        return region.size();
-    }
+//     template<typename REGION>
+//     static BufferType create(const REGION& region)
+//     {
+//         return BufferType();
+//     }
 
-    template<typename REGION>
-    static void resize(BufferType *buffer, const REGION& region)
-    {
-        buffer->resize(storageSize(region));
-    }
-    static ElementType *getData(BufferType& buffer)
-    {
-        return &buffer.front();
-    }
+//     template<typename REGION>
+//     static std::size_t storageSize(const REGION& region)
+//     {
+//         return region.size();
+//     }
 
-#ifdef LIBGEODECOMP_WITH_MPI
-    static inline MPI_Datatype cellMPIDataType()
-    {
-        return MPI_CHAR;
-    }
-#endif
-};
+//     template<typename REGION>
+//     static void resize(BufferType *buffer, const REGION& region)
+//     {
+//         buffer->resize(storageSize(region));
+//     }
+//     static ElementType *getData(BufferType& buffer)
+//     {
+//         return &buffer.front();
+//     }
+
+// #ifdef LIBGEODECOMP_WITH_MPI
+//     static inline MPI_Datatype cellMPIDataType()
+//     {
+//         return MPI_CHAR;
+//     }
+// #endif
+// };
 
 }
 
