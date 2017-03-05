891c64052aecfe88e78ef65fbbfb3fc192c7c752
Andreas Schäfer <gentryx@gmx.de>
2017-03-05 17:40:08 +0100

    debug test generation on AppVeyor

--- lib/cxxtest/bin/cxxtestgen.bat
+++ lib/cxxtest/bin/cxxtestgen.bat
@@ -1,3 +1,13 @@
 @echo off
 rem Just run the python script
+
+echo "ANDI1 %PYTHON%"
+
+IF "%PYTHON%" == "" GOTO DEFAULT_PYTHON
+:PYTHON_OVERRIDE
+"%PYTHON%" %0 %*
+GOTO END
+:DEFAULT_PYTHON
 python %0 %*
+GOTO END
+:END
