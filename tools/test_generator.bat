c425f7f2cf98f61072bfa80c146e15b6099358f4
Andreas Schäfer <gentryx@gmx.de>
2017-03-16 02:10:27 +0100

    add Windows wrapper for test generator

---
+++ tools/test_generator.bat
@@ -0,0 +1,15 @@
+@echo off
+rem Windows wrapper for the original Python script as Windows doesn't
+rem know what a shebang is.
+
+IF "%PYTHON%" == "" GOTO DEFAULT_PYTHON
+:PYTHON_OVERRIDE
+"%PYTHON%" %0 %*
+GOTO END
+:DEFAULT_PYTHON
+python %0 %*
+GOTO END
+:END
+
+exit /B 0
+
