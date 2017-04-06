7325d7ba34a7b765b3112077daa092209515d9c7
Andreas Schäfer <gentryx@gmx.de>
2017-04-06 10:02:55 +0200

    safari, stop 5

--- src/libgeodecomp/io/mockinitializer.h
+++ src/libgeodecomp/io/mockinitializer.h
@@ -8,7 +8,7 @@
 
 // #include <libgeodecomp/geometry/adjacencymanufacturer.h>
 #include <stdexcept>
-// #include <libgeodecomp/geometry/adjacency.h>
+#include <libgeodecomp/geometry/adjacency.h>
 // #include <libgeodecomp/geometry/region.h>
 // #include <libgeodecomp/geometry/regionbasedadjacency.h>
 // #include <libgeodecomp/misc/sharedptr.h>
