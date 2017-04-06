8ad518767f43e9c27917ebf83f41975d0d2f7707
Andreas Schäfer <gentryx@gmx.de>
2017-04-06 15:54:41 +0200

    safari, stop 27

--- src/libgeodecomp/io/mockinitializer.h
+++ src/libgeodecomp/io/mockinitializer.h
@@ -28,14 +28,17 @@
 
 // ...fine above
 
+#include <libgeodecomp/storage/filter.h>
+
 // #include <libgeodecomp/storage/defaultarrayfilter.h>
 // #include <libgeodecomp/config.h>
 // #include <libgeodecomp/io/logger.h>
 // #include <libgeodecomp/storage/filterbase.h>
 // #include <libgeodecomp/storage/memorylocation.h>
+// fine below...
+
 #include <typeinfo>
 
-// fine below...
 
 // #include <libgeodecomp/storage/defaultcudafilter.h>
 // #include <libgeodecomp/storage/defaultcudaarrayfilter.h>
