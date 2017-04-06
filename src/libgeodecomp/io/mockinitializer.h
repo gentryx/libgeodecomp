80a94c28c369ae61c97ecf4ff3a41ecdc63698b5
Andreas Schäfer <gentryx@gmx.de>
2017-04-06 15:13:52 +0200

    safari, stop 18

--- src/libgeodecomp/io/mockinitializer.h
+++ src/libgeodecomp/io/mockinitializer.h
@@ -25,16 +25,21 @@
 
 // ...fine above
 
-#include <libgeodecomp/storage/defaultfilterfactory.h>
+// #include <libgeodecomp/storage/defaultfilterfactory.h>
+#include <libgeodecomp/misc/sharedptr.h>
+// #include <libgeodecomp/storage/defaultarrayfilter.h>
+// #include <libgeodecomp/storage/defaultcudafilter.h>
+// #include <libgeodecomp/storage/defaultcudaarrayfilter.h>
+// #include <libgeodecomp/storage/defaultfilter.h>
 
+
+// fine below...
 
 // #include <libgeodecomp/storage/filterbase.h>
 // #include <libgeodecomp/storage/memberfilter.h>
 // #include <stdexcept>
 // #include <typeinfo>
 
-
-// fine below...
 
 #include <cstddef>
 #include <vector>
